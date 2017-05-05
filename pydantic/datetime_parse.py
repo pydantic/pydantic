90bc68113724c622c57c254c7cab635744a10d71
Samuel Colvin <s@muelcolvin.com>
2017-05-05 19:38:12 +0100

    adding datetime parsing stolen from django

---
+++ pydantic/datetime_parse.py
@@ -0,0 +1,181 @@
+"""
+Functions to parse datetime objects.
+
+We're using regular expressions rather than time.strptime because:
+- They provide both validation and parsing.
+- They're more flexible for datetimes.
+- The date/datetime/time constructors produce friendlier error messages.
+
+Stolen from https://raw.githubusercontent.com/django/django/master/django/utils/dateparse.py at
+9718fa2e8abe430c3526a9278dd976443d4ae3c6
+
+Changed to:
+* use standard python datetime types not django.utils.timezone
+* raise ValueError when regex doesn't match rather than returning None
+* support parsing unix timestamps
+"""
+import re
+from datetime import date, datetime, time, timedelta, timezone
+from typing import Union
+
+date_re = re.compile(r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})$')
+
+time_re = re.compile(
+    r'(?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
+    r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
+)
+
+datetime_re = re.compile(
+    r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})'
+    r'[T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
+    r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
+    r'(?P<tzinfo>Z|[+-]\d{2}(?::?\d{2})?)?$'
+)
+
+standard_duration_re = re.compile(
+    r'^'
+    r'(?:(?P<days>-?\d+) (days?, )?)?'
+    r'((?:(?P<hours>-?\d+):)(?=\d+:\d+))?'
+    r'(?:(?P<minutes>-?\d+):)?'
+    r'(?P<seconds>-?\d+)'
+    r'(?:\.(?P<microseconds>\d{1,6})\d{0,6})?'
+    r'$'
+)
+
+# Support the sections of ISO 8601 date representation that are accepted by
+# timedelta
+iso8601_duration_re = re.compile(
+    r'^(?P<sign>[-+]?)'
+    r'P'
+    r'(?:(?P<days>\d+(.\d+)?)D)?'
+    r'(?:T'
+    r'(?:(?P<hours>\d+(.\d+)?)H)?'
+    r'(?:(?P<minutes>\d+(.\d+)?)M)?'
+    r'(?:(?P<seconds>\d+(.\d+)?)S)?'
+    r')?'
+    r'$'
+)
+
+EPOCH = datetime(1970, 1, 1)
+MS_WATERSHED = int(1e11)  # if greater than this, the number is in ms (in seconds this is 3rd March 5138)
+StrIntFloat = Union[str, int, float]
+
+
+def get_numeric(value: StrIntFloat):
+    if isinstance(value, (int, float)):
+        return value
+    try:
+        return int(value)
+    except ValueError:
+        pass
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+
+def from_unix_seconds(seconds: int) -> datetime:
+    while seconds > MS_WATERSHED:
+        seconds /= 1000
+    dt = EPOCH + timedelta(seconds=seconds)
+    return dt.replace(tzinfo=timezone.utc)
+
+
+def parse_date(value: StrIntFloat) -> date:
+    """
+    Parse a string and return a datetime.date.
+
+    Raise ValueError if the input is well formatted but not a valid date.
+    Raise ValueError if the input isn't well formatted.
+    """
+    number = get_numeric(value)
+    if number:
+        return from_unix_seconds(number).date()
+
+    match = date_re.match(value)
+    if not match:
+        raise ValueError('Invalid date format')
+
+    kw = {k: int(v) for k, v in match.groupdict().items()}
+    return date(**kw)
+
+
+def parse_time(value: StrIntFloat) -> time:
+    """
+    Parse a string and return a datetime.time.
+
+    This function doesn't support time zone offsets.
+
+    Raise ValueError if the input is well formatted but not a valid time.
+    Raise ValueError if the input isn't well formatted, in particular if it contains an offset.
+    """
+    match = time_re.match(value)
+    if not match:
+        raise ValueError('Invalid time format')
+
+    kw = match.groupdict()
+    if kw['microsecond']:
+        kw['microsecond'] = kw['microsecond'].ljust(6, '0')
+    kw = {k: int(v) for k, v in kw.items() if v is not None}
+    return time(**kw)
+
+
+def parse_datetime(value: StrIntFloat) -> datetime:
+    """
+    Parse a string and return a datetime.datetime.
+
+    This function supports time zone offsets. When the input contains one,
+    the output uses a timezone with a fixed offset from UTC.
+
+    Raise ValueError if the input is well formatted but not a valid datetime.
+    Raise ValueError if the input isn't well formatted.
+    """
+    number = get_numeric(value)
+    if number:
+        return from_unix_seconds(number)
+
+    match = datetime_re.match(value)
+    if not match:
+        raise ValueError('Invalid datetime format')
+
+    kw = match.groupdict()
+    if kw['microsecond']:
+        kw['microsecond'] = kw['microsecond'].ljust(6, '0')
+    tzinfo = kw.pop('tzinfo')
+    if tzinfo == 'Z':
+        tzinfo = timezone.utc
+    elif tzinfo is not None:
+        offset_mins = int(tzinfo[-2:]) if len(tzinfo) > 3 else 0
+        offset = 60 * int(tzinfo[1:3]) + offset_mins
+        if tzinfo[0] == '-':
+            offset = -offset
+        tzinfo = timezone(timedelta(minutes=offset))
+    kw = {k: int(v) for k, v in kw.items() if v is not None}
+    kw['tzinfo'] = tzinfo
+    return datetime(**kw)
+
+
+def parse_duration(value: StrIntFloat) -> timedelta:
+    """
+    Parse a duration string and return a datetime.timedelta.
+
+    The preferred format for durations in Django is '%d %H:%M:%S.%f'.
+
+    Also supports ISO 8601 representation.
+    """
+    if isinstance(value, (int, float)):
+        # bellow code requires a string
+        value = str(value)
+
+    match = standard_duration_re.match(value) or iso8601_duration_re.match(value)
+    if not match:
+        raise ValueError('Invalid duration format')
+
+    kw = match.groupdict()
+    sign = -1 if kw.pop('sign', '+') == '-' else 1
+    if kw.get('microseconds'):
+        kw['microseconds'] = kw['microseconds'].ljust(6, '0')
+    if kw.get('seconds') and kw.get('microseconds') and kw['seconds'].startswith('-'):
+        kw['microseconds'] = '-' + kw['microseconds']
+    kw = {k: float(v) for k, v in kw.items() if v is not None}
+    return sign * timedelta(**kw)
---
+++ tests/test_datetime_parse.py
@@ -0,0 +1,128 @@
+"""
+Stolen from https://github.com/django/django/blob/master/tests/utils_tests/test_dateparse.py at
+9718fa2e8abe430c3526a9278dd976443d4ae3c6
+
+Changed to:
+* use standard pytest layout
+* parametrize tests
+"""
+from datetime import date, datetime, time, timedelta, timezone
+
+import pytest
+
+from pydantic.datetime_parse import parse_date, parse_datetime, parse_duration, parse_time
+
+
+def create_tz(minutes):
+    return timezone(timedelta(minutes=minutes))
+
+
+@pytest.mark.parametrize('func,value,result', [
+    # Valid inputs
+    (parse_date, '1494012444.883309', date(2017, 5, 5)),
+    (parse_date, 1494012444.883309, date(2017, 5, 5)),
+    (parse_date, '1494012444', date(2017, 5, 5)),
+    (parse_date, 1494012444, date(2017, 5, 5)),
+    (parse_date, '2012-04-23', date(2012, 4, 23)),
+    (parse_date, '2012-4-9', date(2012, 4, 9)),
+    # Invalid inputs
+    (parse_date, 'x20120423', ValueError),
+    (parse_date, '2012-04-56', ValueError),
+
+    # Valid inputs
+    (parse_time, '09:15:00', time(9, 15)),
+    (parse_time, '10:10', time(10, 10)),
+    (parse_time, '10:20:30.400', time(10, 20, 30, 400000)),
+    (parse_time, '4:8:16', time(4, 8, 16)),
+    # Invalid inputs
+    (parse_time, '091500', ValueError),
+    (parse_time, '09:15:90', ValueError),
+
+    # Valid inputs
+    # values in seconds
+    (parse_datetime, '1494012444.883309', datetime(2017, 5, 5, 19, 27, 24, 883309, tzinfo=timezone.utc)),
+    (parse_datetime, 1494012444.883309, datetime(2017, 5, 5, 19, 27, 24, 883309, tzinfo=timezone.utc)),
+    (parse_datetime, '1494012444', datetime(2017, 5, 5, 19, 27, 24, tzinfo=timezone.utc)),
+    (parse_datetime, 1494012444, datetime(2017, 5, 5, 19, 27, 24, tzinfo=timezone.utc)),
+    # values in ms
+    (parse_datetime, '1494012444000.883309', datetime(2017, 5, 5, 19, 27, 24, 883, tzinfo=timezone.utc)),
+    (parse_datetime, 1494012444000, datetime(2017, 5, 5, 19, 27, 24, tzinfo=timezone.utc)),
+
+    (parse_datetime, '2012-04-23T09:15:00', datetime(2012, 4, 23, 9, 15)),
+    (parse_datetime, '2012-4-9 4:8:16', datetime(2012, 4, 9, 4, 8, 16)),
+    (parse_datetime, '2012-04-23T09:15:00Z', datetime(2012, 4, 23, 9, 15, 0, 0, timezone.utc)),
+    (parse_datetime, '2012-4-9 4:8:16-0320', datetime(2012, 4, 9, 4, 8, 16, 0, create_tz(-200))),
+    (parse_datetime, '2012-04-23T10:20:30.400+02:30', datetime(2012, 4, 23, 10, 20, 30, 400000, create_tz(150))),
+    (parse_datetime, '2012-04-23T10:20:30.400+02', datetime(2012, 4, 23, 10, 20, 30, 400000, create_tz(120))),
+    (parse_datetime, '2012-04-23T10:20:30.400-02', datetime(2012, 4, 23, 10, 20, 30, 400000, create_tz(-120))),
+    # Invalid inputs
+    (parse_datetime, 'x20120423091500', ValueError),
+    (parse_datetime, '2012-04-56T09:15:90', ValueError),
+])
+def test_parsing(func, value, result):
+    if isinstance(result, type) and issubclass(result, BaseException):
+        with pytest.raises(result):
+            func(value)
+    else:
+        print(repr(func(value)))
+        assert func(value) == result
+
+
+@pytest.mark.parametrize('delta', [
+    timedelta(days=4, minutes=15, seconds=30, milliseconds=100),  # fractions of seconds
+    timedelta(hours=10, minutes=15, seconds=30),  # hours, minutes, seconds
+    timedelta(days=4, minutes=15, seconds=30),  # multiple days
+    timedelta(days=1, minutes=00, seconds=00),  # single day
+    timedelta(days=-4, minutes=15, seconds=30),  # negative durations
+    timedelta(minutes=15, seconds=30),  # minute & seconds
+    timedelta(seconds=30),  # seconds
+])
+def test_parse_python_format(delta):
+    assert parse_duration(format(delta)) == delta
+
+
+@pytest.mark.parametrize('value,result', [
+    # seconds
+    ('30', timedelta(seconds=30)),
+    (30, timedelta(seconds=30)),
+    (30.1, timedelta(seconds=30, milliseconds=100)),
+    # minutes seconds
+    ('15:30', timedelta(minutes=15, seconds=30)),
+    ('5:30', timedelta(minutes=5, seconds=30)),
+    # hours minutes seconds
+    ('10:15:30', timedelta(hours=10, minutes=15, seconds=30)),
+    ('1:15:30', timedelta(hours=1, minutes=15, seconds=30)),
+    ('100:200:300', timedelta(hours=100, minutes=200, seconds=300)),
+    # days
+    ('4 15:30', timedelta(days=4, minutes=15, seconds=30)),
+    ('4 10:15:30', timedelta(days=4, hours=10, minutes=15, seconds=30)),
+    # fractions of seconds
+    ('15:30.1', timedelta(minutes=15, seconds=30, milliseconds=100)),
+    ('15:30.01', timedelta(minutes=15, seconds=30, milliseconds=10)),
+    ('15:30.001', timedelta(minutes=15, seconds=30, milliseconds=1)),
+    ('15:30.0001', timedelta(minutes=15, seconds=30, microseconds=100)),
+    ('15:30.00001', timedelta(minutes=15, seconds=30, microseconds=10)),
+    ('15:30.000001', timedelta(minutes=15, seconds=30, microseconds=1)),
+    # negative
+    ('-4 15:30', timedelta(days=-4, minutes=15, seconds=30)),
+    ('-172800', timedelta(days=-2)),
+    ('-15:30', timedelta(minutes=-15, seconds=30)),
+    ('-1:15:30', timedelta(hours=-1, minutes=15, seconds=30)),
+    ('-30.1', timedelta(seconds=-30, milliseconds=-100)),
+    # iso_8601
+    ('P4Y', ValueError),
+    ('P4M', ValueError),
+    ('P4W', ValueError),
+    ('P4D', timedelta(days=4)),
+    ('P0.5D', timedelta(hours=12)),
+    ('PT5H', timedelta(hours=5)),
+    ('PT5M', timedelta(minutes=5)),
+    ('PT5S', timedelta(seconds=5)),
+    ('PT0.000005S', timedelta(microseconds=5)),
+])
+def test_parse_durations(value, result):
+    if isinstance(result, type) and issubclass(result, BaseException):
+        with pytest.raises(result):
+            parse_duration(value)
+    else:
+        assert parse_duration(value) == result
