99d7eafa6157e63bf7f39f1c9f0146373d2a844c
Nikita Grishko <gr1n@protonmail.com>
2018-05-10 14:11:45 +0300

    add ``Decimal``, ``ConstrainedDecimal`` and ``condecimal`` types #170 (#175)

--- pydantic/types.py
+++ pydantic/types.py
@@ -1,10 +1,11 @@
 import re
+from decimal import Decimal
 from typing import Optional, Pattern, Type, Union
 from uuid import UUID
 
 from .utils import import_string, make_dsn, validate_email
-from .validators import (anystr_length_validator, anystr_strip_whitespace, not_none_validator, number_size_validator,
-                         str_validator)
+from .validators import (anystr_length_validator, anystr_strip_whitespace, decimal_validator, not_none_validator,
+                         number_size_validator, str_validator)
 
 try:
     import email_validator
@@ -31,6 +32,8 @@ __all__ = [
     'confloat',
     'PositiveFloat',
     'NegativeFloat',
+    'ConstrainedDecimal',
+    'condecimal',
     'UUID1',
     'UUID3',
     'UUID4',
@@ -216,6 +219,67 @@ class PositiveFloat(ConstrainedFloat):
 
 class NegativeFloat(ConstrainedFloat):
     lt = 0
+
+
+class ConstrainedDecimal(Decimal):
+    gt: Union[None, int, float, Decimal] = None
+    lt: Union[None, int, float, Decimal] = None
+    max_digits: Optional[int] = None
+    decimal_places: Optional[int] = None
+
+    @classmethod
+    def get_validators(cls):
+        yield not_none_validator
+        yield decimal_validator
+        yield number_size_validator
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: Decimal) -> Decimal:
+        digit_tuple, exponent = value.as_tuple()[1:]
+        if exponent in {'F', 'n', 'N'}:
+            raise ValueError(f'value is not a valid decimal, got {value}')
+
+        if exponent >= 0:
+            # A positive exponent adds that many trailing zeros.
+            digits = len(digit_tuple) + exponent
+            decimals = 0
+        else:
+            # If the absolute value of the negative exponent is larger than the
+            # number of digits, then it's the same as the number of digits,
+            # because it'll consume all of the digits in digit_tuple and then
+            # add abs(exponent) - len(digit_tuple) leading zeros after the
+            # decimal point.
+            if abs(exponent) > len(digit_tuple):
+                digits = decimals = abs(exponent)
+            else:
+                digits = len(digit_tuple)
+                decimals = abs(exponent)
+        whole_digits = digits - decimals
+
+        if cls.max_digits is not None and digits > cls.max_digits:
+            raise ValueError(f'ensure that there are no more than {cls.max_digits} digits in total')
+
+        if cls.decimal_places is not None and decimals > cls.decimal_places:
+            raise ValueError(f'ensure that there are no more than {cls.decimal_places} decimal places')
+
+        if cls.max_digits is not None and cls.decimal_places is not None:
+            expected = cls.max_digits - cls.decimal_places
+            if whole_digits > expected:
+                raise ValueError(f'ensure that there are no more than {expected} digits before the decimal point')
+
+        return value
+
+
+def condecimal(*, gt=None, lt=None, max_digits=None, decimal_places=None) -> Type[Decimal]:
+    # use kwargs then define conf in a dict to aid with IDE type hinting
+    namespace = dict(
+        gt=gt,
+        lt=lt,
+        max_digits=max_digits,
+        decimal_places=decimal_places
+    )
+    return type('ConstrainedDecimalValue', (ConstrainedDecimal,), namespace)
 
 
 class UUID1(UUID):
