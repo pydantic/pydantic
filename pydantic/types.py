03517e4e07086b2906fb566f3ffd964af08067c0
Samuel Colvin <samcolvin@gmail.com>
2018-07-07 16:58:58 +0100

    fix FilePath and DirectoryPath (#226)

--- pydantic/types.py
+++ pydantic/types.py
@@ -329,6 +329,7 @@ class FilePath(Path):
         yield path_exists_validator
         yield cls.validate
 
+    @classmethod
     def validate(cls, value: Path) -> Path:
         if not value.is_file():
             raise errors.PathNotAFileError(path=value)
@@ -343,6 +344,7 @@ class DirectoryPath(Path):
         yield path_exists_validator
         yield cls.validate
 
+    @classmethod
     def validate(cls, value: Path) -> Path:
         if not value.is_dir():
             raise errors.PathNotADirectoryError(path=value)
