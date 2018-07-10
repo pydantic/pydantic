0e570e0ef16e04ec884972c462f45817c48fa4aa
Samuel Colvin <s@muelcolvin.com>
2018-07-10 17:27:35 +0100

    rename _create_sub_type

--- pydantic/fields.py
+++ pydantic/fields.py
@@ -171,13 +171,13 @@ class Field:
                     self.allow_none = True
                 else:
                     types_.append(type_)
-            self.sub_fields = [self.create_sub_type(t, f'{self.name}_{display_as_type(t)}') for t in types_]
+            self.sub_fields = [self._create_sub_type(t, f'{self.name}_{display_as_type(t)}') for t in types_]
             return
 
         if issubclass(origin, Tuple):
             self.shape = Shape.TUPLE
             self.sub_fields = [
-                self.create_sub_type(t, f'{self.name}_{i}') for i, t in enumerate(self.type_.__args__)
+                self._create_sub_type(t, f'{self.name}_{i}') for i, t in enumerate(self.type_.__args__)
             ]
             return
 
@@ -189,15 +189,15 @@ class Field:
             self.shape = Shape.SET
         else:
             assert issubclass(origin, Mapping)
-            self.key_field = self.create_sub_type(self.type_.__args__[0], 'key_' + self.name)
+            self.key_field = self._create_sub_type(self.type_.__args__[0], 'key_' + self.name)
             self.type_ = self.type_.__args__[1]
             self.shape = Shape.MAPPING
 
         if _get_type_origin(self.type_):
             # type_ has been refined eg. as the type of a List and sub_fields needs to be populated
-            self.sub_fields = [self.create_sub_type(self.type_, '_' + self.name)]
+            self.sub_fields = [self._create_sub_type(self.type_, '_' + self.name)]
 
-    def create_sub_type(self, type_, name):
+    def _create_sub_type(self, type_, name):
         return self.__class__(
             type_=type_,
             name=name,
