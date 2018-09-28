8f03925060f033b29445257ffd8d02dbea0025b6
Samuel Colvin <s@muelcolvin.com>
2018-09-28 14:08:29 +0100

    reimplement dataclasses

--- pydantic/dataclasses.py
+++ pydantic/dataclasses.py
@@ -1,41 +1,55 @@
-from dataclasses import dataclass as dc
-from .main import validate_model, create_model
+import dataclasses
+
+from pydantic import ValidationError
+
+from .main import create_model, validate_model
 
 
-def pydantic_model(cls):
-    fields = {name: (field.type, field.default)
-              for name, field
-              in cls.__dataclass_fields__.items()}
-    model = create_model('', **fields)
-    return model
+def post_init(self):
+    d = validate_model(self.__pydantic_model__, self.__dict__)
+    object.__setattr__(self, '__dict__', d)
+    object.__setattr__(self, '__initialised__', True)
+    if self.__post_init_original__:
+        self.__post_init_original__()
 
 
-def dataclass(_cls=None, *, init=True, repr=True, eq=True,
-              order=False, unsafe_hash=False, frozen=False):
+def setattr_validate_assignment(self, name, value):
+    if self.__initialised__:
+        d = dict(self.__dict__)
+        d.pop(name)
+        value, error_ = self.__pydantic_model__.__fields__[name].validate(value, d, loc=name)
+        if error_:
+            raise ValidationError([error_])
+
+    object.__setattr__(self, name, value)
+
+
+def _process_class(_cls, init, repr, eq, order, unsafe_hash, frozen, validate_assignment):
+    post_init_original = getattr(_cls, '__post_init__', None)
+    _cls.__post_init__ = post_init
+    cls = dataclasses._process_class(_cls, init, repr, eq, order, unsafe_hash, frozen)
+
+    fields = {name: (field.type, field.default) for name, field in cls.__dataclass_fields__.items()}
+    cls.__post_init_original__ = post_init_original
+    cls.__pydantic_model__ = create_model(cls.__name__, **fields)
+    cls.__initialised__ = False
+
+    if validate_assignment and not frozen:
+        cls.__setattr__ = setattr_validate_assignment
+    return cls
+
+
+def dataclass(_cls=None, *, init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False,
+              validate_assignment=True):
+    """
+    Like the python standard lib dataclasses but with type validation.
+
+    Arguments are the same as for standard dataclasses, except for validate_assignment which has the same meaning
+    as Config.validate_assignment
+    """
+
     def wrap(cls):
-        post_init_orig = getattr(cls, '__post_init__', lambda self: None)
-        cls.__post_init__ = post_init_orig
-        cls = dc(init=init, repr=repr, eq=eq, order=order,
-                 unsafe_hash=unsafe_hash, frozen=frozen)(cls)
-        model = pydantic_model(cls)
-
-        def set_attr(self, name, value):
-            super(cls, self).__setattr__(name, value)
-            if getattr(self, '__initialized', False):
-                validate_model(model, self.__dict__)
-
-        def post_init(self):
-            post_init_orig(self)
-            validate_model(model, self.__dict__)
-            if not frozen:
-                self.__initialized = True
-
-        if not frozen:
-            cls.__setattr__ = set_attr
-
-        cls.__post_init__ = post_init
-
-        return cls
+        return _process_class(cls, init, repr, eq, order, unsafe_hash, frozen, validate_assignment)
 
     if _cls is None:
         return wrap
