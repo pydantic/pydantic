6491fd4a8717bd883785005bd20d63cd74198e2a
Samuel Colvin <s@muelcolvin.com>
2018-09-28 14:08:29 +0100

    reimplement dataclasses

--- tests/test_dataclasses.py
+++ tests/test_dataclasses.py
@@ -1,32 +1,88 @@
 import pytest
-from dataclasses import dataclass
 
 import pydantic
 from pydantic import ValidationError
 
 
-@dataclass
-class ModelDataclass:
-    i: int
-    f: float
+def test_simple():
+    @pydantic.dataclasses.dataclass
+    class MyDataclass:
+        a: int
+        b: float
+
+    d = MyDataclass('1', '2.5')
+    assert d.a == 1
+    assert d.b == 2.5
+    d = MyDataclass(b=10, a=20)
+    assert d.a == 20
+    assert d.b == 10
 
 
-@pydantic.dataclasses.dataclass
-class ModelPydanticDataclass:
-    i: int
-    f: float
+def test_value_error():
+    @pydantic.dataclasses.dataclass
+    class MyDataclass:
+        a: int
+        b: int
+
+    with pytest.raises(ValidationError) as exc_info:
+        MyDataclass(1, 'wrong')
+
+    assert exc_info.value.errors() == [
+        {
+            'loc': ('b',),
+            'msg': 'value is not a valid integer',
+            'type': 'type_error.integer',
+        },
+    ]
 
 
-def test_dataclasses():
-    instance_dataclass = ModelDataclass(1, 2)
-    instance_pydantic_dataclass = ModelPydanticDataclass(1, 2)
-    assert instance_dataclass.i == 1
-    assert instance_pydantic_dataclass.i == 1
-    instance_pydantic_dataclass.i = 0
-    assert instance_pydantic_dataclass.i == 0
+def test_frozen():
+    @pydantic.dataclasses.dataclass(frozen=True)
+    class MyDataclass:
+        a: int
 
-    with pytest.raises(ValidationError):
-        ModelPydanticDataclass(1, '')
+    d = MyDataclass(1)
+    assert d.a == 1
 
-    with pytest.raises(ValidationError):
-        instance_pydantic_dataclass.f = ''
+    with pytest.raises(AttributeError):
+        d.a = 7
+
+
+def test_validate_assignment():
+    @pydantic.dataclasses.dataclass
+    class MyDataclass:
+        a: int
+
+    d = MyDataclass(1)
+    assert d.a == 1
+
+    d.a = '7'
+    assert d.a == 7
+
+
+def test_not_validate_assignment():
+    @pydantic.dataclasses.dataclass(validate_assignment=False)
+    class MyDataclass:
+        a: int
+
+    d = MyDataclass(1)
+    assert d.a == 1
+
+    d.a = '7'
+    assert d.a == '7'
+
+
+def test_post_init():
+    post_init_called = False
+
+    @pydantic.dataclasses.dataclass
+    class MyDataclass:
+        a: int
+
+        def __post_init__(self):
+            nonlocal post_init_called
+            post_init_called = True
+
+    d = MyDataclass('1')
+    assert d.a == 1
+    assert post_init_called
