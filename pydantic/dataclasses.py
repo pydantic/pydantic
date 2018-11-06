177e72ae7ad445140f953b09bc21e399ba39c42d
pydevd <d.gaidukov@scalr.com>
2018-11-06 18:18:09 +0200

    json support

--- pydantic/dataclasses.py
+++ pydantic/dataclasses.py
@@ -1,8 +1,11 @@
+import json
+import typing as tp
 import dataclasses
 
 from pydantic import ValidationError
 
-from .main import create_model, validate_model
+from .json import pydantic_encoder
+from .main import create_model, validate_model, BaseConfig
 
 
 def post_init(self):
@@ -31,9 +34,15 @@ def _process_class(_cls, init, repr, eq, order, unsafe_hash, frozen, validate_as
 
     fields = {name: (field.type, field.default) for name, field in cls.__dataclass_fields__.items()}
     cls.__post_init_original__ = post_init_original
-    cls.__pydantic_model__ = create_model(cls.__name__, **fields)
-    cls.__initialised__ = False
 
+    class DCConfig(BaseConfig):
+        arbitrary_types_allowed = True
+
+    cls.__pydantic_model__ = create_model(cls.__name__, __config__=DCConfig, **fields)
+    cls.__initialised__ = False
+    cls._json_encoder = staticmethod(pydantic_encoder)
+    cls.dict = _dict
+    cls.json = _json
     if validate_assignment and not frozen:
         cls.__setattr__ = setattr_validate_assignment
     return cls
@@ -55,3 +64,28 @@ def dataclass(_cls=None, *, init=True, repr=True, eq=True, order=False, unsafe_h
         return wrap
 
     return wrap(_cls)
+
+
+def _dict(self, *, include: tp.Set[str] = None, exclude: tp.Set[str] = None) -> tp.Dict[str, tp.Any]:
+    """
+    Generate a dictionary representation of the model, optionally specifying which fields to include or exclude.
+    """
+    exclude = exclude or set()
+    return {
+        k: v
+        for k, v in dataclasses.asdict(self).items()
+        if k not in exclude and (not include or k in include)
+    }
+
+
+def _json(self, *, include: tp.Set[str] = None, exclude: tp.Set[str] = None, encoder=None, **dumps_kwargs) -> str:
+    """
+    Generate a JSON representation of the model, `include` and `exclude` arguments as per `dict()`.
+
+    `encoder` is an optional function to supply as `default` to json.dumps(), other arguments as per `json.dumps()`.
+    """
+    exclude = exclude or set()
+    return json.dumps(
+        self.dict(include=include, exclude=exclude),
+        default=encoder or self._json_encoder, **dumps_kwargs
+    )
