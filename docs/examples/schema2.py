0671f6a5b1ce3a95c109e6584a006418e8529bd1
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-18 19:09:42 +0400

    Add schema examples for top-level schema with multiple models

---
+++ docs/examples/schema2.py
@@ -0,0 +1,26 @@
+import json
+from pydantic import BaseModel
+from pydantic.schema import schema
+
+
+class Foo(BaseModel):
+    a: str = None
+
+
+class Model(BaseModel):
+    b: Foo
+
+
+class Bar(BaseModel):
+    c: int
+
+
+top_level_schema = schema([Model, Bar], title='My Schema')
+print(json.dumps(top_level_schema, indent=2))
+
+# {
+#   "title": "My Schema",
+#   "definitions": {
+#     "Foo": {
+#       "title": "Foo",
+#       ...
