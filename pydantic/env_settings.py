19320bf77540c79d10d5a3a81faa3051361f915a
Samuel Colvin <samcolvin@gmail.com>
2018-12-29 12:31:53 +0000

    improve settings priority,  (#343)
    * improve settings priority, ref #341
    
    * add HISTORY.rst
    
    * move is_complex and rename _build_environ
    
    * tweak

--- pydantic/env_settings.py
+++ pydantic/env_settings.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-from .fields import Shape
 from .main import BaseModel
 
 
@@ -9,43 +8,35 @@ class SettingsError(ValueError):
     pass
 
 
-def _complex_field(field):
-    try:
-        return field and (field.shape != Shape.SINGLETON or issubclass(field.type_, (BaseModel, list, set, dict)))
-    except TypeError:
-        # if field.type_ is not a class
-        return False
-
-
 class BaseSettings(BaseModel):
     """
     Base class for settings, allowing values to be overridden by environment variables.
 
-    Environment variables must be upper case and prefixed by APP_ by default. Eg. to override foobar,
-    `export APP_FOOBAR="whatever"`. To change this behaviour set Config options case_insensitive and
-    env_prefix.
+    By default environment variables must be upper case and prefixed by APP_ by default. Eg. to override foobar,
+    `export APP_FOOBAR="whatever"`. To change this behaviour set Config options case_insensitive and env_prefix.
 
-    This is useful in production for secrets you do not wish to save in code, it places nicely with docker(-compose),
+    This is useful in production for secrets you do not wish to save in code, it plays nicely with docker(-compose),
     Heroku and any 12 factor app design.
     """
 
     def __init__(self, **values):
-        values = {**self._substitute_environ(), **values}
-        super().__init__(**values)
+        super().__init__(**self._build_values(values))
 
-    def _substitute_environ(self):
+    def _build_values(self, init_kwargs):
+        return {**self._build_environ(), **init_kwargs}
+
+    def _build_environ(self):
         """
-        Substitute environment variables into values.
+        Build environment variables suitable for passing to the Model.
         """
         d = {}
 
         if self.__config__.case_insensitive:
-            env_vars = {k.lower(): v for (k, v) in os.environ.items()}
+            env_vars = {k.lower(): v for k, v in os.environ.items()}
         else:
             env_vars = os.environ
 
         for field in self.__fields__.values():
-
             if field.has_alias:
                 env_name = field.alias
             else:
@@ -55,7 +46,7 @@ class BaseSettings(BaseModel):
             env_val = env_vars.get(env_name_, None)
 
             if env_val:
-                if _complex_field(field):
+                if field.is_complex():
                     try:
                         env_val = json.loads(env_val)
                     except ValueError as e:
