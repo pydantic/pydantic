e22bbb986268d5b97cfdecb22a266b742926654e
Samuel Colvin <s@muelcolvin.com>
2019-03-18 11:33:31 +0000

    correct imports, fix #420

--- pydantic/__init__.py
+++ pydantic/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+from . import dataclasses
 from .class_validators import validator
 from .env_settings import BaseSettings
 from .error_wrappers import ValidationError
@@ -6,7 +7,6 @@ from .errors import *
 from .fields import Required
 from .main import BaseConfig, BaseModel, Extra, create_model, validate_model
 from .parse import Protocol
+from .schema import Schema
 from .types import *
 from .version import VERSION
-from . import dataclasses
-from .schema import Schema
