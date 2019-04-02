ba895062037c03b916e54d05afa8824448fba3a8
tigerwings <tigerwings@gmail.com>
2019-04-02 11:54:57 -0500

    restore the original example for ForwardRef

--- docs/examples/forward_ref.py
+++ docs/examples/forward_ref.py
@@ -4,14 +4,12 @@ from pydantic import BaseModel
 Foo = ForwardRef('Foo')
 
 class Foo(BaseModel):
-    a: int = None
-    b: Dict[str, FooType] = {}
-    c: List[Foo] = []
-
+    a: int = 123
+    b: Foo = None
 
 Foo.update_forward_refs()
 
 print(Foo())
-#> Foo a=123 b={} c=[]
-print(Foo(b={'bar': {'a': '321'}}, c=[{'a': 345}]))
-#> Foo a=123 b={'bar': <Foo a=321 b={} c=[]>} c=[<Foo a=345 b={} c=[]>]
+#> Foo a=123 b=None
+print(Foo(b={'a': '321'}))
+#> Foo a=123 b=<Foo a=321 b=None>
