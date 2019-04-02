0f3a16a2dddbcff1b1784e72aaa80de0272da00b
tigerwings <tigerwings@gmail.com>
2019-04-01 22:36:20 -0500

    fix ForwardRef collection bug

--- docs/examples/forward_ref.py
+++ docs/examples/forward_ref.py
@@ -4,12 +4,15 @@ from pydantic import BaseModel
 Foo = ForwardRef('Foo')
 
 class Foo(BaseModel):
-    a: int = 123
-    b: Foo = None
+    a: int = None
+    b: Dict[str, FooType] = {}
+    c: List[Foo] = []
+
 
 Foo.update_forward_refs()
 
 print(Foo())
-#> Foo a=123 b=None
-print(Foo(b={'a': '321'}))
+#> Foo a=123 b={} c=[]
+print(Foo(b={'bar': {'a': '321'}}, c=[{'a': 345}]))
+#> Foo a=123 b={'bar': <Foo a=321 b={} c=[]>} c=[<Foo a=345 b={} c=[]>]
 #> Foo a=123 b=<Foo a=321 b=None>
