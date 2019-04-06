4e6eefef92f958bf6e06bad0962828664fb09499
Lasse Gravesen <lasse.gravesen@falcon.io>
2019-04-06 11:22:33 +0200

    feat: adjust documentation for secretstr.

--- docs/examples/ex_secret_types.py
+++ docs/examples/ex_secret_types.py
@@ -6,13 +6,27 @@ class SimpleModel(BaseModel):
     password: SecretStr
     password_bytes: SecretBytes
 
-print(SimpleModel(password='IAmSensitive', password_bytes=b'IAmSensitiveBytes'))
+sm1 = SimpleModel(password='IAmSensitive', password_bytes=b'IAmSensitiveBytes')
+print(sm)
 # > SimpleModel password=SecretStr('**********') password_bytes=SecretBytes(b'**********')
+print(sm.password)
+# > **********
+print(repr(sm.password))
+# > SecretStr('**********')
+print(sm.password_bytes)
+# > **********
+print(repr(sm.password_bytes))
+# > SecretStr(b'**********')
+print(sm.dict())
+# > {'password': SecretStr('**********'), 'password_bytes': SecretBytes(b'**********')}
+print(sm.json())
+# > {"password": "**********", "password_bytes": "**********"}
 
-sm = SimpleModel(password='IAmSensitive', password_bytes=b'IAmSensitiveBytes')
-print(sm.password.get_secret_value())
+
+sm2 = SimpleModel(password='IAmSensitive', password_bytes=b'IAmSensitiveBytes')
+print(sm2.password.get_secret_value())
 # > IAmSensitive
-print(sm.password_bytes.get_secret_value())
+print(sm2.password_bytes.get_secret_value())
 # > b'IAmSensitiveBytes'
 
 
