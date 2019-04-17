a94e5225ddb32a0814fc02b87fb33a7c7301b4c6
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2019-04-17 05:42:52 +0200

    Update test_types_hypothesis.py

--- tests/test_types_hypothesis.py
+++ tests/test_types_hypothesis.py
@@ -1,9 +1,18 @@
 from hypothesis import given, settings
 from hypothesis import strategies as st
-
+import pytest
 from pydantic.types import EmailStr
 
 
+def _is_email_validator_installed():
+    try:
+        import email_validator
+        return True
+    except ModuleNotFoundError:
+        return False
+
+
+@pytest.mark.skipif(not _is_email_validator_installed(), reason='just fow now.')
 @given(t=st.just(EmailStr()), email=st.emails())
 @settings(max_examples=10000000)
 def test_email(t, email):
