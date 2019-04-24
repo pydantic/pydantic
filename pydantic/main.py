80919d94067093920c6aff08edafc2f30afde58a
Jaewon Kim <jakeb@ab180.co>
2019-04-24 21:11:52 +0900

    Fix dict with extra keys (#489)

--- pydantic/main.py
+++ pydantic/main.py
@@ -289,7 +289,7 @@ class BaseModel(metaclass=MetaModel):
 
     def _get_key_factory(self, by_alias: bool) -> Callable[..., str]:
         if by_alias:
-            return lambda fields, key: fields[key].alias
+            return lambda fields, key: fields[key].alias if key in fields else key
 
         return lambda _, key: key
 
