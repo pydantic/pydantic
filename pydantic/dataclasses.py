8b981d7ffd8d06ca64575264d1852611b9e1a7b7
sevaho <sebastiaan@sevaho.io>
2019-05-31 11:58:28 +0200

    added feature post_init_post_parse

--- pydantic/dataclasses.py
+++ pydantic/dataclasses.py
@@ -30,7 +30,8 @@ def _pydantic_post_init(self: 'DataclassType') -> None:
     d = validate_model(self.__pydantic_model__, self.__dict__, cls=self.__class__)[0]
     object.__setattr__(self, '__dict__', d)
     object.__setattr__(self, '__initialised__', True)
-
+    if hasattr(self, '__post_init_post_parse__'):
+        self.__post_init_original__()
 
 def _validate_dataclass(cls: Type['DataclassType'], v: Any) -> 'DataclassType':
     if isinstance(v, cls):
