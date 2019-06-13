0a010c3e9267fd370183d363847db8332df76d13
Paul Frederiks <p.frederiks@developers.nl>
2019-06-13 15:06:09 +0200

    Annotate *initvars as Any

--- pydantic/dataclasses.py
+++ pydantic/dataclasses.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:  # pragma: no cover
             pass
 
 
-def _pydantic_post_init(self: 'DataclassType', *initvars) -> None:
+def _pydantic_post_init(self: 'DataclassType', *initvars: Any) -> None:
     if self.__post_init_original__:
         self.__post_init_original__(*initvars)
     d = validate_model(self.__pydantic_model__, self.__dict__, cls=self.__class__)[0]
