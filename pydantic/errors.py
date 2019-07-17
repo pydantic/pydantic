151aabec76dcbed2883cffabe7a2b6ebb9c3841f
David Montague <david@exactrics.com>
2019-07-17 12:27:44 -0700

    Update error message for enum validator

--- pydantic/errors.py
+++ pydantic/errors.py
@@ -86,7 +86,9 @@ class UrlRegexError(PydanticValueError):
 
 
 class EnumError(PydanticTypeError):
-    msg_template = 'value is not a valid enumeration member'
+    def __str__(self) -> str:
+        permitted = ', '.join(repr(v.value) for v in self.ctx['enum_type'])  # type: ignore
+        return f'value is not a valid enumeration member; permitted: {permitted}'
 
 
 class IntegerError(PydanticTypeError):
--- pydantic/validators.py
+++ pydantic/validators.py
@@ -219,9 +219,10 @@ def set_validator(v: Any) -> Set[Any]:
 
 
 def enum_validator(v: Any, field: 'Field', config: 'BaseConfig') -> Enum:
-    with change_exception(errors.EnumError, ValueError):
+    try:
         enum_v = field.type_(v)
-
+    except ValueError:
+        raise errors.EnumError(enum_type=field.type_)
     return enum_v.value if config.use_enum_values else enum_v
 
 
