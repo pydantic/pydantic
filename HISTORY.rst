74768c153534bb7204079cb1112801cf07d325db
Arseny Boykov <36469655+MrMrRobat@users.noreply.github.com>
2019-07-24 11:58:38 +0300

    Add advanced exclude and include support for dict, json and copy (#648)
    * Add advanced exclude support for dict, json and copy
    
    * Add advanced exclude support for dict, json and copy
    
    Add new version section (v0.31)
    
    * Add advanced include support, add more tests, improve code style
    Rename ValueExclude to ValueItems and move it to utils
    Use old logic to calculate keys, but still exclude it in _iter
    
    * Add more tests for ValueItems
    
    * Removed update arg check in _calculate_keys for return None
    This will increase speed when no include or exclude given and skip_defaults is False
    
    * Fix formatting, remove duplicate imports
    
    * Add # pragma: no cover to 'if TYPE_CHECKING:' block
    
    * tweaks and coverage
    
    * fix history
    
    * Add docs
    
    * tweak docs

--- HISTORY.rst
+++ HISTORY.rst
@@ -7,6 +7,7 @@ v0.31 (unreleased)
 ..................
 * better support for floating point `multiple_of` values, #652 by @justindujardin
 * fix schema generation for ``NewType`` and ``Literal``, #649 by @dmontagu
+* add advanced exclude support for ``dict``, ``json`` and ``copy``, #648 by @MrMrRobat
 * add documentation for Literal type, #651 by @dmontagu
 
 v0.30.1 (2019-07-15)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,7 @@ from typing import NewType, Union
 import pytest
 
 from pydantic.utils import (
+    ValueItems,
     display_as_type,
     import_string,
     is_new_type,
@@ -159,6 +160,50 @@ def test_lenient_issubclass_is_lenient():
 
 def test_truncate_type():
     assert truncate(object) == "<class 'object'>"
+
+
+def test_value_items():
+    v = ['a', 'b', 'c']
+    vi = ValueItems(v, {0, -1})
+    assert vi.is_excluded(2)
+    assert [v_ for i, v_ in enumerate(v) if not vi.is_excluded(i)] == ['b']
+
+    assert vi.is_included(2)
+    assert [v_ for i, v_ in enumerate(v) if vi.is_included(i)] == ['a', 'c']
+
+    v2 = {'a': v, 'b': {'a': 1, 'b': (1, 2)}, 'c': 1}
+
+    vi = ValueItems(v2, {'a': {0, -1}, 'b': {'a': ..., 'b': -1}})
+
+    assert not vi.is_excluded('a')
+    assert vi.is_included('a')
+    assert not vi.is_excluded('c')
+    assert not vi.is_included('c')
+
+    assert str(vi) == "ValueItems: dict({'a': {0, -1}, 'b': {'a': Ellipsis, 'b': -1}})"
+
+    excluded = {k_: v_ for k_, v_ in v2.items() if not vi.is_excluded(k_)}
+    assert excluded == {'a': v, 'b': {'a': 1, 'b': (1, 2)}, 'c': 1}
+
+    included = {k_: v_ for k_, v_ in v2.items() if vi.is_included(k_)}
+    assert included == {'a': v, 'b': {'a': 1, 'b': (1, 2)}}
+
+    sub_v = included['a']
+    sub_vi = ValueItems(sub_v, vi.for_element('a'))
+    assert str(sub_vi) == 'ValueItems: set({0, 2})'
+
+    assert sub_vi.is_excluded(2)
+    assert [v_ for i, v_ in enumerate(sub_v) if not sub_vi.is_excluded(i)] == ['b']
+
+    assert sub_vi.is_included(2)
+    assert [v_ for i, v_ in enumerate(sub_v) if sub_vi.is_included(i)] == ['a', 'c']
+
+
+def test_value_items_error():
+    with pytest.raises(TypeError) as e:
+        ValueItems(1, (1, 2, 3))
+
+    assert str(e.value) == "Unexpected type of exclude value <class 'tuple'>"
 
 
 def test_is_new_type():
