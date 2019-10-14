1b124e31f2f5ea6db434276f7cadf4e11946bc3a
Steve Simmons <stevesimmons@users.noreply.github.com>
2019-10-14 08:45:33 +0100

    Update datetime_parse.py

--- pydantic/datetime_parse.py
+++ pydantic/datetime_parse.py
@@ -20,7 +20,7 @@ from typing import Dict, Union
 
 from . import errors
 
-date_re = re.compile(r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})$')
+date_re = re.compile(r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})([ T]00:00:00)?$')
 
 time_re = re.compile(
     r'(?P<hour>\d{1,2}):(?P<minute>\d{1,2})' r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
