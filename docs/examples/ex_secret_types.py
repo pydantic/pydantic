d7f9224eedc8f97c52a6750ce64daa7d32772b19
Ashton Shears <ashton.shears@teradata.com>
2019-10-14 09:20:36 -0700

    add address review comments

--- docs/examples/ex_secret_types.py
+++ docs/examples/ex_secret_types.py
@@ -10,17 +10,17 @@ sm = SimpleModel(password='IAmSensitive', password_bytes=b'IAmSensitiveBytes')
 
 # Standard access methods will not display the secret
 print(sm)
-# > SimpleModel password=SecretStr('**********') password_bytes=SecretBytes(b'**********')
-print(sm.password.display())
-# > '**********'
+#> SimpleModel password=SecretStr('**********') password_bytes=SecretBytes(b'**********')
+print(str(sm.password.display))
+#> '**********'
 print(sm.json())
-# > '{"password": "**********", "password_bytes": "**********"}'
+#> '{"password": "**********", "password_bytes": "**********"}'
 
 # Use get_secret_value method to see the secret's content.
 print(sm.password.get_secret_value())
-# > IAmSensitive
+#> IAmSensitive
 print(sm.password_bytes.get_secret_value())
-# > b'IAmSensitiveBytes'
+#> b'IAmSensitiveBytes'
 
 
 try:
