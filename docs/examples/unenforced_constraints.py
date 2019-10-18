9a8a712c8616024530997bab8966bae6cc1402c5
Samuel Colvin <s@muelcolvin.com>
2019-10-18 16:59:09 +0100

    update docs as per @tiangolo suggestions

--- docs/examples/unenforced_constraints.py
+++ docs/examples/unenforced_constraints.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, PositiveInt
 
 try:
-    # this won't works since PositiveInt takes precedence over the
+    # this won't work since PositiveInt takes precedence over the
     # constraints defined in Field meaning they're ignored
     class Model(BaseModel):
         foo: PositiveInt = Field(..., lt=10)
@@ -12,5 +12,15 @@ except ValueError as e:
 # (Note: here exclusiveMaximum will not be enforce)
 class Model(BaseModel):
     foo: PositiveInt = Field(..., exclusiveMaximum=10)
+
+print(Model.schema())
+
+# if you find yourself needing this, an alternative is to declare
+# the constraints in Field (or you could use conint())
+# here both constraints will be enforced:
+class Model(BaseModel):
+    # Here both constraints will be applied and the schema
+    # will be generated correctly
+    foo: int = Field(..., gt=0, lt=10)
 
 print(Model.schema())
