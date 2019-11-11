bce9d14133b2f2990cb7845e44da7c0a4960215a
David Montague <david@exactrics.com>
2019-11-11 00:38:46 -0800

    Fix docs example

--- docs/examples/parse_as_type.py
+++ docs/examples/parse_as_type.py
@@ -7,8 +7,7 @@ class Item(BaseModel):
     name: str
 
 # `item_data` could come from an API call, eg., via something like:
-# >>> import requests
-# >>> item_data = requests.get('https://my-api.com/items').json()
+# item_data = requests.get('https://my-api.com/items').json()
 item_data = [{'id': 1, 'name': 'My Item'}]
 
 items = parse_as_type(item_data, List[Item])
