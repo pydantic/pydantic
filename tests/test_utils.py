e2edb95d6615f69263af141a1d36cd4e0710b735
Samuel Colvin <s@muelcolvin.com>
2019-11-28 18:28:51 +0000

    coverage for UndefinedType

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -276,3 +276,7 @@ def test_replace_code():
     assert f.__code__.co_nlocals == 1
     assert f.__code__.co_argcount == 1
     assert f(1) == 1
+
+
+def test_undefined_repr():
+    assert repr(Undefined) == 'PydanticUndefined'
