9255012c083f39732fc539d1a509b5dca9d8f886
Samuel Colvin <s@muelcolvin.com>
2019-12-04 15:14:53 +0000

    add version_info to make creating issues easier (#1071)
    * add version_info to make creating issues easier
    
    * improve version_info()
    
    * improve contributing docs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 import string
 from enum import Enum
 from typing import NewType, Union
@@ -10,7 +11,7 @@ from pydantic.color import Color
 from pydantic.dataclasses import dataclass
 from pydantic.fields import Undefined
 from pydantic.typing import display_as_type, is_new_type, new_type_supertype
-from pydantic.utils import ValueItems, deep_update, get_model, import_string, lenient_issubclass, truncate
+from pydantic.utils import ValueItems, deep_update, get_model, import_string, lenient_issubclass, truncate, version_info
 
 try:
     import devtools
@@ -285,3 +286,9 @@ def test_get_model():
 
     with pytest.raises(TypeError):
         get_model(C)
+
+
+def test_version_info():
+    s = version_info()
+    assert re.match(' *pydantic version: ', s)
+    assert s.count('\n') == 5
