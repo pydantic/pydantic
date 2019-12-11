de6bd1b4694dc7b04c9626e39fd494965f230b7c
Samuel Colvin <s@muelcolvin.com>
2019-12-11 11:58:03 +0000

    Settings config inheritance fix #1091 (#1093)

--- pydantic/fields.py
+++ pydantic/fields.py
@@ -280,6 +280,7 @@ class ModelField(Representation):
     def set_config(self, config: Type['BaseConfig']) -> None:
         self.model_config = config
         info_from_config = config.get_field_info(self.name)
+        config.prepare_field(self)
         if info_from_config:
             self.field_info.alias = info_from_config.get('alias') or self.field_info.alias or self.name
             self.alias = cast(str, self.field_info.alias)
