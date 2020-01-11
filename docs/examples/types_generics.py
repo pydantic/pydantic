1aef357865a80e6113d506dfa1e56974eaec9b30
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-11 15:47:12 +0100

    :memo: Update example for generics, try to simplify and explain better

--- docs/examples/types_generics.py
+++ docs/examples/types_generics.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel, ValidationError
 from pydantic.fields import ModelField
 from typing import TypeVar, Generic
 
-OldType = TypeVar('OldType')
-GradeType = TypeVar('GradeType')
+AgedType = TypeVar('AgedType')
+QualityType = TypeVar('QualityType')
 
 # This is not a pydantic model, it's an arbitrary generic class
-class Assessment(Generic[OldType, GradeType]):
-    def __init__(self, name: str, old: OldType, grade: GradeType):
+class TastingModel(Generic[AgedType, QualityType]):
+    def __init__(self, name: str, aged: AgedType, quality: QualityType):
         self.name = name
-        self.old = old
-        self.grade = grade
+        self.aged = aged
+        self.quality = quality
 
     @classmethod
     def __get_validators__(cls):
@@ -21,21 +21,21 @@ class Assessment(Generic[OldType, GradeType]):
     # editor give you completion and catch errors
     def validate(cls, v, field: ModelField):
         if not isinstance(v, cls):
-            # The value is not even an Assessment
-            raise ValueError('Invalid value')
+            # The value is not even a TastingModel
+            raise TypeError('Invalid value')
         if not field.sub_fields:
             # Generic parameters were not provided so we don't try to validate
             # them and just return the value as is
             return v
-        sub_field_old = field.sub_fields[0]
-        sub_field_grade = field.sub_fields[1]
+        aged_f = field.sub_fields[0]
+        quality_f = field.sub_fields[1]
         errors = []
         # Here we don't need the validated value, but we want the errors
-        value_old, error = sub_field_old.validate(v.old, {}, loc='old')
+        valid_value, error = aged_f.validate(v.aged, {}, loc='aged')
         if error:
             errors.append(error)
         # Here we don't need the validated value, but we want the errors
-        value_grade, error = sub_field_grade.validate(v.grade, {}, loc='grade')
+        valid_value, error = quality_f.validate(v.quality, {}, loc='quality')
         if error:
             errors.append(error)
         if errors:
@@ -44,38 +44,37 @@ class Assessment(Generic[OldType, GradeType]):
         return v
 
 class Model(BaseModel):
-    student: Assessment[int, float]  # old is an int, grade a float
-    cheese: Assessment[bool, str]  # old is a bool, grade a str
-    thing: Assessment  # old is Any, grade is Any
+    # for wine, "aged" is an int with years, "quality" is a float
+    wine: TastingModel[int, float]
+    # for cheese, "aged" is a bool, "quality" is a str
+    cheese: TastingModel[bool, str]
+    # for thing, "aged" is a Any, "quality" is Any
+    thing: TastingModel
 
 model = Model(
-    student=Assessment(name='Harry', old=15, grade=87),
-    cheese=Assessment(name='Gouda', old=True, grade='Good'),
-    thing=Assessment(name='Python', old='Nah', grade='Awesome')
+    # This wine was aged for 20 years and has a quality of 85.6
+    wine=TastingModel(name='Cabernet Sauvignon', aged=20, quality=85.6),
+    # This cheese is aged (is mature) and has "Good" quality
+    cheese=TastingModel(name='Gouda', aged=True, quality='Good'),
+    # This Python thing has aged "Not much" and has a quality "Awesome"
+    thing=TastingModel(name='Python', aged='Not much', quality='Awesome')
     )
 print(model)
-print(model.student)
-print(model.student.name)
-print(model.student.old)
-print(model.student.grade)
-print(model.cheese)
-print(model.cheese.name)
-print(model.cheese.old)
-print(model.cheese.grade)
-print(model.thing)
-print(model.thing.name)
-print(model.thing.old)
-print(model.thing.grade)
+print(model.wine.aged)
+print(model.wine.quality)
+print(model.cheese.aged)
+print(model.cheese.quality)
+print(model.thing.aged)
 try:
     # If the values of the sub-types are invalid, we get an error
     Model(
-        # For student, old should be an int, and grade a float
-        student=Assessment(name='Harry', old=True, grade='Fail'),
-        # For cheese, old should be a bool, and grade a str
-        cheese=Assessment(name='Gouda', old='yeah', grade=5),
+        # For wine, aged should be an int with the years, and quality a float
+        wine=TastingModel(name='Merlot', aged=True, quality='Kinda good'),
+        # For cheese, aged should be a bool, and quality a str
+        cheese=TastingModel(name='Gouda', aged='yeah', quality=5),
         # For thing, no type parameters are declared, and we skipped validation
         # in those cases in the Assessment.validate() function
-        thing=Assessment(name='Python', old='Nah', grade='Awesome')
-    )
+        thing=TastingModel(name='Python', aged='Not much', quality='Awesome')
+)
 except ValidationError as e:
     print(e)
