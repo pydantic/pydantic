e2533e04c924c20d3f7f8c4e8bfd2179bae7c31d
Samuel Colvin <s@muelcolvin.com>
2020-01-17 17:13:55 +0000

    regex for username and password in URLs (#1175)
    * regex for username and password in URLs, fix #1115
    
    * fix linting

--- pydantic/networks.py
+++ pydantic/networks.py
@@ -54,7 +54,7 @@ def url_regex() -> Pattern[str]:
     if _url_regex_cache is None:
         _url_regex_cache = re.compile(
             r'(?:(?P<scheme>[a-z][a-z0-9+\-.]+)://)?'  # scheme https://tools.ietf.org/html/rfc3986#appendix-A
-            r'(?:(?P<user>[^\s:]+)(?::(?P<password>\S*))?@)?'  # user info
+            r'(?:(?P<user>[^\s:/]+)(?::(?P<password>[^\s/]*))?@)?'  # user info
             r'(?:'
             r'(?P<ipv4>(?:\d{1,3}\.){3}\d{1,3})|'  # ipv4
             r'(?P<ipv6>\[[A-F0-9]*:[A-F0-9:]+\])|'  # ipv6
