0b5ccffc76fb47c41088692a91165cdd404c234e
Samuel Colvin <s@muelcolvin.com>
2020-01-17 16:30:21 +0000

    Fix  argument in validators during assignment, fix #1172 (#1174)

--- pydantic/main.py
+++ pydantic/main.py
@@ -284,7 +284,7 @@ class BaseModel(metaclass=ModelMetaclass):
         elif self.__config__.validate_assignment:
             known_field = self.__fields__.get(name, None)
             if known_field:
-                value, error_ = known_field.validate(value, self.dict(exclude={name}), loc=name)
+                value, error_ = known_field.validate(value, self.dict(exclude={name}), loc=name, cls=self.__class__)
                 if error_:
                     raise ValidationError([error_], type(self))
         self.__dict__[name] = value
