09258b502004d38bfb04a07a34ce9789993b4705
Samuel Colvin <s@muelcolvin.com>
2020-02-03 14:12:42 +0000

    python 3.8 in docs

--- docs/examples/validation_decorator_parameter_types.py
+++ docs/examples/validation_decorator_parameter_types.py
@@ -16,12 +16,12 @@ def kw_only(*, a: int, b: int = 2) -> str:
 print(kw_only(a=1))
 print(kw_only(a=1, b=3))
 
-# @validate_arguments
-# def pos_only(a: int, b: int = 2, /) -> str:
-#     return f'a={a} b={b}'
-#
-# print(pos_only(1))
-# print(pos_only(1, 2))
+@validate_arguments
+def pos_only(a: int, b: int = 2, /) -> str:  # python 3.8 only
+    return f'a={a} b={b}'
+
+print(pos_only(1))
+print(pos_only(1, 2))
 
 @validate_arguments
 def var_args(*args: int) -> str:
@@ -38,18 +38,18 @@ def var_kwargs(**kwargs: int) -> str:
 print(var_kwargs(a=1))
 print(var_kwargs(a=1, b=2))
 
-# @validate_arguments
-# def armageddon(
-#     a: int,
-#     /,
-#     b: int,
-#     c: int = None,
-#     *d: int,
-#     e: int,
-#     f: int = None,
-#     **g: int
-# ) -> str:
-#     return f'a={a} b={b} c={c} d={d} e={e} f={f} g={g}'
-#
-# print(armageddon(1, 2, e=3))
-# print(armageddon(1, 2, 3, 4, 5, 6, c=7, e=8, f=9, g=10, spam=11))
+@validate_arguments
+def armageddon(
+    a: int,
+    /,  # python 3.8 only
+    b: int,
+    c: int = None,
+    *d: int,
+    e: int,
+    f: int = None,
+    **g: int
+) -> str:
+    return f'a={a} b={b} c={c} d={d} e={e} f={f} g={g}'
+
+print(armageddon(1, 2, e=3))
+print(armageddon(1, 2, 3, 4, 5, 6, c=7, e=8, f=9, g=10, spam=11))
