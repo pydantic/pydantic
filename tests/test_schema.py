aaec3c9c15581d2de31bcda0fbf096638386ee20
PrettyWood <em.jolibois@gmail.com>
2020-03-26 16:44:11 +0100

    Remove typing-extension dependency on python 3.8 (#1342)
    * Remove typing-extension dependency on python 3.8
    
    * retrigger CI
    
    * retrigger CI bis

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -61,11 +61,6 @@ try:
 except ImportError:
     email_validator = None
 
-try:
-    import typing_extensions
-except ImportError:
-    typing_extensions = None
-
 
 def test_key():
     class ApplePie(BaseModel):
@@ -1459,7 +1454,7 @@ def test_new_type_schema():
     }
 
 
-@pytest.mark.skipif(not typing_extensions, reason='typing_extensions not installed')
+@pytest.mark.skipif(not Literal, reason='typing_extensions not installed and python version < 3.8')
 def test_literal_schema():
     class Model(BaseModel):
         a: Literal[1]
