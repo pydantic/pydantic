0f974d9d01b794d4fae1f0e9e9ac8a5310613a0b
Michael Kuhn <michael.kuhn@uni-tuebingen.de>
2020-04-07 14:14:46 +0200

    docs: add hint for custom root types

---
+++ docs/examples/models_custom_root_access.py
@@ -0,0 +1,15 @@
+from typing import List, Dict
+from pydantic import BaseModel, ValidationError
+
+class Pets(BaseModel):
+    __root__: List[str]
+
+    def __iter__(self):
+        return iter(self.__root__)
+
+    def __getitem__(self, item):
+        return self.__root__[item]
+
+pets = Pets.parse_obj(['dog', 'cat'])
+print(pets[0])
+print([pet for pet in pets])
