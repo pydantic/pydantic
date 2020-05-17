8039add415f0ee86452d1ffec7bb75da1bc35724
Joseph Perez <joperez@hotmail.fr>
2020-05-18 00:14:49 +0200

    Add 'apischema' benchmark

--- benchmarks/run.py
+++ benchmarks/run.py
@@ -51,6 +51,11 @@ try:
 except Exception as e:
     TestVoluptuous = None
 
+try:
+    from test_apischema import TestApischema, TestApischemaOptimized
+except ImportError:
+    TestApischema, TestApischemaOptimized = None, None
+
 PUNCTUATION = ' \t\n!"#$%&\'()*+,-./'
 LETTERS = string.ascii_letters
 UNICODE = '\xa0\xad¡¢£¤¥¦§¨©ª«¬ ®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ'
@@ -60,7 +65,7 @@ random = random.SystemRandom()
 # in order of performance for csv
 other_tests = [
     t for t in
-    [TestCAttrs, TestValideer, TestMarshmallow, TestVoluptuous, TestTrafaret, TestDRF, TestCerberus]
+    [TestCAttrs, TestValideer, TestApischema, TestApischemaOptimized, TestMarshmallow, TestVoluptuous, TestTrafaret, TestDRF, TestCerberus]
     if t is not None
 ]
 
---
+++ benchmarks/test_apischema.py
@@ -0,0 +1,170 @@
+from dataclasses import Field, MISSING, dataclass, field, is_dataclass
+from datetime import datetime
+from typing import Any, Dict, List, NewType, Optional, Type
+
+import apischema
+import importlib_metadata
+from apischema import (ValidationError, from_data, input_converter, schema,
+                       with_fields_set)
+
+PositiveInt = NewType("PositiveInt", int)
+schema(exc_min=0)(PositiveInt)
+
+
+Contractor = NewType("Contractor", PositiveInt)
+
+
+# Coercion not handled easily, by choice
+@input_converter
+def coerce_contractor(s: str) -> Contractor:
+    contractor = int(s)
+    if contractor <= 0:
+        yield "contractor < 0"
+    return Contractor(PositiveInt(contractor))
+
+
+# Defined on top level in order to be able to evaluate annotations (see PEP 563)
+# `apischema.set_type_hints` could be used instead, but I don't like it
+@dataclass
+class Location:
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+
+
+@dataclass
+class Skill:
+    subject: str
+    subject_id: int
+    category: str
+    qual_level: str
+    qual_level_id: int
+    qual_level_ranking: float = 0
+
+
+@with_fields_set
+@dataclass
+class Model:
+    id: int
+    client_name: str = field(metadata=schema(max_len=255))
+    sort_index: float
+    # must be before fields with default value
+    grecaptcha_response: str = field(metadata=schema(min_len=20, max_len=1000))
+    client_phone: Optional[str] = field(default=None, metadata=schema(max_len=255))
+
+    location: Optional[Location] = None
+
+    # cannot use PositiveInt because of coercion
+    # contractor: Optional[PositiveInt] = None
+    contractor: Optional[Contractor] = None
+    upstream_http_referrer: Optional[str] = field(default=None,
+                                                  metadata=schema(max_len=1023))
+    last_updated: Optional[datetime] = None
+
+    skills: List[Skill] = field(default_factory=list)
+
+
+class TestApischema:
+    package = apischema.__name__
+    version = importlib_metadata.version("apischema")
+
+    def __init__(self, allow_extra):
+        self.allow_extra = allow_extra
+        self.cls = Model
+
+    def validate(self, data):
+        try:
+            result = from_data(data, self.cls, additional_properties=self.allow_extra)
+        except ValidationError as e:
+            return False, e
+        else:
+            return True, result
+
+
+from apischema.alias import ALIAS_METADATA
+from apischema.data.from_data import DataWithConstraint, FromData, check_type
+from apischema.fields import init_fields
+from apischema.schema import CONSTRAINT_METADATA
+from apischema.typing import get_type_hints
+from apischema.utils import PREFIX
+from apischema.validation.errors import exception, merge
+from apischema.validation.mock import ValidatorMock
+from apischema.validation.validator import Validator, get_validators, validate
+
+FIELDS_CACHE = f"{PREFIX}fields"
+
+
+class OptimizedFromData(FromData):
+    def dataclass(self, cls: Type, data2: DataWithConstraint):
+        assert is_dataclass(cls)
+        data, constraint = data2
+        check_type(data, dict)
+        types = get_type_hints(cls)
+        values: Dict[str, Any] = {}
+        default: Dict[str, Field] = {}
+        aliases: List[str] = []
+        field_errors: Dict[str, ValidationError] = {}
+        try:
+            fields = getattr(cls, FIELDS_CACHE)
+        except AttributeError:
+            fields = tuple(init_fields(cls))
+            setattr(cls, FIELDS_CACHE, fields)
+        for field in fields:
+            assert isinstance(field, Field)
+            name = field.name
+            metadata = field.metadata
+            if ALIAS_METADATA in metadata:
+                alias = metadata[ALIAS_METADATA]
+            else:
+                alias = name
+            if alias in data:
+                if CONSTRAINT_METADATA in metadata:
+                    to_visit = data[alias], metadata[CONSTRAINT_METADATA]
+                else:
+                    to_visit = data[alias], None
+                try:
+                    values[name] = self.visit(types[name], to_visit)
+                except ValidationError as err:
+                    field_errors[alias] = err
+                aliases.append(alias)
+            elif (field.default is not MISSING
+                  or field.default_factory is not MISSING):
+                default[name] = field
+                aliases.append(alias)
+            else:
+                field_errors[alias] = ValidationError(["missing field"])
+        if len(data) != len(aliases) and not self.additional_properties:
+            remain = set(data).difference(aliases)
+            field_errors.update((field, ValidationError(["field not allowed"]))
+                                for field in sorted(remain))
+        if field_errors:
+            error = ValidationError(children=field_errors)
+            partial: List[Validator] = []
+            whole: List[Validator] = []
+            for val in get_validators(cls):
+                if val.can_be_called(values.keys()):
+                    partial.append(val)
+                else:
+                    whole.append(val)
+            try:
+                validate(ValidatorMock(cls, values, default), partial)
+            except ValidationError as err:
+                error = merge(error, err)
+            raise error
+        validators = get_validators(cls)
+        try:
+            res = cls(**values)
+        except Exception as err:
+            raise ValidationError([exception(err)])
+        validate(res, validators)
+        return res
+
+
+class TestApischemaOptimized(TestApischema):
+    package = "apischema (optimized)"
+
+    def validate(self, data):
+        visitor = OptimizedFromData(self.allow_extra)
+        try:
+            return True, visitor.visit(self.cls, (data, None))
+        except ValidationError as e:
+            return False, e
