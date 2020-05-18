cfe18bc1c08ec79e70040575385b3b491335ab34
Stephen Bunn <stephen@bunn.io>
2020-05-18 17:56:06 -0400

    Update tests/test_networks.py
    Co-authored-by: Samuel Colvin <samcolvin@gmail.com>

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -453,5 +453,5 @@ def test_name_email():
 
     assert str(Model(v=NameEmail('foo bar', 'foobaR@example.com')).v) == 'foo bar <foobaR@example.com>'
     assert str(Model(v='foo bar  <foobaR@example.com>').v) == 'foo bar <foobaR@example.com>'
-    assert Model(v=NameEmail('foo bar', 'foobaR@example.com')) == Model(v=NameEmail('foo bar', 'foobaR@example.com'))
-    assert Model(v=NameEmail('foo bar', 'foobaR@example.com')) != 'foo bar <foobaR@example.com>'
+        assert NameEmail('foo bar', 'foobaR@example.com') == NameEmail('foo bar', 'foobaR@example.com')
+        assert NameEmail('foo bar', 'foobaR@example.com') != NameEmail('foo bar', 'different@example.com')
