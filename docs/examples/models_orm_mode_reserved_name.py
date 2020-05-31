dc094f12116af9a92cc5e21b54e491f8361070ed
Samuel Colvin <s@muelcolvin.com>
2020-05-31 15:05:06 +0100

    fix blank lines in docs

--- docs/examples/models_orm_mode_reserved_name.py
+++ docs/examples/models_orm_mode_reserved_name.py
@@ -4,20 +4,23 @@ from pydantic import BaseModel, Field
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class MyModel(BaseModel):
     metadata: typing.Dict[str, str] = Field(alias='metadata_')
 
     class Config:
         orm_mode = True
 
+
 BaseModel = declarative_base()
 
-class SQLModel(BaseModel):
 
+class SQLModel(BaseModel):
     __tablename__ = 'my_table'
     id = sa.Column('id', sa.Integer, primary_key=True)
     # 'metadata' is reserved by SQLAlchemy, hence the '_'
     metadata_ = sa.Column('metadata', sa.JSON)
+
 
 sql_model = SQLModel(metadata_={'key': 'val'}, id=1)
 
