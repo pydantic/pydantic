50229a56bd65be8483601fbb1a822d69af38de00
bsitruk <sitruk.benjamin@gmail.com>
2020-10-04 11:01:21 +0000

    Add dynamic validators to doc

---
+++ docs/examples/models_dynamic_validators.py
@@ -0,0 +1,11 @@
+from pydantic import create_model, validator
+
+
+def username_alphanumeric(cls, v):
+    assert v.isalnum(), 'must be alphanumeric'
+    return v
+
+
+validators = {'username_validator': validator('username')(username_alphanumeric)}
+
+WithValidatorModel = create_model('WithValidatorModel', username=(str, ...), __validators__=validators)
