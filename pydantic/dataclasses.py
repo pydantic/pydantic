e05e544b0d39c486f4bcaabbf81dad27d02895a3
PrettyWood <em.jolibois@gmail.com>
2020-10-10 13:15:35 +0200

    docs: add docstring for `make_dataclass_validator`

--- pydantic/dataclasses.py
+++ pydantic/dataclasses.py
@@ -210,5 +210,9 @@ def dataclass(
 
 
 def make_dataclass_validator(_cls: Type[Any], **kwargs: Any) -> 'CallableGenerator':
+    """
+    Create a pydantic.dataclass from a builtin dataclass to add type validation
+    and yield the validators
+    """
     cls = dataclass(_cls, **kwargs)
     yield from _get_validators(cls)
