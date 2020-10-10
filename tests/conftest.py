371e263d334e3a5c98ce51ddb0d312c15e58ed44
MrMrRobat <appkiller16@gmail.com>
2020-10-10 20:48:15 +0300

    fix path to modules in test_module fixture, capture stderr and stdout

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,11 @@ import secrets
 import subprocess
 import sys
 from importlib.machinery import SourceFileLoader
+from pathlib import Path
 
 import pytest
+
+TEST_DIR = Path(__file__).parent
 
 
 class SetEnv:
@@ -47,8 +50,14 @@ def test_module():
     """
 
     def _test_module(module_name):
-        result = subprocess.run([sys.executable, module_name], timeout=5)
+        result = subprocess.run(
+            [sys.executable, TEST_DIR / module_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=5
+        )
         if result.returncode != 0:
-            pytest.fail(f'Running {module_name} failed with non-zero return code: {result.returncode}')
+            pytest.fail(
+                f'Running {module_name} failed with non-zero return code: {result.returncode}\n'
+                f'Captured stdout:\n{result.stdout.decode()}\n'
+                f'Captured stderr:\n{result.stderr.decode()}'
+            )
 
     return _test_module
