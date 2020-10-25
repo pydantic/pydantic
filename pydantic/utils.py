9096f6363ab6ce931cf4c3c84f0e5a12673a34ed
MrMrRobat <appkiller16@gmail.com>
2020-10-25 21:34:11 +0300

    remove unrelated change in utils.py

--- pydantic/utils.py
+++ pydantic/utils.py
@@ -138,7 +138,7 @@ def validate_field_name(bases: List[Type['BaseModel']], field_name: str) -> None
     Ensure that the field's name does not shadow an existing attribute of the model.
     """
     for base in bases:
-        if hasattr(base, field_name):
+        if getattr(base, field_name, None):
             raise NameError(
                 f'Field name "{field_name}" shadows a BaseModel attribute; '
                 f'use a different field name with "alias=\'{field_name}\'".'
