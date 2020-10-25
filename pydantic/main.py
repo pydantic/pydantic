f2af9f78064f60849d9c0c505f83d2cfb2cb1daa
me-ransh <62830748+me-ransh@users.noreply.github.com>
2020-10-25 21:18:56 +0200

    Fix #1999: call validator with correct "values" type in `setattr` (#2000)
    * call validator with correct "values" type in BaseModel.__setattr__
    
    * Fix code review comments
    - use `self.__dict__` as the values parameter
    - refine changed markdown
    
    Co-authored-by: Ran Shaham <ransh@mobileye.com>

--- pydantic/main.py
+++ pydantic/main.py
@@ -365,8 +365,10 @@ class BaseModel(Representation, metaclass=ModelMetaclass):
 
             known_field = self.__fields__.get(name, None)
             if known_field:
-                value, error_ = known_field.validate(value, self.dict(exclude={name}), loc=name, cls=self.__class__)
+                original_value = self.__dict__.pop(name)
+                value, error_ = known_field.validate(value, self.__dict__, loc=name, cls=self.__class__)
                 if error_:
+                    self.__dict__[name] = original_value
                     raise ValidationError([error_], self.__class__)
                 new_values[name] = value
 
