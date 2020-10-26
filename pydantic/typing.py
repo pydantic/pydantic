d067c71cff52a55595c778a8924a73c136ee481f
PrettyWood <em.jolibois@gmail.com>
2020-10-26 10:42:37 +0100

    chore: remove useless except

--- pydantic/typing.py
+++ pydantic/typing.py
@@ -121,9 +121,8 @@ else:
             get_args(Union[int, Tuple[T, int]][str]) == (int, Tuple[str, int])
             get_args(Callable[[], T][int]) == ([], int)
         """
-        args = typing_get_args(tp)
         # the fallback is needed for the same reasons as `get_origin` (see above)
-        return args or getattr(tp, '__args__', ()) or generic_get_args(tp)
+        return typing_get_args(tp) or getattr(tp, '__args__', ()) or generic_get_args(tp)
 
 
 if TYPE_CHECKING:
