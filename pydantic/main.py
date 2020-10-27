cc8ee191789901a5d17ea950acf8289c82ef618d
John Sabath <john.sabath@equipmentshare.com>
2020-10-26 21:56:31 -0500

    don't pop the value off __dict__

--- pydantic/main.py
+++ pydantic/main.py
@@ -384,7 +384,7 @@ class BaseModel(Representation, metaclass=ModelMetaclass):
 
             known_field = self.__fields__.get(name, None)
             if known_field:
-                original_value = self.__dict__.pop(name)
+                original_value = self.__dict__[name]
                 value, error_ = known_field.validate(value, self.__dict__, loc=name, cls=self.__class__)
                 if error_:
                     self.__dict__[name] = original_value
