3bee946090a91e7aeee4d81c84250fce9761e5ed
John Sabath <john.sabath@equipmentshare.com>
2020-10-27 11:05:42 -0500

    fixing test

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1157,20 +1157,25 @@ def test_nested_literal_validator():
     ]
 
 
-def test_field_that_is_being_validated_is_excluded_from_validator_values():
-    validators_values = None
+def test_field_that_is_being_validated_is_excluded_from_validator_values(mocker):
+    check_values = mocker.MagicMock()
 
     class Model(BaseModel):
         foo: str
         bar: str
 
+        class Config:
+            validate_assignment = True
+
         @validator('foo')
         def validate_foo(cls, v, values):
-            nonlocal validators_values
-            validators_values = values
+            check_values({**values})
 
-    Model(foo='foo_value', bar='bar_value')
-    assert validators_values == {'bar': 'bar_value'}
+    model = Model(foo='foo_value', bar='bar_value')
+    check_values.reset_mock()
+
+    model.foo = 'new_foo_value'
+    check_values.assert_called_once_with({'bar': 'bar_value'})
 
 
 def test_exceptions_in_field_validators_restore_original_field_value():
