9ae40a209f929e421736a69d093d01749861c772
Eric Jolibois <em.jolibois@gmail.com>
2020-11-30 18:55:50 +0100

    fix: set right default value for required (optional) fields (#2143)
    closes #2142

--- pydantic/fields.py
+++ pydantic/fields.py
@@ -307,7 +307,7 @@ class ModelField(Representation):
         required: 'BoolUndefined' = Undefined
         if value is Required:
             required = True
-            value = None
+            value = Ellipsis
         elif value is not Undefined:
             required = False
         field_info.alias = field_info.alias or field_info_from_config.get('alias')
