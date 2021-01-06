81172810f4f18a29e224273aadb543ba1df246fc
Mo Abrahams <3750013+dashavoo@users.noreply.github.com>
2021-01-06 20:08:14 +0000

    Format and fix bug in schema

--- pydantic/fragments.py
+++ pydantic/fragments.py
@@ -19,7 +19,7 @@ from pydantic.generics import GenericModel
 
 DictStrAny = Dict[str, Any]
 
-T = TypeVar("T", bound=BaseModel)
+T = TypeVar('T', bound=BaseModel)
 
 _missing = object()
 
@@ -36,10 +36,10 @@ class Fragment(GenericModel, Generic[T]):
     model_: Type[T]
 
     class Config:
-        extra = "allow"  # essentially everything is extra.
+        extra = 'allow'  # essentially everything is extra.
         allow_mutation = False
 
-    def __init__(__pydantic_self__, __model__: Optional[Type[T]] = None, **data):
+    def __init__(__pydantic_self__, __model__: Optional[Type[T]] = None, **data: Any):
         """Initialises the Fragment by validating against __model__'s fields.
         Args:
             __model__: The model this is a fragment of - positional only.
@@ -49,7 +49,7 @@ class Fragment(GenericModel, Generic[T]):
         model = __model__  # __model__ is underscored in the method arguments to prevent a name clash.
 
         if model is None and __pydantic_self__.__concrete__:
-            expected_model_type = __pydantic_self__.__fields__["model_"].type_.__args__[0]
+            expected_model_type = __pydantic_self__.__fields__['model_'].type_.__args__[0]
             model = expected_model_type
 
         elif model and not __pydantic_self__.__concrete__:
@@ -57,11 +57,11 @@ class Fragment(GenericModel, Generic[T]):
             expected_model_type = model
 
         elif model is None and not __pydantic_self__.__concrete__:
-            raise TypeError("Fragments must be of another model.")
+            raise TypeError('Fragments must be of another model.')
 
         else:
             if model != expected_model_type:
-                raise TypeError(f"Expected a fragment of {expected_model_type}.")
+                raise TypeError(f'Expected a fragment of {expected_model_type}.')
 
         values: Dict[str, Any] = {}
         errors = []
@@ -85,39 +85,38 @@ class Fragment(GenericModel, Generic[T]):
         if errors:
             raise (ValidationError(errors, model))
         else:
-            if model.__config__.extra == "allow":
+            if model.__config__.extra == 'allow':
                 values.update(data)
-            values["model_"] = model
-            object.__setattr__(__pydantic_self__, "__dict__", values)
-            object.__setattr__(__pydantic_self__, "__fields_set__", fields_set)
+            values['model_'] = model
+            object.__setattr__(__pydantic_self__, '__dict__', values)
+            object.__setattr__(__pydantic_self__, '__fields_set__', fields_set)
             __pydantic_self__._init_private_attributes()
 
     @classmethod
-    def schema(cls, *args, **kwargs) -> DictStrAny:
+    def schema(cls, *args: Any, **kwargs: Any) -> DictStrAny:
         """Overrides the standard BaseModel schema.
 
         The standard basemodel schema is generated, but all fields
-        are removed from "required". "Fragment" is appended to the title of the schema.
+        are removed from 'required'. 'Fragment' is appended to the title of the schema.
 
         The model_ field is excluded from the schema as it represents a type.
         """
         if cls.__concrete__:
-            model = cls.__fields__["model_"].type_.__args__[0]
+            model = cls.__fields__['model_'].type_.__args__[0]
             model_schema = model.schema(*args, **kwargs)
-            model_schema["required"] = []
-            model_schema["title"] = model_schema["title"] + "Fragment"
-            return model_schema
+            title = model_schema['title'] + 'Fragment'
+            return {**model_schema, **{'required': [], 'title': title}}
         else:
             # todo: no idea what to return of not concrete,
             #       and because classmethod can't use __pydantic_self__.model_
             return {}
 
-    def dict(__pydantic_self__, *args, **kwargs) -> DictStrAny:
-        """Overrides the standard BaseModel dict to exclude "model_"."""
+    def dict(self, *args: Any, **kwargs: Any) -> DictStrAny:
+        """Overrides the standard BaseModel dict to exclude 'model_'."""
         exclude = {
-            "model_",
+            'model_',
         }
-        user_exclude = kwargs.pop("exclude", None)
+        user_exclude = kwargs.pop('exclude', None)
         if user_exclude:
             exclude = exclude.union(user_exclude)
         return super().dict(*args, exclude=exclude, **kwargs)
