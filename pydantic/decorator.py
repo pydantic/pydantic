29bab677d2335c889e3058445e2ce459ba3669b0
Anthony King <anthonydking@gmail.com>
2021-01-10 21:32:56 +0000

    fix: also prevent overwriting variable args
    Introduces tests for both, which compare against the native python implementation
    
    Co-authored-by: Eric Jolibois <em.jolibois@gmail.com>

--- pydantic/decorator.py
+++ pydantic/decorator.py
@@ -161,7 +161,7 @@ class ValidatedFunction:
 
         var_kwargs = {}
         wrong_positional_args = []
-        non_var_fields = set(self.model.__fields__) - {self.v_kwargs_name}
+        non_var_fields = set(self.model.__fields__) - {self.v_args_name, self.v_kwargs_name}
         for k, v in kwargs.items():
             if k in non_var_fields:
                 if k in self.positional_only_args:
@@ -178,9 +178,7 @@ class ValidatedFunction:
 
     def execute(self, m: BaseModel) -> Any:
         d = {k: v for k, v in m._iter() if k in m.__fields_set__}
-        kwargs = d.pop(self.v_kwargs_name, None)
-        if kwargs:
-            d.update(kwargs)
+        var_kwargs = d.pop(self.v_kwargs_name, None) or {}
 
         if self.v_args_name in d:
             args_: List[Any] = []
@@ -194,7 +192,7 @@ class ValidatedFunction:
                     in_kwargs = True
                 else:
                     args_.append(value)
-            return self.raw_function(*args_, **kwargs)
+            return self.raw_function(*args_, **kwargs, **var_kwargs)
         elif self.positional_only_args:
             args_ = []
             kwargs = {}
@@ -203,9 +201,9 @@ class ValidatedFunction:
                     args_.append(value)
                 else:
                     kwargs[name] = value
-            return self.raw_function(*args_, **kwargs)
+            return self.raw_function(*args_, **kwargs, **var_kwargs)
         else:
-            return self.raw_function(**d)
+            return self.raw_function(**d, **var_kwargs)
 
     def create_model(self, fields: Dict[str, Any], takes_args: bool, takes_kwargs: bool, config: 'ConfigType') -> None:
         pos_args = len(self.arg_mapping)
