dbcee3344b5d3e4dfb70c97a05b2c7ed4dfa4313
Eric Jolibois <em.jolibois@gmail.com>
2021-02-11 00:05:05 +0100

    improve caught warning in pytest
    Co-authored-by: Thomas Grainger <tagrain@gmail.com>

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -12,4 +12,4 @@ pip install -U flit
 flit install
 
 # ignore cryptography warning https://github.com/mpdavis/python-jose/issues/208
-PYTHONPATH=./docs/src pytest -W ignore::DeprecationWarning:cryptography
+PYTHONPATH=./docs/src pytest -W 'ignore:int_from_bytes is deprecated, use int.from_bytes instead:cryptography.utils.CryptographyDeprecationWarning'
