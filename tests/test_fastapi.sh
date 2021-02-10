eae8960b66ab6d68a82edccb1e27fc4d44e7cb1d
PrettyWood <em.jolibois@gmail.com>
2021-02-10 23:58:08 +0100

    fix CI temporarily

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -11,4 +11,5 @@ git checkout "${latest_tag}"
 pip install -U flit
 flit install
 
-PYTHONPATH=./docs/src pytest
+# ignore cryptography warning https://github.com/mpdavis/python-jose/issues/208
+PYTHONPATH=./docs/src pytest -W ignore::DeprecationWarning:cryptography
