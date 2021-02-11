688107ec8e66f8da3bd1f97be6093a3cfab06a22
Justin Sexton <justinsexton.dev@gmail.com>
2021-02-11 11:39:59 -0600

    Fixed KeyError when creating schema from models with same names declared within different classes (#2170)
    * Fixed issue causing KeyError to be raised when building schema from multiple `BaseModel` with the same names declared in separate classes (#1912)
    
    * Update test
    
    * Removed redundant f-string
    
    * Fixed formatting
    
    * Fixed lint

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -2107,6 +2107,44 @@ def test_new_type():
     }
 
 
+def test_multiple_models_with_same_name(create_module):
+    module = create_module(
+        # language=Python
+        """
+from pydantic import BaseModel
+
+
+class ModelOne(BaseModel):
+    class NestedModel(BaseModel):
+        a: float
+
+    nested: NestedModel
+
+
+class ModelTwo(BaseModel):
+    class NestedModel(BaseModel):
+        b: float
+
+    nested: NestedModel
+
+
+class NestedModel(BaseModel):
+    c: float
+        """
+    )
+
+    models = [module.ModelOne, module.ModelTwo, module.NestedModel]
+    model_names = set(schema(models)['definitions'].keys())
+    expected_model_names = {
+        'ModelOne',
+        'ModelTwo',
+        f'{module.__name__}__ModelOne__NestedModel',
+        f'{module.__name__}__ModelTwo__NestedModel',
+        f'{module.__name__}__NestedModel',
+    }
+    assert model_names == expected_model_names
+
+
 def test_multiple_enums_with_same_name(create_module):
     module_1 = create_module(
         # language=Python
