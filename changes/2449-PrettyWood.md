28aa4fb66d26e7dda4f080d95202d6884405edb4
PrettyWood <em.jolibois@gmail.com>
2021-03-02 13:21:17 +0100

    fix: validation with a `BaseModel` field and a custom root type

---
+++ changes/2449-PrettyWood.md
@@ -0,0 +1,1 @@
+fix validation with a `BaseModel` field and a custom root type
--- pydantic/main.py
+++ pydantic/main.py
@@ -717,11 +717,12 @@ class BaseModel(Representation, metaclass=ModelMetaclass):
 
     @classmethod
     def validate(cls: Type['Model'], value: Any) -> 'Model':
+        if isinstance(value, cls):
+            return value.copy() if cls.__config__.copy_on_model_validation else value
+
         value = cls._enforce_dict_if_root(value)
         if isinstance(value, dict):
             return cls(**value)
-        elif isinstance(value, cls):
-            return value.copy() if cls.__config__.copy_on_model_validation else value
         elif cls.__config__.orm_mode:
             return cls.from_orm(value)
         else:
