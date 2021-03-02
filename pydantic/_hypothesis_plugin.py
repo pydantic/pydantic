429b4398309454374dbe2432b20c287acd80be9b
Tobi Lipede <51169226+tobi-lipede-oodle@users.noreply.github.com>
2021-03-02 18:56:45 +0000

    Allow for generation of a constrained float with multiple_of argument for hypothesis plugin (#2442)
    * added method to generate a constrained float with multiple_of argument
    
    * removed HealthCheck.filter_too_much
    
    * Added change file
    
    * fixes for case when min/max aren't provided; adding an extra test for float gt/lt with multiple of

--- pydantic/_hypothesis_plugin.py
+++ pydantic/_hypothesis_plugin.py
@@ -276,6 +276,7 @@ def resolve_confloat(cls):  # type: ignore[no-untyped-def]
     max_value = cls.le
     exclude_min = False
     exclude_max = False
+
     if cls.gt is not None:
         assert min_value is None, 'Set `gt` or `ge`, but not both'
         min_value = cls.gt
@@ -284,7 +285,21 @@ def resolve_confloat(cls):  # type: ignore[no-untyped-def]
         assert max_value is None, 'Set `lt` or `le`, but not both'
         max_value = cls.lt
         exclude_max = True
-    return st.floats(min_value, max_value, exclude_min=exclude_min, exclude_max=exclude_max, allow_nan=False)
+
+    if cls.multiple_of is None:
+        return st.floats(min_value, max_value, exclude_min=exclude_min, exclude_max=exclude_max, allow_nan=False)
+
+    if min_value is not None:
+        min_value = math.ceil(min_value / cls.multiple_of)
+        if exclude_min:
+            min_value = min_value + 1
+    if max_value is not None:
+        assert max_value >= cls.multiple_of, 'Cannot build model with max value smaller than multiple of'
+        max_value = math.floor(max_value / cls.multiple_of)
+        if exclude_max:
+            max_value = max_value - 1
+
+    return st.integers(min_value, max_value).map(lambda x: x * cls.multiple_of)
 
 
 @resolves(pydantic.ConstrainedInt)
