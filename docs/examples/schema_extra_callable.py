884f22e77468f21a926bd5671e20c57a6f388226
Maarten Pronk <evetion@users.noreply.github.com>
2021-10-29 08:43:22 +0000

    fix: Documentation example
    The `title` attribute to filter in the schema was missing and the `schema_extra` method wasn't modifying the `Dict` at all.

--- docs/examples/schema_extra_callable.py
+++ docs/examples/schema_extra_callable.py
@@ -6,12 +6,13 @@ from pydantic import BaseModel
 class Person(BaseModel):
     name: str
     age: int
+    title: str
 
     class Config:
         @staticmethod
         def schema_extra(schema: Dict[str, Any], model: Type['Person']) -> None:
-            for prop in schema.get('properties', {}).values():
-                prop.pop('title', None)
+            props = schema.get('properties', {})
+            props.pop('title', None)
 
 
 print(Person.schema_json(indent=2))
