5c0ee231a82491183ddadd3820d72b61e826e3c5
Schlerp <schlerp@users.noreply.github.com>
2021-12-02 15:30:10 +0930

    Create utils.py
    I needed non pascal case camel case support

--- pydantic/utils.py
+++ pydantic/utils.py
@@ -275,6 +275,12 @@ def get_model(obj: Union[Type['BaseModel'], Type['Dataclass']]) -> Type['BaseMod
 def to_camel(string: str) -> str:
     return ''.join(word.capitalize() for word in string.split('_'))
 
+def to_lower_camel(string: str) -> str:
+    if len(string) >= 1:
+        pascal_string = to_camel(string)
+        return pascal_string[0].lower() + pascal_string[1:]
+    return string.lower()
+
 
 T = TypeVar('T')
 
