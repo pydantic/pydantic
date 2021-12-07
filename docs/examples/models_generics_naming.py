0c18f035f156bb95bab544f3d1b06a80e87f32be
Eric Jolibois <em.jolibois@gmail.com>
2021-12-07 10:43:07 +0100

    use `repr` to display class name

--- docs/examples/models_generics_naming.py
+++ docs/examples/models_generics_naming.py
@@ -13,5 +13,5 @@ class Response(GenericModel, Generic[DataT]):
         return f'{params[0].__name__.title()}Response'
 
 
-print(Response[int](data=1).__class__.__name__)
-print(Response[str](data='a').__class__.__name__)
+print(repr(Response[int](data=1)))
+print(repr(Response[str](data='a')))
