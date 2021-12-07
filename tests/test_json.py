270e105bc258c82664b01a1cb6be86434b34c205
Yurii Karabas <1998uriyyo@gmail.com>
2021-12-07 20:40:55 +0200

    fix lint error

--- tests/test_json.py
+++ tests/test_json.py
@@ -291,7 +291,7 @@ def test_custom_decode_encode():
         (int, 1, str, '{"field": "1"}'),
         (bool, True, int, '{"field": 1}'),
         (float, 0.5, str, '{"field": "0.5"}'),
-        (bytes, b"1", lambda v: str(v * 2, "utf-8"), '{"field": "11"}'),
+        (bytes, b'1', lambda v: str(v * 2, 'utf-8'), '{"field": "11"}'),
         (frozenset, frozenset([1, 2, 3]), lambda v: dict(enumerate(v)), '{"field": {"0": 1, "1": 2, "2": 3}}'),
     ],
     ids=[
