dc4710b56cbd427ca47ea2da7b45b1f669c4d45f
João M. Martins <joaomartins139@gmail.com>
2021-12-08 22:52:25 +0100

    Replacing encode_default instance check with strict type check (#3197)
    * Replacing encode_default instance check with strict type check
    
    * Adding change notes
    
    * Changing to dictionary indexing in tests
    
    * Adding explicit Enum check and returning its value

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1435,6 +1435,26 @@ def test_list_default():
     }
 
 
+def test_enum_str_default():
+    class MyEnum(str, Enum):
+        FOO = 'foo'
+
+    class UserModel(BaseModel):
+        friends: MyEnum = MyEnum.FOO
+
+    assert UserModel.schema()['properties']['friends']['default'] is MyEnum.FOO.value
+
+
+def test_enum_int_default():
+    class MyEnum(IntEnum):
+        FOO = 1
+
+    class UserModel(BaseModel):
+        friends: MyEnum = MyEnum.FOO
+
+    assert UserModel.schema()['properties']['friends']['default'] is MyEnum.FOO.value
+
+
 def test_dict_default():
     class UserModel(BaseModel):
         friends: Dict[str, float] = {'a': 1.1, 'b': 2.2}
