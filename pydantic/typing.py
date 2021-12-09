f8cc585451407896d6a08eb8954e2b39f5fe3c18
PrettyWood <em.jolibois@gmail.com>
2021-12-09 13:57:29 +0100

    fix: duplicate

--- pydantic/typing.py
+++ pydantic/typing.py
@@ -298,18 +298,6 @@ elif sys.version_info[:2] == (3, 8):
                 return True
         return False
 
-elif sys.version_info[:2] == (3, 8):
-    # We can use the fast implementation for 3.8 but there is a very weird bug
-    # where it can fail for `Literal[None]`.
-    # We just need to redefine a useless `Literal[None]` inside the function body to fix this
-
-    def is_none_type(type_: Any) -> bool:
-        Literal[None]  # fix edge case
-        for none_type in NONE_TYPES:
-            if type_ is none_type:
-                return True
-        return False
-
 else:
 
     def is_none_type(type_: Any) -> bool:
