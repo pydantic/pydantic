e012089f1209106d215a8c3d87920e18bb920b6e
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2021-12-11 22:44:09 +0100

    Mention Python >= 3.9.2 as an alternative to `typing_extensions.TypedDict` (#3374)
    * Mention python >= 3.9.2 as an alternative to `typing_extensions.TypedDict`
    
    * Narrow the upper version for `LegacyTypedDict`: 3.9 -> 3.9.2
    
    * Add an entry to `changes`
    
    * Update pydantic/annotated_types.py
    
    Co-authored-by: Samuel Colvin <samcolvin@gmail.com>
    
    * Update `TypedDict` exception message in the test suite
    
    * linting
    
    Co-authored-by: Samuel Colvin <samcolvin@gmail.com>
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>

--- tests/test_annotated_types.py
+++ tests/test_annotated_types.py
@@ -13,7 +13,7 @@ from typing_extensions import TypedDict
 
 from pydantic import BaseModel, PositiveInt, ValidationError
 
-if sys.version_info < (3, 9):
+if sys.version_info < (3, 9, 2):
     try:
         from typing import TypedDict as LegacyTypedDict
     except ImportError:
@@ -221,7 +221,10 @@ def test_partial_legacy_typeddict():
     class User(OptionalUser):
         id: int
 
-    with pytest.raises(TypeError, match='^You should use `typing_extensions.TypedDict` instead of `typing.TypedDict`'):
+    with pytest.raises(
+        TypeError,
+        match='^You should use `typing_extensions.TypedDict` instead of `typing.TypedDict` with Python < 3.9.2.',
+    ):
 
         class Model(BaseModel):
             user: User
