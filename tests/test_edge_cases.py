973f363d170d36a765b73efd8bb249e622a65247
Samuel Colvin <s@muelcolvin.com>
2022-01-21 09:34:19 +0000

    fix and add change description

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -1917,4 +1917,18 @@ def test_bytes_subclass():
             self = bytes.__new__(cls, data)
             return self
 
-    assert MyModel(my_bytes=BytesSubclass(b'foobar')).my_bytes.__class__ == BytesSubclass
+    m = MyModel(my_bytes=BytesSubclass(b'foobar'))
+    assert m.my_bytes.__class__ == BytesSubclass
+
+
+def test_int_subclass():
+    class MyModel(BaseModel):
+        my_int: int
+
+    class IntSubclass(int):
+        def __new__(cls, data: int):
+            self = int.__new__(cls, data)
+            return self
+
+    m = MyModel(my_int=IntSubclass(123))
+    assert m.my_int.__class__ == IntSubclass
