05a16a2ad588eba7535ed5ed08201bc823cf3ae2
Lucas Tosetto Morvillo <lucastosetto@outlook.com>
2022-01-22 16:21:41 -0300

    Fixed doc about custom encoder

--- docs/examples/exporting_models_json_encoders_merge.py
+++ docs/examples/exporting_models_json_encoders_merge.py
@@ -21,4 +21,4 @@ class ChildClassWithEncoders(BaseClassWithEncoders):
 
 
 m = ChildClassWithEncoders(dt=datetime(2032, 6, 1), diff=timedelta(hours=100))
-print(m.json())
+print(m.json(models_as_dict=False))
