43f67ab4bf0a4dee4cd88118e0fd4b38fb8fce3c
Samuel Colvin <s@muelcolvin.com>
2022-04-05 17:15:51 +0100

    string benchmarks
    Original-commit-hash: e6db077833a4fd63210471c72c1a4373c714b831

--- src/lib.rs
+++ src/lib.rs
@@ -6,29 +6,19 @@ use pyo3::prelude::*;
 use pyo3::{create_exception, wrap_pyfunction};
 
 mod core;
+mod validators;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 create_exception!(_pydantic_core, ValidationError, PyValueError);
 
-#[pyfunction]
-fn parse(py: Python, _schema: PyObject, _obj: PyObject) -> PyResult<PyObject> {
-    Ok("foobar".to_object(py))
-    // let schema: &PyAny = schema.extract(py)?;
-    // let schema = core::Schema::extract(schema)?;
-    // //
-    // let obj: &PyAny = obj.extract(py)?;
-    // core::parse(py, &schema, obj)
-
-    // let msg = format!("{:#?}", schema);
-    // Ok(msg.to_object(py))
-}
-
 #[pymodule]
 fn _pydantic_core(py: Python, m: &PyModule) -> PyResult<()> {
     m.add("ValidationError", py.get_type::<ValidationError>())?;
     m.add("__version__", VERSION)?;
-    m.add_wrapped(wrap_pyfunction!(parse))?;
+    m.add_wrapped(wrap_pyfunction!(validators::str_validator))?;
+    m.add_wrapped(wrap_pyfunction!(validators::check_str))?;
+    m.add_wrapped(wrap_pyfunction!(validators::check_list_str))?;
     m.add_class::<core::SchemaValidator>()?;
     Ok(())
 }
