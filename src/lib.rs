5d7a5c1c8f2726baf186a20a146c9b4184289df0
Samuel Colvin <s@muelcolvin.com>
2022-04-06 08:28:42 +0100

    rust a bit faster than python

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,9 @@ create_exception!(_pydantic_core, ValidationError, PyValueError);
 fn _pydantic_core(py: Python, m: &PyModule) -> PyResult<()> {
     m.add("ValidationError", py.get_type::<ValidationError>())?;
     m.add("__version__", VERSION)?;
-    m.add_wrapped(wrap_pyfunction!(validators::str_validator))?;
-    m.add_wrapped(wrap_pyfunction!(validators::check_str))?;
-    m.add_wrapped(wrap_pyfunction!(validators::check_list_str))?;
+    m.add_wrapped(wrap_pyfunction!(validators::validate_str))?;
+    m.add_wrapped(wrap_pyfunction!(validators::validate_str_full))?;
+    m.add_wrapped(wrap_pyfunction!(validators::validate_str_recursive))?;
     m.add_class::<core::SchemaValidator>()?;
     Ok(())
 }
