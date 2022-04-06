e31bed6385ca442393601ff69eb4aa9558029792
Samuel Colvin <s@muelcolvin.com>
2022-04-06 08:28:42 +0100

    rust a bit faster than python
    Original-commit-hash: 5d7a5c1c8f2726baf186a20a146c9b4184289df0

--- src/validators.rs
+++ src/validators.rs
@@ -1,12 +1,12 @@
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
-use pyo3::types::{PyBytes, PyList};
+use pyo3::types::{PyBytes, PyDict, PyList, PyString};
 
 #[pyfunction]
-pub fn str_validator(v: &PyAny) -> PyResult<String> {
-    if let Ok(str) = String::extract(v) {
-        Ok(str)
-    } else if let Ok(bytes) = v.extract::<&PyBytes>() {
+pub fn validate_str(v: &PyAny) -> PyResult<String> {
+    if let Ok(str) = v.downcast::<PyString>() {
+        str.extract()
+    } else if let Ok(bytes) = v.downcast::<PyBytes>() {
         Ok(std::str::from_utf8(bytes.as_bytes())?.to_string())
     } else if let Ok(int) = i64::extract(v) {
         Ok(int.to_string())
@@ -18,7 +18,7 @@ pub fn str_validator(v: &PyAny) -> PyResult<String> {
 }
 
 #[pyfunction]
-pub fn check_str(
+pub fn validate_str_full(
     v: &PyAny,
     min_length: Option<usize>,
     max_length: Option<usize>,
@@ -26,7 +26,12 @@ pub fn check_str(
     to_lower: bool,
     to_upper: bool,
 ) -> PyResult<String> {
-    let mut str = str_validator(v)?;
+    let mut str = validate_str(v)?;
+
+    if strip_whitespace {
+        str = str.trim().to_string();
+    }
+
     if let Some(min_length) = min_length {
         if str.len() < min_length {
             return Err(PyValueError::new_err(format!("{} is shorter than {}", str, min_length)));
@@ -38,10 +43,6 @@ pub fn check_str(
         }
     }
 
-    if strip_whitespace {
-        str = str.trim().to_string();
-    }
-
     if to_lower {
         Ok(str.to_lowercase())
     } else if to_upper {
@@ -49,35 +50,61 @@ pub fn check_str(
     } else {
         Ok(str)
     }
-
-    // Ok(data.to_object(py))
-    // match String::extract(data) {
-    //     Ok(s) => Ok(s),
-    //     Err(e) => Err(e),
-    // }
 }
 
-#[pyfunction]
-pub fn check_list_str(
-    py: Python,
-    items: &PyAny,
+fn validate_str_list<'py>(
+    py: Python<'py>,
+    list: &PyList,
     min_length: Option<usize>,
     max_length: Option<usize>,
     strip_whitespace: bool,
     to_lower: bool,
     to_upper: bool,
-) -> PyResult<PyObject> {
-    let items: &PyList = items.extract()?;
-    let mut new_vec: Vec<String> = Vec::with_capacity(items.len());
-    for item in items.iter() {
-        new_vec.push(check_str(
-            item,
-            min_length,
-            max_length,
-            strip_whitespace,
-            to_lower,
-            to_upper,
-        )?);
+) -> PyResult<&'py PyAny> {
+    let mut new_vec: Vec<&'py PyAny> = Vec::with_capacity(list.len());
+    for value in list.iter() {
+        let value = validate_str_recursive(py, value, min_length, max_length, strip_whitespace, to_lower, to_upper)?;
+        new_vec.push(value);
     }
-    Ok(new_vec.to_object(py))
+    // Ok(new_list.to_object(py))
+    let new_list = PyList::new(py, &new_vec);
+    Ok(new_list)
+}
+
+fn validate_str_dict<'py>(
+    py: Python<'py>,
+    dict: &PyDict,
+    min_length: Option<usize>,
+    max_length: Option<usize>,
+    strip_whitespace: bool,
+    to_lower: bool,
+    to_upper: bool,
+) -> PyResult<&'py PyAny> {
+    let new_dict = PyDict::new(py);
+    for (key, value) in dict.iter() {
+        let value = validate_str_recursive(py, value, min_length, max_length, strip_whitespace, to_lower, to_upper)?;
+        new_dict.set_item(key, value)?;
+    }
+    Ok(new_dict)
+}
+
+#[pyfunction]
+pub fn validate_str_recursive<'py>(
+    py: Python<'py>,
+    value: &PyAny,
+    min_length: Option<usize>,
+    max_length: Option<usize>,
+    strip_whitespace: bool,
+    to_lower: bool,
+    to_upper: bool,
+) -> PyResult<&'py PyAny> {
+    if let Ok(list) = value.downcast::<PyList>() {
+        validate_str_list(py, list, min_length, max_length, strip_whitespace, to_lower, to_upper)
+    } else if let Ok(dict) = value.downcast::<PyDict>() {
+        validate_str_dict(py, dict, min_length, max_length, strip_whitespace, to_lower, to_upper)
+    } else {
+        let s = validate_str_full(value, min_length, max_length, strip_whitespace, to_lower, to_upper)?;
+        let s = PyString::new(py, &s);
+        Ok(s)
+    }
 }
