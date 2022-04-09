836e217c9a30b8eb900878c81a1326318cda76cc
Samuel Colvin <s@muelcolvin.com>
2022-04-09 20:29:30 +0100

    change number constraints
    Original-commit-hash: fabf014ae94805e9ae3d68e78a741dc6463e0a86

--- src/type_validators/int.rs
+++ src/type_validators/int.rs
@@ -11,10 +11,10 @@ impl TypeValidator for SimpleIntValidator {
     fn is_match(type_: &str, dict: &PyDict) -> bool {
         type_ == "int"
             && dict.get_item("multiple_of").is_none()
-            && dict.get_item("maximum").is_none()
-            && dict.get_item("exclusive_maximum").is_none()
-            && dict.get_item("minimum").is_none()
-            && dict.get_item("exclusive_minimum").is_none()
+            && dict.get_item("le").is_none()
+            && dict.get_item("lt").is_none()
+            && dict.get_item("ge").is_none()
+            && dict.get_item("gt").is_none()
     }
 
     fn build(_dict: &PyDict) -> PyResult<Self> {
@@ -34,10 +34,10 @@ impl TypeValidator for SimpleIntValidator {
 #[derive(Debug, Clone)]
 pub struct FullIntValidator {
     multiple_of: Option<i64>,
-    maximum: Option<i64>,
-    exclusive_maximum: Option<i64>,
-    minimum: Option<i64>,
-    exclusive_minimum: Option<i64>,
+    le: Option<i64>,
+    lt: Option<i64>,
+    ge: Option<i64>,
+    gt: Option<i64>,
 }
 
 impl TypeValidator for FullIntValidator {
@@ -48,10 +48,10 @@ impl TypeValidator for FullIntValidator {
     fn build(dict: &PyDict) -> PyResult<Self> {
         Ok(Self {
             multiple_of: dict_get!(dict, "multiple_of", i64),
-            maximum: dict_get!(dict, "maximum", i64),
-            exclusive_maximum: dict_get!(dict, "exclusive_maximum", i64),
-            minimum: dict_get!(dict, "minimum", i64),
-            exclusive_minimum: dict_get!(dict, "exclusive_minimum", i64),
+            le: dict_get!(dict, "le", i64),
+            lt: dict_get!(dict, "lt", i64),
+            ge: dict_get!(dict, "ge", i64),
+            gt: dict_get!(dict, "gt", i64),
         })
     }
 
@@ -62,23 +62,23 @@ impl TypeValidator for FullIntValidator {
                 return py_error!("Value is not multiple of the specified value");
             }
         }
-        if let Some(maximum) = self.maximum {
-            if value > maximum {
+        if let Some(le) = self.le {
+            if value > le {
                 return py_error!("Value is greater than the specified value");
             }
         }
-        if let Some(exclusive_maximum) = self.exclusive_maximum {
-            if value >= exclusive_maximum {
+        if let Some(lt) = self.lt {
+            if value >= lt {
                 return py_error!("Value is greater than or equal to the specified value");
             }
         }
-        if let Some(minimum) = self.minimum {
-            if value < minimum {
+        if let Some(ge) = self.ge {
+            if value < ge {
                 return py_error!("Value is less than the specified value");
             }
         }
-        if let Some(exclusive_minimum) = self.exclusive_minimum {
-            if value <= exclusive_minimum {
+        if let Some(gt) = self.gt {
+            if value <= gt {
                 return py_error!("Value is less than or equal to the specified value");
             }
         }
