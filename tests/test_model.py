c4529b725bf920fec25797730c32732719795e1a
Samuel Colvin <s@muelcolvin.com>
2022-04-12 19:24:21 +0100

    fixing extra
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/ac10a39dcdaf51702831f9635b53b7f8b9e70e7b

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,6 @@
-from pydantic_core import SchemaValidator
+import pytest
+
+from pydantic_core import SchemaValidator, ValidationError
 
 
 def test_simple():
@@ -7,4 +9,29 @@ def test_simple():
     assert v.run({'field_a': 123, 'field_b': 1}) == {
         'fields_set': {'field_b', 'field_a'},
         'values': {'field_a': '123', 'field_b': 1},
+    }
+
+
+def test_ignore_extra():
+    v = SchemaValidator({'type': 'model', 'fields': {'field_a': {'type': 'str'}, 'field_b': {'type': 'int'}}})
+
+    assert v.run({'field_a': 123, 'field_b': 1, 'field_c': 123}) == {
+        'fields_set': {'field_b', 'field_a'},
+        'values': {'field_a': '123', 'field_b': 1},
+    }
+
+
+def test_forbid_extra():
+    v = SchemaValidator({'type': 'model', 'fields': {'field_a': {'type': 'str'}}, 'config': {'extra': 'forbid'}})
+
+    with pytest.raises(ValidationError, match='field_b | Extra values are not permitted'):
+        v.run({'field_a': 123, 'field_b': 1})
+
+
+def test_allow_extra():
+    v = SchemaValidator({'type': 'model', 'fields': {'field_a': {'type': 'str'}}, 'config': {'extra': 'allow'}})
+
+    assert v.run({'field_a': 123, 'field_b': (1, 2)}) == {
+        'fields_set': {'field_a', 'field_b'},
+        'values': {'field_a': '123', 'field_b': (1, 2)},
     }
