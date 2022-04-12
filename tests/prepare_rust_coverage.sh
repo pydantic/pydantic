e2910cbf790fedddd3f2a1bd13c596bea8eb9f97
Samuel Colvin <s@muelcolvin.com>
2022-04-13 00:34:26 +0100

    try rust coverage (pydantic/pydantic-core#12)
    * try rust coverage
    
    * fix ci
    
    * add rustfilt
    
    * fix coverage generate
    
    * try without telling codecov which files
    
    * alter rust caching
    
    * remove used coverage files
    
    * simplify rust coverage logic
    
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/d69c45c001fd562a4e5e15f75e5c9e8ffacf9941

---
+++ tests/prepare_rust_coverage.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rust-profdata merge -sparse default.profraw -o default.profdata
+rust-cov export -Xdemangler=rustfilt $(ls pydantic_core/*.so) \
+    -instr-profile=default.profdata \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex='library/std' \
+    -format=lcov > default.lcov
+
+rm default.profraw default.profdata
