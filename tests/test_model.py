297cf896be6e81060956dbb3650d271622cb3f51
Samuel Colvin <s@muelcolvin.com>
2022-04-12 19:43:54 +0100

    fixing model defaults
    Original-commit-hash: d56192a8c9d45f8514966d2b49d100e6c9d4797b

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,19 +6,31 @@ from pydantic_core import SchemaValidator, ValidationError
 def test_simple():
     v = SchemaValidator({'type': 'model', 'fields': {'field_a': {'type': 'str'}, 'field_b': {'type': 'int'}}})
 
-    assert v.run({'field_a': 123, 'field_b': 1}) == {
-        'fields_set': {'field_b', 'field_a'},
-        'values': {'field_a': '123', 'field_b': 1},
-    }
+    assert v.run({'field_a': 123, 'field_b': 1}) == ({'field_a': '123', 'field_b': 1}, {'field_b', 'field_a'})
+
+
+def test_with_default():
+    v = SchemaValidator(
+        {'type': 'model', 'fields': {'field_a': {'type': 'str'}, 'field_b': {'type': 'int', 'default': 666}}}
+    )
+
+    assert v.run({'field_a': 123}) == ({'field_a': '123', 'field_b': 666}, {'field_a'})
+    assert v.run({'field_a': 123, 'field_b': 1}) == ({'field_a': '123', 'field_b': 1}, {'field_b', 'field_a'})
+
+
+def test_missing_error():
+    v = SchemaValidator({'type': 'model', 'fields': {'field_a': {'type': 'str'}, 'field_b': {'type': 'int'}}})
+    with pytest.raises(ValidationError, match='field_b | Missing data for required field'):
+        v.run({'field_a': 123})
 
 
 def test_ignore_extra():
     v = SchemaValidator({'type': 'model', 'fields': {'field_a': {'type': 'str'}, 'field_b': {'type': 'int'}}})
 
-    assert v.run({'field_a': 123, 'field_b': 1, 'field_c': 123}) == {
-        'fields_set': {'field_b', 'field_a'},
-        'values': {'field_a': '123', 'field_b': 1},
-    }
+    assert v.run({'field_a': 123, 'field_b': 1, 'field_c': 123}) == (
+        {'field_a': '123', 'field_b': 1},
+        {'field_b', 'field_a'},
+    )
 
 
 def test_forbid_extra():
@@ -31,7 +43,4 @@ def test_forbid_extra():
 def test_allow_extra():
     v = SchemaValidator({'type': 'model', 'fields': {'field_a': {'type': 'str'}}, 'config': {'extra': 'allow'}})
 
-    assert v.run({'field_a': 123, 'field_b': (1, 2)}) == {
-        'fields_set': {'field_a', 'field_b'},
-        'values': {'field_a': '123', 'field_b': (1, 2)},
-    }
+    assert v.run({'field_a': 123, 'field_b': (1, 2)}) == ({'field_a': '123', 'field_b': (1, 2)}, {'field_a', 'field_b'})
