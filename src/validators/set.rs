8e2b103c36e8b43b6a8f62248e3a61170e74ab86
Samuel Colvin <s@muelcolvin.com>
2022-04-30 21:19:37 -0600

    Set support (pydantic/pydantic-core#33)
    * Set support, WIP
    
    * implementing tests and starting tests
    
    * tweaking layout
    
    * more tests
    
    * strict tests
    
    * more tests for sets
    
    Original-commit-hash: a227db899d5e8bcb64481d787926c6accae9bd18

---
+++ src/validators/set.rs
@@ -0,0 +1,131 @@
+use pyo3::prelude::*;
+use pyo3::types::{PyDict, PySet};
+
+use crate::build_tools::{is_strict, SchemaDict};
+use crate::errors::{as_internal, context, err_val_error, ErrorKind, InputValue, LocItem, ValError, ValLineError};
+use crate::input::{Input, ListInput};
+
+use super::{build_validator, Extra, ValResult, Validator, ValidatorArc};
+
+#[derive(Debug, Clone)]
+pub struct SetValidator {
+    strict: bool,
+    item_validator: Option<Box<dyn Validator>>,
+    min_items: Option<usize>,
+    max_items: Option<usize>,
+}
+
+impl SetValidator {
+    pub const EXPECTED_TYPE: &'static str = "set";
+}
+
+impl Validator for SetValidator {
+    fn build(schema: &PyDict, config: Option<&PyDict>) -> PyResult<Box<dyn Validator>> {
+        Ok(Box::new(Self {
+            strict: is_strict(schema, config)?,
+            item_validator: match schema.get_item("items") {
+                Some(d) => Some(build_validator(d, config)?.0),
+                None => None,
+            },
+            min_items: schema.get_as("min_items")?,
+            max_items: schema.get_as("max_items")?,
+        }))
+    }
+
+    fn validate<'s, 'data>(
+        &'s self,
+        py: Python<'data>,
+        input: &'data dyn Input,
+        extra: &Extra,
+    ) -> ValResult<'data, PyObject> {
+        let set = match self.strict {
+            true => input.strict_set()?,
+            false => input.lax_set()?,
+        };
+        self._validation_logic(py, input, set, extra)
+    }
+
+    fn validate_strict<'s, 'data>(
+        &'s self,
+        py: Python<'data>,
+        input: &'data dyn Input,
+        extra: &Extra,
+    ) -> ValResult<'data, PyObject> {
+        self._validation_logic(py, input, input.strict_set()?, extra)
+    }
+
+    fn set_ref(&mut self, name: &str, validator_arc: &ValidatorArc) -> PyResult<()> {
+        match self.item_validator {
+            Some(ref mut item_validator) => item_validator.set_ref(name, validator_arc),
+            None => Ok(()),
+        }
+    }
+
+    fn get_name(&self, py: Python) -> String {
+        match &self.item_validator {
+            Some(v) => format!("{}-{}", Self::EXPECTED_TYPE, v.get_name(py)),
+            None => Self::EXPECTED_TYPE.to_string(),
+        }
+    }
+
+    #[no_coverage]
+    fn clone_dyn(&self) -> Box<dyn Validator> {
+        Box::new(self.clone())
+    }
+}
+
+impl SetValidator {
+    fn _validation_logic<'s, 'data>(
+        &'s self,
+        py: Python<'data>,
+        input: &'data dyn Input,
+        set: Box<dyn ListInput<'data> + 'data>,
+        extra: &Extra,
+    ) -> ValResult<'data, PyObject> {
+        let length = set.input_len();
+        if let Some(min_length) = self.min_items {
+            if length < min_length {
+                return err_val_error!(
+                    input_value = InputValue::InputRef(input),
+                    kind = ErrorKind::SetTooShort,
+                    context = context!("min_length" => min_length)
+                );
+            }
+        }
+        if let Some(max_length) = self.max_items {
+            if length > max_length {
+                return err_val_error!(
+                    input_value = InputValue::InputRef(input),
+                    kind = ErrorKind::SetTooLong,
+                    context = context!("max_length" => max_length)
+                );
+            }
+        }
+
+        match self.item_validator {
+            Some(ref validator) => {
+                let mut errors: Vec<ValLineError> = Vec::new();
+                let mut output: Vec<PyObject> = Vec::with_capacity(length);
+                for (index, item) in set.input_iter().enumerate() {
+                    match validator.validate(py, item, extra) {
+                        Ok(item) => output.push(item),
+                        Err(ValError::LineErrors(line_errors)) => {
+                            let loc = vec![LocItem::I(index)];
+                            errors.extend(line_errors.into_iter().map(|err| err.with_prefix_location(&loc)));
+                        }
+                        Err(err) => return Err(err),
+                    };
+                }
+                if errors.is_empty() {
+                    Ok(PySet::new(py, &output).map_err(as_internal)?.into_py(py))
+                } else {
+                    Err(ValError::LineErrors(errors))
+                }
+            }
+            None => {
+                let output: Vec<PyObject> = set.input_iter().map(|item| item.to_py(py)).collect();
+                Ok(PySet::new(py, &output).map_err(as_internal)?.into_py(py))
+            }
+        }
+    }
+}
---
+++ tests/validators/test_set.py
@@ -0,0 +1,153 @@
+import re
+
+import pytest
+
+from pydantic_core import SchemaValidator, ValidationError
+
+from ..conftest import Err
+
+
+@pytest.mark.parametrize(
+    'input_value,expected',
+    [([], set()), ([1, 2, 3], {1, 2, 3}), ([1, 2, '3'], {1, 2, 3}), ([1, 2, 3, 2, 3], {1, 2, 3})],
+)
+def test_set_ints_both(py_or_json, input_value, expected):
+    v = py_or_json({'type': 'set', 'items': {'type': 'int'}})
+    assert v.validate_test(input_value) == expected
+
+
+@pytest.mark.parametrize('input_value,expected', [([1, 2.5, '3'], {1, 2.5, '3'})])
+def test_set_no_validators_both(py_or_json, input_value, expected):
+    v = py_or_json({'type': 'set'})
+    assert v.validate_test(input_value) == expected
+
+
+@pytest.mark.parametrize(
+    'input_value,expected',
+    [
+        ({1, 2, 3}, {1, 2, 3}),
+        (set(), set()),
+        ([1, 2, 3, 2, 3], {1, 2, 3}),
+        ([], set()),
+        ((1, 2, 3, 2, 3), {1, 2, 3}),
+        ((), set()),
+        (frozenset([1, 2, 3, 2, 3]), {1, 2, 3}),
+        ({'abc'}, Err('0\n  Value must be a valid integer')),
+        ({1: 2}, Err('1 validation error for set-int\n  Value must be a valid list/array')),
+        ('abc', Err('Value must be a valid list/array')),
+        # Technically correct, but does anyone actually need this? I think needs a new type in pyo3
+        pytest.param({1: 10, 2: 20, 3: 30}.keys(), {1, 2, 3}, marks=pytest.mark.xfail(raises=ValidationError)),
+    ],
+)
+def test_set_ints_python(input_value, expected):
+    v = SchemaValidator({'type': 'set', 'items': {'type': 'int'}})
+    if isinstance(expected, Err):
+        with pytest.raises(ValidationError, match=re.escape(expected.message)):
+            v.validate_python(input_value)
+    else:
+        assert v.validate_python(input_value) == expected
+
+
+@pytest.mark.parametrize('input_value,expected', [([1, 2.5, '3'], {1, 2.5, '3'}), ([(1, 2), (3, 4)], {(1, 2), (3, 4)})])
+def test_set_no_validators_python(input_value, expected):
+    v = SchemaValidator({'type': 'set'})
+    assert v.validate_python(input_value) == expected
+
+
+def test_set_multiple_errors():
+    v = SchemaValidator({'type': 'set', 'items': {'type': 'int'}})
+    with pytest.raises(ValidationError) as exc_info:
+        v.validate_python(['a', (1, 2), []])
+    assert exc_info.value.errors() == [
+        {
+            'kind': 'int_parsing',
+            'loc': [0],
+            'message': 'Value must be a valid integer, unable to parse string as an integer',
+            'input_value': 'a',
+        },
+        {'kind': 'int_type', 'loc': [1], 'message': 'Value must be a valid integer', 'input_value': (1, 2)},
+        {'kind': 'int_type', 'loc': [2], 'message': 'Value must be a valid integer', 'input_value': []},
+    ]
+
+
+@pytest.mark.parametrize(
+    'kwargs,input_value,expected',
+    [
+        ({'strict': True}, {1, 2, 3}, {1, 2, 3}),
+        ({'strict': True}, set(), set()),
+        ({'strict': True}, [1, 2, 3, 2, 3], Err('Value must be a valid list/array [kind=set_type,')),
+        ({'strict': True}, [], Err('Value must be a valid list/array [kind=set_type,')),
+        ({'strict': True}, (), Err('Value must be a valid list/array [kind=set_type,')),
+        ({'strict': True}, (1, 2, 3), Err('Value must be a valid list/array [kind=set_type,')),
+        ({'strict': True}, frozenset([1, 2, 3]), Err('Value must be a valid list/array [kind=set_type,')),
+        ({'strict': True}, 'abc', Err('Value must be a valid list/array [kind=set_type,')),
+        ({'min_items': 3}, {1, 2, 3}, {1, 2, 3}),
+        ({'min_items': 3}, {1, 2}, Err('Set must have at least 3 items [kind=set_too_short,')),
+        ({'max_items': 3}, {1, 2, 3}, {1, 2, 3}),
+        ({'max_items': 3}, {1, 2, 3, 4}, Err('Set must have at most 3 items [kind=set_too_long,')),
+    ],
+)
+def test_set_kwargs(kwargs, input_value, expected):
+    v = SchemaValidator({'type': 'set', **kwargs})
+    if isinstance(expected, Err):
+        with pytest.raises(ValidationError, match=re.escape(expected.message)):
+            v.validate_python(input_value)
+    else:
+        assert v.validate_python(input_value) == expected
+
+
+@pytest.mark.parametrize('input_value,expected', [({1, 2, 3}, {1, 2, 3}), ([1, 2, 3], [1, 2, 3])])
+def test_union_set_list(input_value, expected):
+    v = SchemaValidator({'type': 'union', 'choices': [{'type': 'set'}, {'type': 'list'}]})
+    if isinstance(expected, Err):
+        with pytest.raises(ValidationError, match=re.escape(expected.message)):
+            v.validate_python(input_value)
+    else:
+        assert v.validate_python(input_value) == expected
+
+
+@pytest.mark.parametrize(
+    'input_value,expected',
+    [
+        ({1, 2, 3}, {1, 2, 3}),
+        ({'a', 'b', 'c'}, {'a', 'b', 'c'}),
+        (
+            [1, 'a'],
+            Err(
+                '2 validation errors for union',
+                errors=[
+                    {
+                        'kind': 'int_type',
+                        'loc': ['set-strict-int', 1],
+                        'message': 'Value must be a valid integer',
+                        'input_value': 'a',
+                    },
+                    # second because validation on the string choice comes second
+                    {
+                        'kind': 'str_type',
+                        'loc': ['set-strict-str', 0],
+                        'message': 'Value must be a valid string',
+                        'input_value': 1,
+                    },
+                ],
+            ),
+        ),
+    ],
+)
+def test_union_set_int_set_str(input_value, expected):
+    v = SchemaValidator(
+        {
+            'type': 'union',
+            'choices': [
+                {'type': 'set', 'items': {'type': 'int', 'strict': True}},
+                {'type': 'set', 'items': {'type': 'str', 'strict': True}},
+            ],
+        }
+    )
+    if isinstance(expected, Err):
+        with pytest.raises(ValidationError, match=re.escape(expected.message)) as exc_info:
+            v.validate_python(input_value)
+        if expected.errors is not None:
+            assert exc_info.value.errors() == expected.errors
+    else:
+        assert v.validate_python(input_value) == expected
