34d2d06c41a3b64249d96ccc333e9680df89ff5e
Samuel Colvin <s@muelcolvin.com>
2022-05-02 21:14:21 -0600

    move all benchmarks, fix 3.7
    Original-commit-hash: 68226ea3b367118e3e6a61218f8fd8f8803e1d44

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,68 +1,80 @@
 import json
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Set
 
 import pytest
-from pydantic.main import BaseModel
 
 from pydantic_core import SchemaValidator
 
+try:
+    from pydantic import BaseModel
+except ImportError:
+    BaseModel = None
+
+
+pytestmark = pytest.mark.skipif(BaseModel is None, reason='pydantic not installed; tests fail on 3.7')
+
 
 class TestBenchmarkSimpleModel:
-    class PydanticModel(BaseModel):
-        name: str
-        age: int
-        friends: List[int]
-        settings: Dict[str, float]
+    @pytest.fixture(scope='class')
+    def pydantic_model(self):
+        class PydanticModel(BaseModel):
+            name: str
+            age: int
+            friends: List[int]
+            settings: Dict[str, float]
+        return PydanticModel
 
-    class CoreModel:
-        __slots__ = '__dict__', '__fields_set__'
+    @pytest.fixture(scope='class')
+    def core_validator(self):
+        class CoreModel:
+            __slots__ = '__dict__', '__fields_set__'
 
-    schema_validator = SchemaValidator(
-        {
-            'type': 'model-class',
-            'class': CoreModel,
-            'model': {
-                'type': 'model',
-                'fields': {
-                    'name': {'type': 'str'},
-                    'age': {'type': 'int'},
-                    'friends': {'type': 'list', 'items': {'type': 'int'}},
-                    'settings': {'type': 'dict', 'keys': {'type': 'str'}, 'values': {'type': 'float'}},
+        return SchemaValidator(
+            {
+                'type': 'model-class',
+                'class': CoreModel,
+                'model': {
+                    'type': 'model',
+                    'fields': {
+                        'name': {'type': 'str'},
+                        'age': {'type': 'int'},
+                        'friends': {'type': 'list', 'items': {'type': 'int'}},
+                        'settings': {'type': 'dict', 'keys': {'type': 'str'}, 'values': {'type': 'float'}},
+                    },
                 },
-            },
-        }
-    )
+            }
+        )
 
     data = {'name': 'John', 'age': 42, 'friends': list(range(200)), 'settings': {f'v_{i}': i / 2.0 for i in range(50)}}
 
     @pytest.mark.benchmark(group='simple model - python')
-    def test_pydantic_python(self, benchmark):
-        benchmark(self.PydanticModel.parse_obj, self.data)
+    def test_pyd_python(self, pydantic_model, benchmark):
+        benchmark(pydantic_model.parse_obj, self.data)
 
     @pytest.mark.benchmark(group='simple model - python')
-    def test_core_python(self, benchmark):
-        benchmark(self.schema_validator.validate_python, self.data)
+    def test_core_python(self, core_validator, benchmark):
+        benchmark(core_validator.validate_python, self.data)
 
     @pytest.mark.benchmark(group='simple model - JSON')
-    def test_pydantic_json(self, benchmark):
+    def test_pyd_json(self, pydantic_model, benchmark):
         json_data = json.dumps(self.data)
 
         @benchmark
         def pydantic_json():
             obj = json.loads(json_data)
-            return self.PydanticModel.parse_obj(obj)
+            return pydantic_model.parse_obj(obj)
 
     @pytest.mark.benchmark(group='simple model - JSON')
-    def test_core_json(self, benchmark):
+    def test_core_json(self, core_validator, benchmark):
         json_data = json.dumps(self.data)
-        benchmark(self.schema_validator.validate_json, json_data)
+        benchmark(core_validator.validate_json, json_data)
 
 
 bool_cases = [True, False, 0, 1, '0', '1', 'true', 'false', 'True', 'False']
 
 
 @pytest.mark.benchmark(group='bool')
-def test_bool_pydantic(benchmark):
+def test_bool_pyd(benchmark):
     class PydanticModel(BaseModel):
         value: bool
 
@@ -86,7 +98,7 @@ small_class_data = {'name': 'John', 'age': 42}
 
 
 @pytest.mark.benchmark(group='create small model')
-def test_small_class_pydantic(benchmark):
+def test_small_class_pyd(benchmark):
     class PydanticModel(BaseModel):
         name: str
         age: int
@@ -132,7 +144,7 @@ def recursive_model_data():
 
 
 @pytest.mark.benchmark(group='recursive model')
-def test_recursive_model_pydantic(recursive_model_data, benchmark):
+def test_recursive_model_pyd(recursive_model_data, benchmark):
     class PydanticBranch(BaseModel):
         width: int
         branch: Optional['PydanticBranch'] = None
@@ -168,3 +180,190 @@ def test_recursive_model_core(recursive_model_data, benchmark):
         }
     )
     benchmark(v.validate_python, recursive_model_data)
+
+
+@pytest.mark.benchmark(group='list of dict models')
+def test_list_of_dict_models_pyd(benchmark):
+    class PydanticBranch(BaseModel):
+        width: int
+
+    class PydanticRoot(BaseModel):
+        __root__: List[PydanticBranch]
+
+    data = [{'width': i} for i in range(100)]
+    benchmark(PydanticRoot.parse_obj, data)
+
+
+@pytest.mark.benchmark(group='list of dict models')
+def test_list_of_dict_models_core(benchmark):
+    v = SchemaValidator(
+        {
+            'type': 'list',
+            'name': 'Branch',
+            'items': {
+                'type': 'model',
+                'fields': {
+                    'width': {'type': 'int'},
+                },
+            },
+        }
+    )
+
+    data = [{'width': i} for i in range(100)]
+    benchmark(v.validate_python, data)
+
+
+list_of_ints_data = (
+    [i for i in range(1000)],
+    [str(i) for i in range(1000)],
+)
+
+
+@pytest.mark.benchmark(group='list of ints')
+def test_list_of_ints_pyd_py(benchmark):
+    class PydanticTree(BaseModel):
+        __root__: List[int]
+
+    @benchmark
+    def t():
+        PydanticTree.parse_obj(list_of_ints_data[0])
+        PydanticTree.parse_obj(list_of_ints_data[1])
+
+
+@pytest.mark.benchmark(group='list of ints')
+def test_list_of_ints_core_py(benchmark):
+    v = SchemaValidator({'type': 'list', 'items': {'type': 'int'}})
+
+    @benchmark
+    def t():
+        v.validate_python(list_of_ints_data[0])
+        v.validate_python(list_of_ints_data[1])
+
+
+@pytest.mark.benchmark(group='list of ints')
+def test_list_of_ints_pyd_json(benchmark):
+    class PydanticTree(BaseModel):
+        __root__: List[int]
+
+    json_data = [json.dumps(d) for d in list_of_ints_data]
+
+    @benchmark
+    def t():
+        PydanticTree.parse_obj(json.loads(json_data[0]))
+        PydanticTree.parse_obj(json.loads(json_data[1]))
+
+
+@pytest.mark.benchmark(group='list of ints')
+def test_list_of_ints_core_json(benchmark):
+    v = SchemaValidator({'type': 'list', 'items': {'type': 'int'}})
+
+    json_data = [json.dumps(d) for d in list_of_ints_data]
+
+    @benchmark
+    def t():
+        v.validate_json(json_data[0])
+        v.validate_json(json_data[1])
+
+
+set_of_ints_data = (
+    {i for i in range(1000)},
+    {str(i) for i in range(1000)},
+)
+
+
+@pytest.mark.benchmark(group='set of ints')
+def test_set_of_ints_pyd(benchmark):
+    class PydanticTree(BaseModel):
+        __root__: Set[int]
+
+    @benchmark
+    def t():
+        PydanticTree.parse_obj(set_of_ints_data[0])
+        PydanticTree.parse_obj(set_of_ints_data[1])
+
+
+@pytest.mark.benchmark(group='set of ints')
+def test_set_of_ints_core(benchmark):
+    v = SchemaValidator({'type': 'set', 'items': {'type': 'int'}})
+
+    @benchmark
+    def t():
+        v.validate_python(set_of_ints_data[0])
+        v.validate_python(set_of_ints_data[1])
+
+
+@pytest.mark.benchmark(group='set of ints')
+def test_set_of_ints_pyd_json(benchmark):
+    class PydanticTree(BaseModel):
+        __root__: Set[int]
+
+    json_data = [json.dumps(list(d)) for d in set_of_ints_data]
+
+    @benchmark
+    def t():
+        PydanticTree.parse_obj(json.loads(json_data[0]))
+        PydanticTree.parse_obj(json.loads(json_data[1]))
+
+
+@pytest.mark.benchmark(group='set of ints')
+def test_set_of_ints_core_json(benchmark):
+    v = SchemaValidator({'type': 'set', 'items': {'type': 'int'}})
+
+    json_data = [json.dumps(list(d)) for d in set_of_ints_data]
+
+    @benchmark
+    def t():
+        v.validate_json(json_data[0])
+        v.validate_json(json_data[1])
+
+
+dict_of_ints_data = (
+    {i: i for i in range(1000)},
+    {i: str(i) for i in range(1000)},
+)
+
+
+@pytest.mark.benchmark(group='dict of ints')
+def test_dict_of_ints_pyd(benchmark):
+    class PydanticTree(BaseModel):
+        __root__: Dict[str, int]
+
+    @benchmark
+    def t():
+        PydanticTree.parse_obj(dict_of_ints_data[0])
+        PydanticTree.parse_obj(dict_of_ints_data[1])
+
+
+@pytest.mark.benchmark(group='dict of ints')
+def test_dict_of_ints_core(benchmark):
+    v = SchemaValidator({'type': 'dict', 'keys': 'str', 'values': 'int'})
+
+    @benchmark
+    def t():
+        v.validate_python(dict_of_ints_data[0])
+        v.validate_python(dict_of_ints_data[1])
+
+
+@pytest.mark.benchmark(group='dict of ints')
+def test_dict_of_ints_pyd_json(benchmark):
+    class PydanticTree(BaseModel):
+        __root__: Dict[str, int]
+
+    json_data = [json.dumps(d) for d in dict_of_ints_data]
+
+    @benchmark
+    def t():
+        PydanticTree.parse_obj(json.loads(json_data[0]))
+        PydanticTree.parse_obj(json.loads(json_data[1]))
+
+
+@pytest.mark.benchmark(group='dict of ints')
+def test_dict_of_ints_core_json(benchmark):
+    v = SchemaValidator({'type': 'dict', 'keys': 'str', 'values': 'int'})
+
+    json_data = [json.dumps(d) for d in dict_of_ints_data]
+
+    @benchmark
+    def t():
+        v.validate_json(json_data[0])
+        v.validate_json(json_data[1])
