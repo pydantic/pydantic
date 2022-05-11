180ee537013d3300a766f70905c6b40f52d7ac7b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-05-11 06:25:50 -0500

    add types (pydantic/pydantic-core#50)
    * add types
    
    * guard versions
    
    * move TypedDict
    
    * remove mypy in favor of pyright, add test for typing
    
    * revert unecessary change
    
    * Update pydantic_core/_types.py
    
    Co-authored-by: Samuel Colvin <samcolvin@gmail.com>
    
    * pr feedback
    
    * fix filename
    
    * Update Makefile
    
    * a few tweaks
    
    * :-( fix tests
    
    * fix pyright complaint with pytest
    
    Co-authored-by: Samuel Colvin <samcolvin@gmail.com>
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>
    
    Original-commit-hash: 6d0da7806d338ed9f2d34561921ed1dc0a1eb67b

--- tests/validators/test_model_class.py
+++ tests/validators/test_model_class.py
@@ -15,7 +15,7 @@ def test_model_class():
     v = SchemaValidator(
         {
             'type': 'model-class',
-            'class': MyModel,
+            'class_type': MyModel,
             'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}, 'field_b': {'type': 'int'}}},
         }
     )
@@ -45,7 +45,11 @@ def test_model_class_setattr():
     setattr_calls.clear()
 
     v = SchemaValidator(
-        {'type': 'model-class', 'class': MyModel, 'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}}}}
+        {
+            'type': 'model-class',
+            'class_type': MyModel,
+            'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}}},
+        }
     )
     m = v.validate_python({'field_a': 'test'})
     assert isinstance(m, MyModel)
@@ -70,7 +74,7 @@ def test_model_class_root_validator():
             'function': f,
             'schema': {
                 'type': 'model-class',
-                'class': MyModel,
+                'class_type': MyModel,
                 'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}}},
             },
         }
@@ -85,12 +89,12 @@ def test_model_class_bad_model():
         pass
 
     with pytest.raises(SchemaError, match=re.escape("model-class expected a 'model' schema, got 'str'")):
-        SchemaValidator({'type': 'model-class', 'class': MyModel, 'model': {'type': 'str'}})
+        SchemaValidator({'type': 'model-class', 'class_type': MyModel, 'model': {'type': 'str'}})
 
 
 def test_model_class_not_type():
     with pytest.raises(SchemaError, match=re.escape("TypeError: 'int' object cannot be converted to 'PyType'")):
-        SchemaValidator({'type': 'model-class', 'class': 123})
+        SchemaValidator({'type': 'model-class', 'class_type': 123})
 
 
 def test_model_class_instance_direct():
@@ -102,7 +106,11 @@ def test_model_class_instance_direct():
             self.field_a = 'init'
 
     v = SchemaValidator(
-        {'type': 'model-class', 'class': MyModel, 'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}}}}
+        {
+            'type': 'model-class',
+            'class_type': MyModel,
+            'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}}},
+        }
     )
     m1 = v.validate_python({'field_a': 'test'})
     assert isinstance(m1, MyModel)
@@ -131,7 +139,11 @@ def test_model_class_instance_subclass():
             self.field_b = 'init_b'
 
     v = SchemaValidator(
-        {'type': 'model-class', 'class': MyModel, 'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}}}}
+        {
+            'type': 'model-class',
+            'class_type': MyModel,
+            'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}}},
+        }
     )
 
     m2 = MySubModel()
@@ -152,7 +164,7 @@ def test_model_class_strict():
         {
             'type': 'model-class',
             'strict': True,
-            'class': MyModel,
+            'class_type': MyModel,
             'model': {'type': 'model', 'fields': {'field_a': {'type': 'str'}, 'field_b': {'type': 'int'}}},
         }
     )
