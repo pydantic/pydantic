16f9679a3c017786959064ee210ecc9fda4e50fa
Eric Jolibois <em.jolibois@gmail.com>
2022-06-22 19:40:10 +0200

    perf: support strict union (pydantic/pydantic-core#101)
    * support strict union
    
    * some stricts have been forgotten
    
    * add test
    
    * add benchmark
    
    * split implem
    
    * Revert "split implem"
    
    This reverts commit 40009427f9ba28094b9be638f9df7c15b26b6925.
    
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/35478b5a3018d1cd6bbe7cc74a850910acf9bf55

--- src/validators/union.rs
+++ src/validators/union.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 use pyo3::types::{PyDict, PyList};
 
-use crate::build_tools::SchemaDict;
+use crate::build_tools::{is_strict, SchemaDict};
 use crate::errors::{LocItem, ValError, ValLineError};
 use crate::input::Input;
 
@@ -10,6 +10,7 @@ use super::{build_validator, BuildContext, BuildValidator, CombinedValidator, Ex
 #[derive(Debug, Clone)]
 pub struct UnionValidator {
     choices: Vec<CombinedValidator>,
+    strict: bool,
 }
 
 impl BuildValidator for UnionValidator {
@@ -25,7 +26,8 @@ impl BuildValidator for UnionValidator {
             .iter()
             .map(|choice| build_validator(choice, config, build_context).map(|result| result.0))
             .collect::<PyResult<Vec<CombinedValidator>>>()?;
-        Ok(Self { choices }.into())
+        let strict = is_strict(schema, config)?;
+        Ok(Self { choices, strict }.into())
     }
 }
 
@@ -37,33 +39,52 @@ impl Validator for UnionValidator {
         extra: &Extra,
         slots: &'data [CombinedValidator],
     ) -> ValResult<'data, PyObject> {
-        // 1st pass: check if the value is an exact instance of one of the Union types
-        if let Some(res) = self
-            .choices
-            .iter()
-            .map(|validator| validator.validate_strict(py, input, extra, slots))
-            .find(ValResult::is_ok)
-        {
-            return res;
+        if self.strict {
+            let mut errors: Vec<ValLineError> = Vec::with_capacity(self.choices.len());
+
+            for validator in &self.choices {
+                let line_errors = match validator.validate_strict(py, input, extra, slots) {
+                    Err(ValError::LineErrors(line_errors)) => line_errors,
+                    otherwise => return otherwise,
+                };
+
+                errors.extend(
+                    line_errors
+                        .into_iter()
+                        .map(|err| err.with_prefix_location(LocItem::S(validator.get_name(py)))),
+                );
+            }
+
+            Err(ValError::LineErrors(errors))
+        } else {
+            // 1st pass: check if the value is an exact instance of one of the Union types
+            if let Some(res) = self
+                .choices
+                .iter()
+                .map(|validator| validator.validate_strict(py, input, extra, slots))
+                .find(ValResult::is_ok)
+            {
+                return res;
+            }
+
+            let mut errors: Vec<ValLineError> = Vec::with_capacity(self.choices.len());
+
+            // 2nd pass: check if the value can be coerced into one of the Union types
+            for validator in &self.choices {
+                let line_errors = match validator.validate(py, input, extra, slots) {
+                    Err(ValError::LineErrors(line_errors)) => line_errors,
+                    otherwise => return otherwise,
+                };
+
+                errors.extend(
+                    line_errors
+                        .into_iter()
+                        .map(|err| err.with_prefix_location(LocItem::S(validator.get_name(py)))),
+                );
+            }
+
+            Err(ValError::LineErrors(errors))
         }
-
-        let mut errors: Vec<ValLineError> = Vec::with_capacity(self.choices.len());
-
-        // 3rd pass: check if the value can be coerced into one of the Union types
-        for validator in &self.choices {
-            let line_errors = match validator.validate(py, input, extra, slots) {
-                Err(ValError::LineErrors(line_errors)) => line_errors,
-                otherwise => return otherwise,
-            };
-
-            errors.extend(
-                line_errors
-                    .into_iter()
-                    .map(|err| err.with_prefix_location(LocItem::S(validator.get_name(py)))),
-            );
-        }
-
-        Err(ValError::LineErrors(errors))
     }
 
     fn get_name(&self, py: Python) -> String {
