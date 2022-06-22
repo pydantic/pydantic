ae1acfee587375a45b9d973d6515abe85ddb903f
Eric Jolibois <em.jolibois@gmail.com>
2022-06-22 19:40:10 +0200

    perf: support strict union (pydantic/pydantic-core#101)
    * support strict union
    
    * some stricts have been forgotten
    
    * add test
    
    * add benchmark
    
    * split implem
    
    * Revert "split implem"
    
    This reverts commit 40009427f9ba28094b9be638f9df7c15b26b6925.
    
    Original-commit-hash: 35478b5a3018d1cd6bbe7cc74a850910acf9bf55

--- tests/validators/test_union.py
+++ tests/validators/test_union.py
@@ -215,3 +215,17 @@ def test_no_choices():
         SchemaValidator({'type': 'union'})
 
     assert exc_info.value.args[0] == ('Error building "union" validator:\n' '  KeyError: \'"choices" is required\'')
+
+
+def test_strict_union():
+    v = SchemaValidator({'type': 'union', 'strict': True, 'choices': [{'type': 'bool'}, {'type': 'int'}]})
+    assert v.validate_python(1) == 1
+    assert v.validate_python(123) == 123
+
+    with pytest.raises(ValidationError) as exc_info:
+        v.validate_python('123') == 123
+
+    assert exc_info.value.errors() == [
+        {'kind': 'bool_type', 'loc': ['bool'], 'message': 'Value must be a valid boolean', 'input_value': '123'},
+        {'kind': 'int_type', 'loc': ['int'], 'message': 'Value must be a valid integer', 'input_value': '123'},
+    ]
