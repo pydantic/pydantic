acd106c65833d74e885c619ce72e234cf0659ef7
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-11 10:54:09 -0700

    Use new values in known field validators

--- pydantic/main.py
+++ pydantic/main.py
@@ -363,7 +363,6 @@ class BaseModel(Representation, metaclass=ModelMetaclass):
             for validator in self.__pre_root_validators__:
                 try:
                     new_values = validator(self.__class__, new_values)
-                    value = new_values[name]
                 except (ValueError, TypeError, AssertionError) as exc:
                     raise ValidationError([ErrorWrapper(exc, loc=ROOT_KEY)], self.__class__)
 
@@ -376,7 +375,7 @@ class BaseModel(Representation, metaclass=ModelMetaclass):
                 if not known_field.field_info.allow_mutation:
                     raise TypeError(f'"{known_field.name}" has allow_mutation set to False and cannot be assigned')
                 dict_without_original_value = {k: v for k, v in self.__dict__.items() if k != name}
-                value, error_ = known_field.validate(value, dict_without_original_value, loc=name, cls=self.__class__)
+                value, error_ = known_field.validate(new_values[name], dict_without_original_value, loc=name, cls=self.__class__)
                 if error_:
                     raise ValidationError([error_], self.__class__)
                 else:
