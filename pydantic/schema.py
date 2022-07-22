4b6fc61719db1fdf867474ee68878ad0a791cd70
Eric Jolibois <em.jolibois@gmail.com>
2022-07-22 19:48:15 +0200

    Handle enum in schemas
    Enums have now a (huge) doc generated by inspect.getdoc.
    It's very verbose and doesn't add anything.
    I reckon it's best to keep the old generic description unless
    a specific doc is provided (a test exists for this)

--- pydantic/schema.py
+++ pydantic/schema.py
@@ -667,13 +667,11 @@ def enum_process_schema(enum: Type[Enum], *, field: Optional[ModelField] = None)
 
     This is similar to the `model_process_schema` function, but applies to ``Enum`` objects.
     """
-    from inspect import getdoc
-
     schema_: Dict[str, Any] = {
         'title': enum.__name__,
         # Python assigns all enums a default docstring value of 'An enumeration', so
         # all enums will have a description field even if not explicitly provided.
-        'description': getdoc(enum),
+        'description': enum.__doc__ or 'An enumeration.',
         # Add enum values and the enum field type to the schema.
         'enum': [item.value for item in cast(Iterable[Enum], enum)],
     }
