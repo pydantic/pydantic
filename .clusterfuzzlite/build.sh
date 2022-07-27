dcba130312a5c9a7d3cd73e970eb68415bb76eaf
AdamKorcz <Adam@adalogics.com>
2022-07-27 16:10:21 +0100

    fix dependency issue

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -3,4 +3,5 @@ cp $SRC/parse.py $SRC/pydantic/
 pip3 install --upgrade pip
 pip3 install .
 
-compile_python_fuzzer pydantic/fuzz_parse.py
+cp pydantic/fuzz_parse.py $SRC/
+compile_python_fuzzer $SRC/fuzz_parse.py
