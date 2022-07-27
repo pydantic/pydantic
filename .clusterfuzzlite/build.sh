2e536ce9187006c1a2fc3734b727bfaef8b352fb
AdamKorcz <Adam@adalogics.com>
2022-07-27 16:12:26 +0100

    move fuzzer to tests

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,6 +2,6 @@ cp $SRC/parse.py $SRC/pydantic/
 # Build and install project (using current CFLAGS, CXXFLAGS).
 pip3 install --upgrade pip
 pip3 install .
-
-cp pydantic/fuzz_parse.py $SRC/
-compile_python_fuzzer $SRC/fuzz_parse.py
+for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
+  compile_python_fuzzer $fuzzer
+done
