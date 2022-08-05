d0e5f98be418ed648d0d8e4e6989de548b3c26d1
PattyC <schlerp@users.noreply.github.com>
2022-08-05 23:48:19 +0930

    new `to_lower_camel()` function (#3473)
    * Create utils.py
    
    I needed non pascal case camel case support
    
    * write tests for new `to_lower_camel()` function
    
    * Create 3463-schlerp.md
    
    * added mention to to_lower_camel()
    
    * changed quotes to single tick
    
    * adding second blank line at end of file.
    
    * again, adding second blank line...
    
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>

--- pydantic/utils.py
+++ pydantic/utils.py
@@ -303,6 +303,13 @@ def to_camel(string: str) -> str:
     return ''.join(word.capitalize() for word in string.split('_'))
 
 
+def to_lower_camel(string: str) -> str:
+    if len(string) >= 1:
+        pascal_string = to_camel(string)
+        return pascal_string[0].lower() + pascal_string[1:]
+    return string.lower()
+
+
 T = TypeVar('T')
 
 
