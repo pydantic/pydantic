8b03070a187e41e249e2dd8f379c9b2a02ce85e4
MrMrRobat <appkiller16@gmail.com>
2022-08-07 04:59:19 +0400

    Refactor and improve imlementetion

--- docs/build/exec_examples.py
+++ docs/build/exec_examples.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 import importlib
 import inspect
 import json
@@ -10,7 +11,7 @@ import sys
 import textwrap
 import traceback
 from pathlib import Path
-from typing import Any, List, Tuple
+from typing import Any, Callable
 from unittest.mock import patch
 
 from ansi2html import Ansi2HTMLConverter
@@ -20,9 +21,29 @@ THIS_DIR = Path(__file__).parent
 DOCS_DIR = (THIS_DIR / '..').resolve()
 EXAMPLES_DIR = DOCS_DIR / 'examples'
 TMP_EXAMPLES_DIR = DOCS_DIR / '.tmp_examples'
+UPGRADED_TMP_EXAMPLES_DIR = TMP_EXAMPLES_DIR / "upgraded"
+
 MAX_LINE_LENGTH = int(re.search(r'max_line_length = (\d+)', (EXAMPLES_DIR / '.editorconfig').read_text()).group(1))
 LONG_LINE = 50
+LOWEST_VERSION = (3, 7)
+HIGHEST_VERSION = (3, 10)
 pformat = PrettyFormat(simple_cutoff=LONG_LINE)
+Error = Callable[..., None]
+Version = tuple[int, int]
+
+PYTHON_CODE_MD_TMPL = """
+=== "Python {version} and above"    
+
+    ```py
+{code}
+    ```
+""".strip()
+JSON_OUTPUT_MD_TMPL = """
+Outputs:
+```json
+{output}
+```
+""".strip()
 
 
 def to_string(value: Any) -> str:
@@ -46,29 +67,28 @@ def to_string(value: Any) -> str:
 
 
 class MockPrint:
-    def __init__(self, file: Path):
+    def __init__(self, file: Path) -> None:
         self.file = file
-        self.statements = []
+        self.statements: list[tuple[int, str]] = []
 
-    def __call__(self, *args, file=None, flush=None):
+    def __call__(self, *args: Any, sep: str = ' ', **kwargs) -> None:
         frame = inspect.currentframe().f_back.f_back.f_back
         if sys.version_info >= (3, 8):
             frame = frame.f_back
         if not self.file.samefile(frame.f_code.co_filename):
             # happens when index_error.py imports index_main.py
             return
-        s = ' '.join(map(to_string, args))
+        s = sep.join(map(to_string, args))
 
         self.statements.append((frame.f_lineno, s))
 
 
 class MockPath:
-
-    def read_text(self, *args, **kwargs) -> str:
+    def read_text(self, *args: Any, **kwargs: Any) -> str:
         return '{"foobar": "spam"}'
 
 
-def build_print_lines(s: str, max_len_reduction: int = 0):
+def build_print_lines(s: str, max_len_reduction: int = 0) -> list[str]:
     print_lines = []
     max_len = MAX_LINE_LENGTH - 3 - max_len_reduction
     for line in s.split('\n'):
@@ -79,7 +99,7 @@ def build_print_lines(s: str, max_len_reduction: int = 0):
     return print_lines
 
 
-def build_print_statement(line_no: int, s: str, lines: List[str]) -> None:
+def build_print_statement(line_no: int, s: str, lines: list[str]) -> None:
     indent = ''
     for back in range(1, 100):
         m = re.search(r'^( *)print\(', lines[line_no - back])
@@ -102,7 +122,7 @@ def all_md_contents() -> str:
     return '\n\n\n'.join(file_contents)
 
 
-def gen_ansi_output():
+def gen_ansi_output() -> None:
 
     conv = Ansi2HTMLConverter()
 
@@ -121,31 +141,32 @@ dont_upgrade_re = re.compile(r'^# dont-upgrade\n', flags=re.M | re.I)
 required_py_re = re.compile(r'^# *requires *python *(\d+).(\d+)', flags=re.M)
 
 
-def should_execute(file_name: str, file_text: str) -> Tuple[str, bool, Tuple[str, str]]:
+def should_execute(file_name: str, file_text: str) -> tuple[str, bool, Version]:
     m = required_py_re.search(file_text)
     if m:
-        if sys.version_info >= tuple(map(int, m.groups())):
-            return required_py_re.sub('', file_text), True, m.groups()
+        lowest_version = (int(m.groups()[0]), int(m.groups()[1]))
+        if sys.version_info >= lowest_version:
+            return required_py_re.sub('', file_text), True, lowest_version
         else:
             v = '.'.join(m.groups())
             print(f'WARNING: {file_name} requires python {v}, not running')
             return (
                 required_py_re.sub(f'# requires python {v}, NOT EXECUTED!', file_text),
                 False,
-                m.groups(),
+                lowest_version,
             )
     elif dont_execute_re.search(file_text):
-        return dont_execute_re.sub('', file_text), False, ("3", "7")
-    return file_text, True, ("3", "7")
+        return dont_execute_re.sub('', file_text), False, LOWEST_VERSION
+    return file_text, True, LOWEST_VERSION
 
 
-def should_upgrade(file_text: str) -> Tuple[str, bool]:
+def should_upgrade(file_text: str) -> tuple[str, bool]:
     if dont_upgrade_re.search(file_text):
         return dont_upgrade_re.sub('', file_text), False
     return file_text, True
 
 
-def exec_file(file, file_text, error, new_files):
+def exec_file(file: Path, file_text: str, error: Error) -> tuple[list[str], str | None]:
     no_print_intercept_re = re.compile(r'^# no-print-intercept\n', flags=re.M)
     print_intercept = not bool(no_print_intercept_re.search(file_text))
     if not print_intercept:
@@ -156,27 +177,17 @@ def exec_file(file, file_text, error, new_files):
     mp = MockPrint(file)
     mod = None
 
-    with patch.object(Path, "read_text", MockPath.read_text), patch(
-        'builtins.print'
-    ) as patch_print:
+    with patch.object(Path, "read_text", MockPath.read_text), patch('builtins.print') as patch_print:
         if print_intercept:
             patch_print.side_effect = mp
         try:
             mod = importlib.import_module(file.stem)
         except Exception:
             tb = traceback.format_exception(*sys.exc_info())
-            error(
-                ''.join(
-                    e
-                    for e in tb
-                    if '/pydantic/docs/examples/' in e or not e.startswith('  File ')
-                )
-            )
+            error(''.join(e for e in tb if '/pydantic/docs/examples/' in e or not e.startswith('  File ')))
 
-    if mod and Path(mod.__file__) != file:
-        error(
-            f'module path "{mod.__file__}" is not same as "{file}", name may shadow another module?'
-        )
+    if mod and mod.__file__ != str(file):
+        error(f'module path "{mod.__file__}" is not same as "{file}", name may shadow another module?')
 
     lines = file_text.split('\n')
 
@@ -193,7 +204,7 @@ def exec_file(file, file_text, error, new_files):
         return lines, None
 
 
-def filter_lines(lines: List[str], error: Any) -> Tuple[List[str], bool]:
+def filter_lines(lines: list[str], error: Any) -> tuple[list[str], bool]:
     changed = False
     try:
         ignore_above = lines.index('# ignore-above')
@@ -217,25 +228,67 @@ def filter_lines(lines: List[str], error: Any) -> Tuple[List[str], bool]:
     return lines, changed
 
 
-def upgrade_code(content: str, min_version=(3, 10)) -> str:
-    from pyupgrade._main import Settings, _fix_plugins
-    from autoflake import fix_code
+def upgrade_code(content: str, min_version: Version = HIGHEST_VERSION) -> str:
+    import pyupgrade._main
+    import autoflake
 
-    upgraded = _fix_plugins(
+    upgraded = pyupgrade._main._fix_plugins(
         content,
-        settings=Settings(
+        settings=pyupgrade._main.Settings(
             min_version=min_version,
             keep_percent_format=True,
             keep_mock=False,
             keep_runtime_typing=False,
         ),
     )
-
-    upgraded = fix_code(upgraded, remove_all_unused_imports=True)
+    upgraded = autoflake.fix_code(upgraded, remove_all_unused_imports=True)
     return upgraded
 
 
-def exec_examples():
+def ensure_used(file: Path, all_md: str, error: Error) -> None:
+    """Ensures that example is used appropriately"""
+    file_tmpl = '{{!.tmp_examples/{}!}}'
+    md_name = file.stem + '.md'
+    if file_tmpl.format(md_name) not in all_md:
+        if file_tmpl.format(file.name) in all_md:
+            error(
+                f'incorrect usage, change filename to {md_name!r} in docs.'
+                "make sure you don't specify ```py code blocks around examples,"
+                "they are automatically generated now."
+            )
+        else:
+            error(
+                'file not used anywhere. correct usage:',
+                file_tmpl.format(md_name),
+            )
+
+
+def check_style(file_text: str, error: Error) -> None:
+    if '\n\n\n\n' in file_text:
+        error('too many new lines')
+    if not file_text.endswith('\n'):
+        error('no trailing new line')
+    if re.search('^ *# *>', file_text, flags=re.M):
+        error('contains comments with print output, please remove')
+
+
+def populate_upgraded_versions(file: Path, file_text: str, lowest_version: Version) -> list[tuple[Path, str, Version]]:
+    versions = []
+    major, minor = lowest_version
+    assert major == HIGHEST_VERSION[0], "Wow, Python 4 is out? Congrats!"
+    upgraded_file_text = file_text
+    while minor < HIGHEST_VERSION[1]:
+        minor += 1
+        new_file_text = upgrade_code(file_text, min_version=(major, minor))
+        if upgraded_file_text != new_file_text:
+            upgraded_file_text = new_file_text
+            new_file = UPGRADED_TMP_EXAMPLES_DIR / (file.stem + f"_{major}_{minor}" + file.suffix)
+            new_file.write_text(upgraded_file_text)
+            versions.append((new_file, upgraded_file_text, (major, minor)))
+    return versions
+
+
+def exec_examples() -> int:
     errors = []
     all_md = all_md_contents()
     new_files = {}
@@ -251,24 +304,21 @@ def exec_examples():
             'sub_model__deep__v4': 'v4',
         }
     )
-    if sys.version_info < (3, 10):
-        execute_latest_version = False
-        print(
-            "WARNING: examples for 3.10+ requires python 3.10. They won't be executed"
-        )
-    else:
-        example_3_10_dir = TMP_EXAMPLES_DIR / "3.10"
-        example_3_10_dir.mkdir(parents=True, exist_ok=True)
-        sys.path.append(str(example_3_10_dir))
-        execute_latest_version = True
-
     sys.path.append(str(EXAMPLES_DIR))
-    for file in sorted(EXAMPLES_DIR.iterdir()):
+    if sys.version_info < HIGHEST_VERSION:
+        print("WARNING: examples for 3.10+ requires python 3.10. They won't be executed")
+    else:
+        UPGRADED_TMP_EXAMPLES_DIR.mkdir(parents=True, exist_ok=True)
+        sys.path.append(str(UPGRADED_TMP_EXAMPLES_DIR))
 
-        def error(*desc: str):
+    for file in sorted(EXAMPLES_DIR.iterdir()):
+        markdown_name = file.stem + '.md'
+
+        def error(*desc: str) -> None:
             errors.append((file, desc))
-            previous_frame = sys._getframe(-2).f_back
-            location = f'{Path(previous_frame.f_globals["__file__"]).relative_to(Path.cwd())}:{previous_frame.f_lineno}'
+            previous_frame = sys._getframe(-3)
+            filename = Path(previous_frame.f_globals["__file__"]).relative_to(Path.cwd())
+            location = f'{filename}:{previous_frame.f_lineno}'
             sys.stderr.write(f'{location}: error in {file.name}: {" ".join(desc)}\n')
 
         if not file.is_file():
@@ -280,96 +330,52 @@ def exec_examples():
             new_files[file.name] = file.read_text()
             continue
 
-        file_tmpl = '{{!.tmp_examples/{}!}}'
-        if file_tmpl.format(file.stem + '.md') not in all_md:
-            if file_tmpl.format(file.name) in all_md:
-                error(
-                    f'incorrect usage, change filename to {file.stem + ".md"!r} in docs'
-                )
-            else:
-                error(
-                    'file not used anywhere. correct usage:',
-                    file_tmpl.format(file.stem + 'md'),
-                )
-
         file_text = file.read_text('utf-8')
-        if '\n\n\n\n' in file_text:
-            error('too many new lines')
-        if not file_text.endswith('\n'):
-            error('no trailing new line')
-        if re.search('^ *# *>', file_text, flags=re.M):
-            error('contains comments with print output, please remove')
+        ensure_used(file, all_md, error)
+        check_style(file_text, error)
 
         file_text, execute, lowest_version = should_execute(file.name, file_text)
         file_text, upgrade = should_upgrade(file_text)
-        if upgrade and upgrade_code(file_text, min_version=(3, 7)) != file_text:
-            error(
-                "pyupgrade would upgrade file. If it's not desired, add '# dont-upgrade' line at the top of the file"
+
+        if upgrade and upgrade_code(file_text, min_version=lowest_version) != file_text:
+            error("pyupgrade would upgrade file. If it's not desired, add '# dont-upgrade' line at the top of the file")
+
+        versions: list[tuple[Path, str, Version]] = [(file, file_text, lowest_version)]
+        if upgrade:
+            versions.extend(populate_upgraded_versions(file, file_text, lowest_version))
+
+        json_outputs: set[str | None] = set()
+        should_run_as_is = True
+        final_content: list[str] = []
+        for file, file_text, lowest_version in versions:
+            if execute and sys.version_info >= lowest_version:
+                lines, json_output = exec_file(file, file_text, error)
+                json_outputs.add(json_output)
+            else:
+                lines = file_text.split('\n')
+
+            lines, changed = filter_lines(lines, error)
+            should_run_as_is = not changed
+
+            final_content.append(
+                PYTHON_CODE_MD_TMPL.format(
+                    version='.'.join(map(str, lowest_version)),
+                    code=textwrap.indent('\n'.join(lines), "    "),
+                )
             )
 
-        if upgrade and tuple(map(int, lowest_version)) < (3, 10):
-            upgraded = upgrade_code(file_text)
-            upgraded_lines = upgraded.split('\n')
-        else:
-            upgraded = None
-            upgraded_lines = []
-            upgraded_json_output = None
+        if should_run_as_is:
+            final_content.append('_(This script is complete, it should run "as is")_')
 
-        if execute:
-            lines, json_output = exec_file(file, file_text, error, new_files)
-            if upgraded and execute_latest_version:
-                # create temp module to execute
-                new_file = example_3_10_dir / (file.stem + "3_10" + file.suffix)
-                new_file.write_text(upgraded)
-                upgraded_lines, upgraded_json_output = exec_file(
-                    new_file, upgraded, error, new_files
-                )
-            else:
-                upgraded_json_output = None
-        else:
-            upgraded_json_output = None
-            lines = file_text.split('\n')
-
-        if json_output != upgraded_json_output:
+        if len(json_outputs) > 1:
             error('json output should not differ between versions')
 
-        lines, changed = filter_lines(lines, error)
-        upgraded_lines, _ = filter_lines(upgraded_lines, error)
+        if json_outputs:
+            json_output, *_ = json_outputs
+            if json_output:
+                final_content.append(JSON_OUTPUT_MD_TMPL.format(output=json_output))
 
-        new_content = '\n'.join(lines)
-
-        final_content = f"""
-=== "Python {'.'.join(lowest_version)} and above"    
-    ```py
-{textwrap.indent(new_content, "    ")}
-    ```
-"""
-        if upgraded_lines and upgraded_lines != lines:
-            upgraded_content = '\n'.join(upgraded_lines)
-            final_content += f"""
-=== "Python 3.10 and above"
-
-    ```py
-{textwrap.indent(upgraded_content, "    ")}
-    ```
-"""
-            if not changed:
-                final_content += (
-                    '_(These scripts are complete, it should run "as is")_\n'
-                )
-        else:
-            if not changed:
-                final_content += '_(This script is complete, it should run "as is")_\n'
-
-        if json_output:
-            final_content += f"""
-Outputs:
-```json
-{json_output}
-```
-"""
-
-        new_files[file.stem + '.md'] = final_content
+        new_files[markdown_name] = "\n".join(final_content)
 
     if errors:
         print(f'\n{len(errors)} errors, not writing files\n')
