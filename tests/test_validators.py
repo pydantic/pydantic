177ca2a6b21d4c333944ba0204a4efa37beff779
Michael <SunsetOrange@users.noreply.github.com>
2022-08-08 22:34:45 +1000

    Validator decorator field check. Closes #3215 (#3216)
    * Descriptive exception for incorrectly defined validator fields.
    
    Throws a ConfigError wth helpful feedback if a validator is passed incorrectly defined fields.
    
    * Minor grammar correction.
    
    * Expanded example.
    
    * Added test for bad validator decorator fields.
    
    * Added changes .md file.
    
    * Improved wording in error message.
    
    * Test fix.
    
    * Update pydantic/class_validators.py
    
    Encloses expression into a list comprehension.
    
    Co-authored-by: Eric Jolibois <em.jolibois@gmail.com>
    
    * Update pydantic/class_validators.py
    
    Removed some exception verbosity.
    
    Co-authored-by: Eric Jolibois <em.jolibois@gmail.com>
    
    * Update changes/3215-SunsetOrange.md
    
    Removed some changes verbosity.
    
    Co-authored-by: Eric Jolibois <em.jolibois@gmail.com>
    
    * use pytest.raises(.., match=...)
    
    Co-authored-by: Michael <michael.bedford@superloop.com>
    Co-authored-by: Eric Jolibois <em.jolibois@gmail.com>
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -670,6 +670,22 @@ def test_validator_always_post_optional():
     assert Model().a == 'default value'
 
 
+def test_validator_bad_fields_throws_configerror():
+    """
+    Attempts to create a validator with fields set as a list of strings,
+    rather than just multiple string args. Expects ConfigError to be raised.
+    """
+    with pytest.raises(ConfigError, match='validator fields should be passed as separate string args.'):
+
+        class Model(BaseModel):
+            a: str
+            b: str
+
+            @validator(['a', 'b'])
+            def check_fields(cls, v):
+                return v
+
+
 def test_datetime_validator():
     check_calls = 0
 
