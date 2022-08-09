2ac817e71c75e3c0ba06e83cec9068df75c76265
dosisod <39638017+dosisod@users.noreply.github.com>
2022-08-08 21:03:15 -0700

    Fix docs

--- docs/examples/models_orm_mode_kwargs.py
+++ docs/examples/models_orm_mode_kwargs.py
@@ -18,12 +18,13 @@ BaseModel = declarative_base()
 class SQLModel(BaseModel):
     __tablename__ = 'my_table'
     id = sa.Column('id', sa.Integer, primary_key=True)
-    foo = sa.Column('metadata', sa.String(32))
-    bar = sa.Column('metadata', sa.Integer)
+    foo = sa.Column('foo', sa.String(32))
+    bar = sa.Column('bar', sa.Integer)
 
-sql_model = SQLModel(id=1, foo="hello world", bar=123)
 
-pydantic_model = MyModel.from_orm(sql_model, bar=456, spam=b"placeholder")
+sql_model = SQLModel(id=1, foo='hello world', bar=123)
+
+pydantic_model = MyModel.from_orm(sql_model, bar=456, spam=b'placeholder')
 
 print(pydantic_model.dict())
 print(pydantic_model.dict(by_alias=True))
