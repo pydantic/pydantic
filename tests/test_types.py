67b653f19a00b60e410003bca428bb20b920ec36
Jean <jean.arhancetebehere@gmail.com>
2022-08-16 23:41:17 +0200

    fix: strictbytes-max-length (#4383)
    * fix: strictbytes-max-length
    
    * fix: apply feedbacks
    
    * Update changes/4380-JeanArhancet.md
    
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>
    
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>

--- tests/test_types.py
+++ tests/test_types.py
@@ -1634,6 +1634,18 @@ def test_strict_bytes():
         Model(v=0.42)
 
 
+def test_strict_bytes_max_length():
+    class Model(BaseModel):
+        u: StrictBytes = Field(..., max_length=5)
+
+    assert Model(u=b'foo').u == b'foo'
+
+    with pytest.raises(ValidationError, match='byte type expected'):
+        Model(u=123)
+    with pytest.raises(ValidationError, match='ensure this value has at most 5 characters'):
+        Model(u=b'1234567')
+
+
 def test_strict_bytes_subclass():
     class MyStrictBytes(StrictBytes):
         pass
