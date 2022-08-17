fd122d14db228c62f95e8bd3e1056b92a335f68f
Fares Ahmed <fares@duck.com>
2022-08-17 22:49:27 +0200

    Fix formatting for `docs/examples/types_url_building.py`

--- docs/examples/types_url_building.py
+++ docs/examples/types_url_building.py
@@ -4,5 +4,7 @@ url = AnyUrl.build(scheme='https', host='example.com', query='query=my query')
 print(url)
 
 my_url_builder = stricturl(quote_plus=True)
-url = my_url_builder.build(scheme='https', host='example.com', query='query=my query')
+url = my_url_builder.build(
+    scheme='http', host='example.com', query='query=my query'
+)
 print(url)
