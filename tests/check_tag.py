01f01fc15d210a48267dd268dec88c677617b20f
Samuel Colvin <s@muelcolvin.com>
2022-08-24 12:19:18 +0100

    avoid docs update on pre-release

--- tests/check_tag.py
+++ tests/check_tag.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
+import json
 import os
 import re
 import sys
+from importlib.machinery import SourceFileLoader
 
-from pydantic.version import VERSION
+from packaging.version import parse
 
 
 def main(env_var='GITHUB_REF') -> int:
     git_ref = os.getenv(env_var, 'none')
     tag = re.sub('^refs/tags/v*', '', git_ref.lower())
-    version = VERSION.lower()
+    version = SourceFileLoader('version', 'pydantic/version.py').load_module().VERSION.lower()
     if tag == version:
-        print(f'✓ {env_var} env var {git_ref!r} matches package version: {tag!r} == {version!r}')
+        is_prerelease = parse(version).is_prerelease
+        print(
+            f'✓ {env_var} env var {git_ref!r} matches package version: {tag!r} == {version!r}, '
+            f'is pre-release: {is_prerelease}'
+        )
+        print(f'::set-output name=IS_PRERELEASE::{json.dumps(is_prerelease)}')
         return 0
     else:
         print(f'✖ {env_var} env var {git_ref!r} does not match package version: {tag!r} != {version!r}')
