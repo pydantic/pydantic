eccd85e4d012e70ffbd81f379179da900d4621c5
Samuel Colvin <s@muelcolvin.com>
2022-09-05 12:35:08 +0100

    prevent long strings as int inputs (#4480)
    * prevent long strings as int inputs, fix #1477
    
    * fix tests :facepalm:
    
    * fix length in change file

--- pydantic/validators.py
+++ pydantic/validators.py
@@ -120,9 +120,22 @@ def bool_validator(v: Any) -> bool:
     raise errors.BoolError()
 
 
+# matches the default limit cpython, see https://github.com/python/cpython/pull/96500
+max_str_int = 4_300
+
+
 def int_validator(v: Any) -> int:
     if isinstance(v, int) and not (v is True or v is False):
         return v
+
+    # see https://github.com/pydantic/pydantic/issues/1477 and in turn, https://github.com/python/cpython/issues/95778
+    # this check should be unnecessary once patch releases are out for 3.7, 3.8, 3.9 and 3.10
+    # but better to check here until then.
+    # NOTICE: this does not fully protect user from the DOS risk since the standard library JSON implementation
+    # (and other std lib modules like xml) use `int()` and are likely called before this, the best workaround is to
+    # 1. update to the latest patch release of python once released, 2. use a different JSON library like ujson
+    if isinstance(v, (str, bytes, bytearray)) and len(v) > max_str_int:
+        raise errors.IntegerError()
 
     try:
         return int(v)
