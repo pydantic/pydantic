d3330677d07623f072bb909a70a343354adbf321
Samuel Colvin <s@muelcolvin.com>
2022-10-14 09:46:34 +0100

    follow up on #290

--- tests/validators/test_custom_error.py
+++ tests/validators/test_custom_error.py
@@ -2,7 +2,7 @@ import pytest
 
 from pydantic_core import SchemaError, SchemaValidator, ValidationError, core_schema
 
-from ..conftest import PyAndJson
+from ..conftest import PyAndJson, plain_repr
 
 
 def test_custom_error(py_and_json: PyAndJson):
@@ -44,3 +44,40 @@ def test_custom_error_invalid():
         SchemaValidator(
             core_schema.custom_error_schema(core_schema.int_schema(), 'recursion_loop', custom_error_message='xxx')
         )
+
+
+def test_ask():
+    class MyModel:
+        # this is not required, but it avoids `__fields_set__` being included in `__dict__`
+        __slots__ = '__dict__', '__fields_set__'
+        field_a: str
+        field_b: int
+
+    v = SchemaValidator(
+        {
+            'type': 'new-class',
+            'cls': MyModel,
+            'schema': {
+                'type': 'custom_error',
+                'custom_error_kind': 'foobar',
+                'custom_error_message': 'Hello there',
+                'schema': {
+                    'type': 'typed-dict',
+                    'return_fields_set': True,
+                    'fields': {'field_a': {'schema': {'type': 'str'}}, 'field_b': {'schema': {'type': 'int'}}},
+                },
+            },
+        }
+    )
+    assert 'expect_fields_set:true' in plain_repr(v)
+    m = v.validate_python({'field_a': 'test', 'field_b': 12})
+    assert isinstance(m, MyModel)
+    assert m.field_a == 'test'
+    assert m.field_b == 12
+    assert m.__fields_set__ == {'field_a', 'field_b'}
+    with pytest.raises(ValidationError) as exc_info:
+        v.validate_python({'field_a': 'test'})
+    # insert_assert(exc_info.value.errors())
+    assert exc_info.value.errors() == [
+        {'kind': 'foobar', 'loc': [], 'message': 'Hello there', 'input_value': {'field_a': 'test'}}
+    ]
