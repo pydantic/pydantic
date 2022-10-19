41c0530e37ba9d1f1062c0785d7916d4da77180f
Samuel Colvin <s@muelcolvin.com>
2022-10-19 09:14:56 +0100

    fix linting

--- pydantic/main.py
+++ pydantic/main.py
@@ -27,9 +27,8 @@ from typing_extensions import dataclass_transform
 
 from ._internal._model_construction import complete_model_class, inspect_namespace
 from ._internal._typing_extra import is_namedtuple
-from ._internal._validation_functions import ValidationFunctions
 from ._internal._utils import GetterDict, Representation, ValueItems, is_valid_field, sequence_like
-
+from ._internal._validation_functions import ValidationFunctions
 from .config import BaseConfig, Extra, build_config, inherit_config
 from .errors import ConfigError, DictError
 from .fields import Field, FieldInfo, ModelPrivateAttr, Undefined
