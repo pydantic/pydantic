fa0462cbe217c083f5cf718acf8d93b91792540f
Samuel Colvin <s@muelcolvin.com>
2022-10-20 16:59:25 +0100

    test is_union

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,10 +1,12 @@
+import typing
 from collections import namedtuple
 from typing import Callable, NamedTuple
 
-from typing_extensions import Literal
+import pytest
+from typing_extensions import Literal, get_origin
 
 from pydantic import Field  # noqa: F401
-from pydantic._internal._typing_extra import is_namedtuple, is_none_type
+from pydantic._internal._typing_extra import is_namedtuple, is_none_type, origin_is_union
 
 try:
     from typing import TypedDict as typing_TypedDict
@@ -51,3 +53,14 @@ def test_is_none_type():
     # `collections.abc.Callable` (even with python >= 3.9) as they behave
     # differently
     assert is_none_type(Callable) is False
+
+
+@pytest.mark.parametrize('union_gen', [lambda: typing.Union[int, str], lambda: int | str])
+def test_is_union(union_gen):
+    try:
+        union = union_gen()
+    except TypeError:
+        pytest.skip('not supported in this python version')
+    else:
+        origin = get_origin(union)
+        assert origin_is_union(origin)
