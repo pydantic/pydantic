718f0fe06d19dab9f7530f577c3b7b10100e7425
Mix <32300164+mnixry@users.noreply.github.com>
2022-11-08 19:05:11 +0800

    Make inspect import locally

--- pydantic/schema.py
+++ pydantic/schema.py
@@ -1,4 +1,3 @@
-import inspect
 import re
 import warnings
 from collections import defaultdict
@@ -615,11 +614,13 @@ def enum_process_schema(enum: Type[Enum], *, field: Optional[ModelField] = None)
 
     This is similar to the `model_process_schema` function, but applies to ``Enum`` objects.
     """
+    import inspect
+
     schema_: Dict[str, Any] = {
         'title': enum.__name__,
         # Python assigns all enums a default docstring value of 'An enumeration', so
         # all enums will have a description field even if not explicitly provided.
-        'description': inspect.cleandoc(enum.__doc__) or 'An enumeration.',
+        'description': inspect.cleandoc(enum.__doc__ or 'An enumeration.'),
         # Add enum values and the enum field type to the schema.
         'enum': [item.value for item in cast(Iterable[Enum], enum)],
     }
