90dcf182cd500f154dec49bf358de28f5b84cfb3
Gavin D'souza <gavin18d@gmail.com>
2022-12-01 13:01:20 +0530

    feat(minor): Allow passing config to parse_obj_as

--- pydantic/tools.py
+++ pydantic/tools.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 from typing import Any, Callable, Optional, Type, TypeVar, Union
 
 from ._internal import _repr
+from .config import BaseConfig
 
 __all__ = 'parse_obj_as', 'schema_of', 'schema_json_of'
 
@@ -13,21 +14,25 @@ def _generate_parsing_type_name(type_: Any) -> str:
 
 
 @lru_cache(maxsize=2048)
-def _get_parsing_type(type_: Any, *, type_name: Optional[NameFactory] = None) -> Any:
+def _get_parsing_type(
+    type_: Any, *, type_name: Optional[NameFactory] = None, config: Optional[Type[BaseConfig]] = None
+) -> Any:
     from pydantic.main import create_model
 
     if type_name is None:
         type_name = _generate_parsing_type_name
     if not isinstance(type_name, str):
         type_name = type_name(type_)
-    return create_model(type_name, __root__=(type_, ...))
+    return create_model(type_name, __root__=(type_, ...), __config__=config)
 
 
 T = TypeVar('T')
 
 
-def parse_obj_as(type_: Type[T], obj: Any, *, type_name: Optional[NameFactory] = None) -> T:
-    model_type = _get_parsing_type(type_, type_name=type_name)  # type: ignore[arg-type]
+def parse_obj_as(
+    type_: Type[T], obj: Any, *, type_name: Optional[NameFactory] = None, config: Optional[Type[BaseConfig]] = None
+) -> T:
+    model_type = _get_parsing_type(type_, type_name=type_name, config=config)  # type: ignore[arg-type]
     return model_type(__root__=obj).__root__
 
 
