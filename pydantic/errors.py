0f3ae471f00810c1de9784ba4214c37381edff91
Ben Goldstone <bgoldstone1@gmail.com>
2022-12-24 16:18:34 -0500

    added formatting

--- pydantic/errors.py
+++ pydantic/errors.py
@@ -7,37 +7,39 @@ class PydanticUserError(TypeError):
     """
     Error caused by incorrect use of pydantic
     """
-    def __init__(self, type: str, message: str):     
-        super().__init__(self.message)
+
+    def __init__(self, type: str, message: str):
+        super().__init__(message)
         self.type = type
         self.message = message
-    
+
     def __str__(self) -> str:
         return f'PydanticUserError(type={self.type}, message={self.message})'
-
-
 
 
 class PydanticUndefinedAnnotation(NameError):
     """
     Error occurs when annotations are not yet defined
     """
-    def __init__(self, type: str, message: str):     
-        super().__init__(self.message)
+
+    def __init__(self, type: str, message: str):
+        super().__init__(message)
         self.type = type
         self.message = message
-    
+
     def __str__(self) -> str:
         return f'PydanticUndefinedAnnotation(type={self.type}, message={self.message})'
+
 
 class PydanticSchemaGenerationError(PydanticUserError):
     """
     Error occurs when schema has not been generated correctly.
     """
-    def __init__(self, type: str, message: str):     
-        super().__init__(self.message)
+
+    def __init__(self, type: str, message: str):
+        super().__init__(type, message)
         self.type = type
         self.message = message
-    
+
     def __str__(self) -> str:
         return f'PydanticSchemaGenerationError(type={self.type}, message={self.message})'
