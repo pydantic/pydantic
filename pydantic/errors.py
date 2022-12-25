69a5f1fb7943db1598d502f7cc2cae728c6e0954
Ben Goldstone <bgoldstone1@gmail.com>
2022-12-24 20:59:07 -0500

    added init variable mixins

--- pydantic/errors.py
+++ pydantic/errors.py
@@ -3,10 +3,16 @@ from __future__ import annotations as _annotations
 __all__ = 'PydanticUserError', 'PydanticSchemaGenerationError'
 
 class PydanticErrorMixin:
+    """
+     Pydantic Error Mixin for common functions
+    """
     def __str__(self) -> str:
          return f'{self.__class__.__name__}(type="{self.type}", message="{self.message}")'
     def __repr__(self) -> str:
          return self.__str__()
+    def init_variables(self,type,message) -> None:
+        self.type = type
+        self.message = message
 
 
 class PydanticUserError(PydanticErrorMixin,TypeError):
@@ -14,10 +20,10 @@ class PydanticUserError(PydanticErrorMixin,TypeError):
     Error caused by incorrect use of pydantic
     """
 
-    def __init__(self, type: str, message: str):
+    def __init__(self, type: str, message: str) -> None:
         super(TypeError,self).__init__(message)
-        self.type = type
-        self.message = message
+        self.init_variables(type,message)
+        
 
 
 class PydanticUndefinedAnnotation(PydanticErrorMixin,NameError):
@@ -25,10 +31,9 @@ class PydanticUndefinedAnnotation(PydanticErrorMixin,NameError):
     Error occurs when annotations are not yet defined
     """
 
-    def __init__(self, type: str, message: str):
+    def __init__(self, type: str, message: str) -> None:
         super(NameError,self).__init__(message)
-        self.type = type
-        self.message = message
+        self.init_variables(type,message)
 
 
 class PydanticSchemaGenerationError(PydanticUserError):
@@ -36,7 +41,6 @@ class PydanticSchemaGenerationError(PydanticUserError):
     Error occurs when schema has not been generated correctly.
     """
 
-    def __init__(self, type: str, message: str):
+    def __init__(self, type: str, message: str) -> None:
         super(PydanticUserError,self).__init__(type, message)
-        self.type = type
-        self.message = message
+        self.init_variables(type,message)
