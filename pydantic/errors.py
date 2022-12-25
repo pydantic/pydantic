cb33a00f046e1dcb577c82183376854f06fa77af
Ben Goldstone <bgoldstone1@gmail.com>
2022-12-24 21:04:44 -0500

    added make formatting

--- pydantic/errors.py
+++ pydantic/errors.py
@@ -2,38 +2,41 @@ from __future__ import annotations as _annotations
 
 __all__ = 'PydanticUserError', 'PydanticSchemaGenerationError'
 
+
 class PydanticErrorMixin:
     """
-     Pydantic Error Mixin for common functions
+    Pydantic Error Mixin for common functions
     """
+
     def __str__(self) -> str:
-         return f'{self.__class__.__name__}(type="{self.type}", message="{self.message}")'
+        return f'{self.__class__.__name__}(type="{self.type}", message="{self.message}")'
+
     def __repr__(self) -> str:
-         return self.__str__()
-    def init_variables(self,type,message) -> None:
+        return self.__str__()
+
+    def init_variables(self, type: str, message: str) -> None:
         self.type = type
         self.message = message
 
 
-class PydanticUserError(PydanticErrorMixin,TypeError):
+class PydanticUserError(PydanticErrorMixin, TypeError):
     """
     Error caused by incorrect use of pydantic
     """
 
     def __init__(self, type: str, message: str) -> None:
-        super(TypeError,self).__init__(message)
-        self.init_variables(type,message)
-        
+        super(TypeError, self).__init__(message)
+        self.init_variables(type, message)
 
 
-class PydanticUndefinedAnnotation(PydanticErrorMixin,NameError):
+class PydanticUndefinedAnnotation(PydanticErrorMixin, NameError):
     """
     Error occurs when annotations are not yet defined
     """
 
     def __init__(self, type: str, message: str) -> None:
-        super(NameError,self).__init__(message)
-        self.init_variables(type,message)
+        super(NameError, self).__init__(message)
+        self.init_variables(type, message)
 
 
 class PydanticSchemaGenerationError(PydanticUserError):
@@ -42,5 +45,5 @@ class PydanticSchemaGenerationError(PydanticUserError):
     """
 
     def __init__(self, type: str, message: str) -> None:
-        super(PydanticUserError,self).__init__(type, message)
-        self.init_variables(type,message)
+        super(PydanticUserError, self).__init__(type, message)
+        self.init_variables(type, message)
