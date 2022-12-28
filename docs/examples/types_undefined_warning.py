faf39bc5b21247c4dc91531d3d84d47d67fdbf19
Jim Kring <jim.kring@gmail.com>
2022-12-28 14:52:42 -0800

    fixed linting

--- docs/examples/types_undefined_warning.py
+++ docs/examples/types_undefined_warning.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
-from typing import Optional
 
 from pydantic import BaseModel
 
 
 # This example shows how Book uses the Person type and Person uses the Book type.
 # We will demonstrate how to suppress the undefined types warning and define all the models.
+
 
 class Book(BaseModel):
     title: str
@@ -15,9 +15,11 @@ class Book(BaseModel):
     class Config:
         undefined_types_warning = False
 
+
 class Person(BaseModel):
     name: str
     books_read: list[Book] | None = None
+
 
 # Now, we can rebuild the `Book` model, since the `Person` model is now defined.
 Book.model_rebuild()
@@ -26,7 +28,9 @@ Book.model_rebuild()
 python_crash_course = Book(title='Python Crash Course', author=Person(name='Eric Matthes'))
 jane_doe = Person(name='Jane Doe', books_read=[python_crash_course])
 
-assert jane_doe.json(indent=2, exclude_unset=True) == """
+assert (
+    jane_doe.json(indent=2, exclude_unset=True)
+    == """
 {
   "name": "Jane Doe",
   "books_read": [
@@ -39,3 +43,4 @@ assert jane_doe.json(indent=2, exclude_unset=True) == """
   ]
 }
 """.strip()
+)
