ab7cedc86233aaff164c6e9229d372928dd42231
= <=>
2023-01-17 17:40:48 -0500

    cleanup

--- pydantic/json_schema.py
+++ pydantic/json_schema.py
@@ -1,6 +1,6 @@
 import re
 import typing
-from typing import Any, Dict
+from typing import Any, Dict, List, Sequence, Union, Type, Optional
 
 # from ._generate_schema import generate_config, model_fields_schema
 TYPE_MAP = {
@@ -11,7 +11,7 @@ TYPE_MAP = {
     # bool: 'boolean',
     # float: 'number',
     # None: 'null',
-    # dict: 'object',
+    'dict': 'object',
     'new-class': 'object',
     'datetime': 'date-time',
     'str': 'string',
@@ -19,12 +19,51 @@ TYPE_MAP = {
 }
 
 DEFAULT_JSON_SCHEMA_URI = 'https://json-schema.org/draft/2020-12/schema'
-DEFAULT_JSON_SCHEMA_PREFIX = '#/definitions/'
+DEFAULT_JSON_SCHEMA_REF_PREFIX = '#/definitions/'
 DEFAULT_JSON_SCHEMA_REF_TEMPLATE = '#/definitions/{model}'
+
+if typing.TYPE_CHECKING:
+    Model = typing.TypeVar('Model', bound='BaseModel')
 
 
 def internal_to_json_types(s: str) -> str:
     return TYPE_MAP.get(s, s)
+
+
+def generate_schema(
+    models: Sequence[Union[Type['BaseModel'], Type['Dataclass']]],
+    *,
+    by_alias: bool = True,
+    title: Optional[str] = None,
+    description: Optional[str] = None,
+    ref_prefix: Optional[str] = None,
+    ref_template: str = DEFAULT_JSON_SCHEMA_REF_TEMPLATE,
+) -> Dict[str, Any]:
+    """
+    Process a list of models and generate a single JSON Schema with all of them defined in the ``definitions``
+    top-level JSON key, including their sub-models.
+
+    :param models: a list of models to include in the generated JSON Schema
+    :param by_alias: generate the schemas using the aliases defined, if any
+    :param title: title for the generated schema that includes the definitions
+    :param description: description for the generated schema
+    :param ref_prefix: the JSON Pointer prefix for schema references with ``$ref``, if None, will be set to the
+      default of ``#/definitions/``. Update it if you want the schemas to reference the definitions somewhere
+      else, e.g. for OpenAPI use ``#/components/schemas/``. The resulting generated schemas will still be at the
+      top-level key ``definitions``, so you can extract them from there. But all the references will have the set
+      prefix.
+    :param ref_template: Use a ``string.format()`` template for ``$ref`` instead of a prefix. This can be useful
+      for references that cannot be represented by ``ref_prefix`` such as a definition stored in another file. For
+      a sibling json file in a ``/schemas`` directory use ``"/schemas/${model}.json#"``.
+    :return: dict with the JSON Schema with a ``definitions`` top-level key including the schema definitions for
+      the models and sub-models passed in ``models``.
+    """
+
+    if ref_prefix is None:
+        ref_prefix = DEFAULT_JSON_SCHEMA_PREFIX
+
+    # TODO: implement.
+    pass
 
 
 def get_schema_property_json(field_name: str, inner_schema_field: Dict[str, Any]):
@@ -49,7 +88,7 @@ def get_schema_property_json(field_name: str, inner_schema_field: Dict[str, Any]
     return {'title': normalize_name(field_name), 'type': _types}
 
 
-def internal_to_json_schema(inner_schema: typing.Dict[str, Any], fields) -> Dict[str, Any]:
+def internal_to_json_schema(inner_schema: Dict[str, Any], fields) -> Dict[str, Any]:
     """Returns a JSON Schema document, compatible with draft 2020-12."""
 
     # print(inner_schema)
