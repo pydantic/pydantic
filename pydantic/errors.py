d99a02aad8c5205f4a234bcc1cffd9d6a30b0c03
Benjamin Goldstone <bgoldstone1@gmail.com>
2023-02-02 16:34:11 -0500

    fixed branch to be in sync with upstream

--- pydantic/errors.py
+++ pydantic/errors.py
@@ -1,53 +1,19 @@
 from __future__ import annotations as _annotations
 
-from typing import Any, Optional
-
 __all__ = 'PydanticUserError', 'PydanticSchemaGenerationError'
 
-ErrorCodes = {}
 
-
-class PydanticErrorMixin:
+class PydanticUserError(TypeError):
     """
-    Pydantic Error Mixin for common functions
+    Error caused by incorrect use of pydantic
     """
 
-    def __str__(self) -> str:
-        return f'{self.__class__.__name__}(code={self.code!r}, message={self.message!r})'
 
-    def __repr__(self) -> str:
-        return self.__str__()
-
-    def __init__(self, code: ErrorCodes, *, message: Optional[str] = None) -> None:
-        self.code = code
-        self.message = message
-        print(self.__class__.mro())
-        # If using mixin directly
-        if PydanticErrorMixin in self.__class__.mro():
-            super(self.__class__.mro()[2], self).__init__(message)
-        else:
-            super(self.__class__.mro()[3], self).__init__(message)
-
-
-class PydanticUserError(PydanticErrorMixin, TypeError):
-    """
-    Error caused by incorrect use of Pydantic
-    """
-
-    pass
-
-
-class PydanticUndefinedAnnotation(PydanticErrorMixin, NameError):
+class PydanticUndefinedAnnotation(NameError):
     """
     Error occurs when annotations are not yet defined
     """
 
-    pass
-
 
 class PydanticSchemaGenerationError(PydanticUserError):
-    """
-    Error occurs when schema has not been generated correctly.
-    """
-
     pass
