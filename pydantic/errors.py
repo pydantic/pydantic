2cb2625723ae3175adbb91e0dbe8469dd6052c9a
Ben Goldstone <bgoldstone1@gmail.com>
2023-02-07 08:46:42 -0500

    Error definitions (#4870)
    * added constructors an string method to errors.py
    
    * added formatting
    
    * added mixins to errors.py
    
    * added init variable mixins
    
    * added make formatting
    
    * capitalized Pydantic
    
    * fixed __init__ method for ErrorMixin
    
    * added formatting
    
    * added ErrorCodes Literal
    
    * fixed type hinting
    
    * fixed some errors
    
    * fixed errors on errors.py
    
    * moved __init__ method to top of mixin
    
    * fixed __all__ from errors not going into init
    
    * fixed missing Field
    
    * fixed missing comma
    
    * added constructors an string method to errors.py
    
    * added formatting
    
    * added mixins to errors.py
    
    * added init variable mixins
    
    * added make formatting
    
    * capitalized Pydantic
    
    * fixed __init__ method for ErrorMixin
    
    * added formatting
    
    * added ErrorCodes Literal
    
    * fixed branch to be in sync with upstream
    
    * debugged errors
    
    * git merge upstream/main resolve conflicts.

--- pydantic/errors.py
+++ pydantic/errors.py
@@ -1,19 +1,41 @@
 from __future__ import annotations as _annotations
 
-__all__ = 'PydanticUserError', 'PydanticSchemaGenerationError'
+from typing import Optional
+
+__all__ = 'PydanticUserError', 'PydanticSchemaGenerationError', 'PydanticUndefinedAnnotation'
 
 
-class PydanticUserError(TypeError):
+class PydanticErrorMixin:
     """
-    Error caused by incorrect use of pydantic
+    Pydantic Error Mixin for common functions
     """
 
+    def __init__(self, code: str, *, message: Optional[str] = None) -> None:
+        self.code = code
+        self.message = message
 
-class PydanticUndefinedAnnotation(NameError):
+
+class PydanticUserError(PydanticErrorMixin, TypeError):
+    """
+    Error caused by incorrect use of Pydantic
+    """
+
+    pass
+
+
+class PydanticUndefinedAnnotation(PydanticErrorMixin, NameError):
     """
     Error occurs when annotations are not yet defined
     """
 
+    def __init__(self, name: str, *, message: Optional[str] = None) -> None:
+        self.name = name
+        super().__init__(code=name, message=message)
+
 
 class PydanticSchemaGenerationError(PydanticUserError):
+    """
+    Error occurs when schema has not been generated correctly.
+    """
+
     pass
