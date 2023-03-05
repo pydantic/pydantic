e25bb54a3867fdc3044f58f21bb3e9cfc80cb226
Reza Rohani <theonlykingpin@gmail.com>
2023-03-05 10:16:12 +0330

    Update metadata function description
    I think these changes will explain what this function does.

--- pydantic/_internal/_core_metadata.py
+++ pydantic/_internal/_core_metadata.py
@@ -55,8 +55,8 @@ class CoreMetadataHandler:
     @property
     def metadata(self) -> _CoreMetadata:
         """
-        Retrieves the metadata dict off the schema, initializing it to a dict if necessary,
-        and raise an error if it is not a dict.
+        Retrieves the metadata dict off the schema, initializing it to a dict if it is None
+        and raises an error if it is not a dict.
         """
         metadata = self._schema.get('metadata')
         if metadata is None:
