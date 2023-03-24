9541d7d2dceb7b774658681ded5060b608eee8aa
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-03-24 17:07:40 -0600

    Remove unused modification

--- pydantic/decorator.py
+++ pydantic/decorator.py
@@ -78,7 +78,6 @@ class ValidatedFunction:
         self.v_kwargs_name = 'kwargs'
 
         type_hints = _typing_extra.get_type_hints(function, include_extras=True)
-
         takes_args = False
         takes_kwargs = False
         fields: Dict[str, Tuple[Any, Any]] = {}
