ab6739476b21f8273b9ea57867ce2b5258d15c9e
Samuel Colvin <s@muelcolvin.com>
2023-04-07 13:31:15 +0100

    add back deprecated __fields_set__

--- pydantic/main.py
+++ pydantic/main.py
@@ -239,6 +239,16 @@ class BaseModel(_repr.Representation, metaclass=ModelMetaclass):
         """
         return self.__pydantic_fields_set__
 
+    @property
+    def __fields_set__(self) -> set[str]:
+        """
+        Deprecated alias for `model_fields_set`
+        """
+        warnings.warn(
+            'The `__fields_set__` attribute is deprecated, use `model_fields_set` instead.', DeprecationWarning
+        )
+        return self.__pydantic_fields_set__
+
     @classmethod
     def model_validate_json(
         cls: type[Model],
