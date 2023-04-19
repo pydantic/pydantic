6a06ec821edf63559bd81d896a27ad6f3eb3be90
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2023-04-19 10:39:18 -0500

    more revert

--- pydantic/decorators.py
+++ pydantic/decorators.py
@@ -412,7 +412,6 @@ def field_serializer(
     def dec(
         f: Callable[..., Any] | staticmethod[Any, Any] | classmethod[Any, Any, Any]
     ) -> _decorators.PydanticDescriptorProxy[Any]:
-        f = _decorators.ensure_classmethod_based_on_signature(f)
         dec_info = _decorators.FieldSerializerDecoratorInfo(
             fields=fields,
             mode=mode,
@@ -450,7 +449,6 @@ def model_serializer(
     """
 
     def dec(f: Callable[..., Any]) -> _decorators.PydanticDescriptorProxy[Any]:
-        f = _decorators.ensure_classmethod_based_on_signature(f)
         dec_info = _decorators.ModelSerializerDecoratorInfo(mode=mode, json_return_type=json_return_type)
         return _decorators.PydanticDescriptorProxy(f, dec_info)
 
