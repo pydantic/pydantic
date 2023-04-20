415b3ef6676a0259ce4ff65ec6440b968c779f5a
Samuel Colvin <s@muelcolvin.com>
2023-04-20 10:46:48 -0600

    Revert "feat: allow accessing attributes as strings (#5501)" (#5541)
    This reverts commit 24515489250de5d63c487e608f2f8d184b427498.

--- pydantic/main.py
+++ pydantic/main.py
@@ -301,9 +301,6 @@ class BaseModel(_repr.Representation, metaclass=ModelMetaclass):
         """
         pass
 
-    def __getitem__(self, item):
-        return getattr(self, item)
-
     def __setattr__(self, name: str, value: Any) -> None:
         if name in self.__class_vars__:
             raise AttributeError(
