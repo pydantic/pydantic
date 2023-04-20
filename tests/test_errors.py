5fa29a240ecb41d1037a0189a2158c29813f66db
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-04-20 07:33:46 -0600

    Keep original test as comment

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -8,6 +8,11 @@ def test_user_error_url():
         BaseModel()
 
     # insert_assert(str(exc_info.value))
+    # assert str(exc_info.value) == (
+    #     'Pydantic models should inherit from BaseModel, BaseModel cannot be instantiated directly\n\n'
+    #     f'For further information visit https://errors.pydantic.dev/{VERSION}/u/base-model-instantiated'
+    # )
+    # TODO: Replace the below with the above once the errors URLs are set up.
     assert str(exc_info.value) == (
         'Pydantic models should inherit from BaseModel, BaseModel cannot be instantiated directly\n\n'
         'For further information visit <TODO: Set up the errors URLs>/base-model-instantiated'
