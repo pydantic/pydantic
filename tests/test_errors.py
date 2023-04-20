a6e71ad4dfcbd75bcccd02105e1bdad2188da451
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-04-20 09:56:13 -0600

    Change errors URL for next release (#5538)
    * Change errors URL for next release
    
    * Keep original test as comment

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pydantic import VERSION, BaseModel, PydanticUserError
+from pydantic import BaseModel, PydanticUserError
 
 
 def test_user_error_url():
@@ -8,7 +8,12 @@ def test_user_error_url():
         BaseModel()
 
     # insert_assert(str(exc_info.value))
+    # assert str(exc_info.value) == (
+    #     'Pydantic models should inherit from BaseModel, BaseModel cannot be instantiated directly\n\n'
+    #     f'For further information visit https://errors.pydantic.dev/{VERSION}/u/base-model-instantiated'
+    # )
+    # TODO: Replace the below with the above once the errors URLs are set up.
     assert str(exc_info.value) == (
         'Pydantic models should inherit from BaseModel, BaseModel cannot be instantiated directly\n\n'
-        f'For further information visit https://errors.pydantic.dev/{VERSION}/u/base-model-instantiated'
+        'For further information visit <TODO: Set up the errors URLs>/base-model-instantiated'
     )
