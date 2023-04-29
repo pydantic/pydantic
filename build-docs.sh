48c98d4b1d70f29ee892da8f2d383c77baef0948
Samuel Colvin <s@muelcolvin.com>
2023-04-29 12:51:00 +0100

    bump

--- build-docs.sh
+++ build-docs.sh
@@ -6,7 +6,7 @@
 set -e
 set -x
 
-apt-get install python3-venv
+sudo apt-get install python3-venv
 curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
 
 pdm install -G docs
