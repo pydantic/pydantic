c600c010418c46cec1f3a0fe8fb6ca4be4a7949f
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-05-01 18:34:38 -0600

    Add alias generators to pydantic (#5568)
    * Add alias generators to pydantic
    
    * Update _migration.py
    
    * Address feedback

---
+++ pydantic/alias_generators.py
@@ -0,0 +1,28 @@
+import re
+
+__all__ = ('to_pascal', 'to_camel', 'to_snake')
+
+
+def to_pascal(snake: str) -> str:
+    """
+    Converts a snake_case string to PascalCase.
+    """
+    camel = snake.title()
+    return re.sub('([0-9A-Za-z])_(?=[0-9A-Z])', lambda m: m.group(1), camel)
+
+
+def to_camel(snake: str) -> str:
+    """
+    Converts a snake_case string to camelCase.
+    """
+    camel = to_pascal(snake)
+    return re.sub('(^_*[A-Z])', lambda m: m.group(1).lower(), camel)
+
+
+def to_snake(camel: str) -> str:
+    """
+    Converts a PascalCase or camelCase string to snake_case.
+    """
+    snake = re.sub(r'([a-zA-Z])([0-9])', lambda m: f'{m.group(1)}_{m.group(2)}', camel)
+    snake = re.sub(r'([a-z0-9])([A-Z])', lambda m: f'{m.group(1)}_{m.group(2)}', snake)
+    return snake.lower()
