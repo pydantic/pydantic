f84998a2720a0e6521aa43d9f224c252b78b50b7
Serge Matveenko <lig@countzero.co>
2023-05-03 17:13:24 +0200

    🐛 `Sequence[str]` does not raise on `str` (#5599)
    ## Change Summary
    
    `sequence_validator` in `_internal._validators` has been changed to
    match Pydantic v1 behavior.
    
    It is now impossible to use strings (`str` or `bytes`) subclasses as
    values for `Sequence[str]` schema.
    
    ## Related issue number
    
    fix #5595 
    
    ## Checklist
    
    * [x] Unit tests for the changes exist
    * [x] Tests pass on CI and coverage remains at 100%
    * [x] Documentation reflects the changes where applicable
    * [x] `changes/<pull request or issue id>-<github username>.md` file
    added describing change
    (see
    [changes/README.md](https://github.com/samuelcolvin/pydantic/blob/master/changes/README.md)
    for details)
    * [x] My PR is ready to review, **please add a comment including the
    phrase "please review" to assign reviewers**
    
    
    Selected Reviewer: @samuelcolvin
    
    ---------
    
    Co-authored-by: Terrence Dorsey <terrend@mishu.com>
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -2448,3 +2448,48 @@ def test_invalid_forward_ref_model():
     assert exc_info.value.errors() == [
         {'input': C(), 'loc': ('B',), 'msg': 'Input should be a valid dictionary', 'type': 'dict_type'}
     ]
+
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason='cannot parametrize types before 3.9')
+@pytest.mark.parametrize(
+    ('sequence_type', 'input_data', 'expected_error_type', 'expected_error_msg', 'expected_error_ctx'),
+    [
+        pytest.param(List[str], '1bc', 'list_type', 'Input should be a valid list', None, id='list[str]'),
+        pytest.param(
+            Sequence[str],
+            '1bc',
+            'sequence_str',
+            "'str' instances are not allowed as a Sequence value",
+            {'type_name': 'str'},
+            id='Sequence[str]',
+        ),
+        pytest.param(
+            Sequence[bytes],
+            b'1bc',
+            'sequence_str',
+            "'bytes' instances are not allowed as a Sequence value",
+            {'type_name': 'bytes'},
+            id='Sequence[bytes]',
+        ),
+    ],
+)
+def test_sequences_str(sequence_type, input_data, expected_error_type, expected_error_msg, expected_error_ctx):
+    input_sequence = [input_data[:1], input_data[1:]]
+    expected_error = {
+        'type': expected_error_type,
+        'input': input_data,
+        'loc': ('str_sequence',),
+        'msg': expected_error_msg,
+    }
+    if expected_error_ctx is not None:
+        expected_error.update(ctx=expected_error_ctx)
+
+    class Model(BaseModel):
+        str_sequence: sequence_type
+
+    assert Model(str_sequence=input_sequence).str_sequence == input_sequence
+
+    with pytest.raises(ValidationError) as e:
+        Model(str_sequence=input_data)
+
+    assert e.value.errors() == [expected_error]
