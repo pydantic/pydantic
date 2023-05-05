6cf63e2d9ed06aa698449838143b89f00e6bf84e
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-05-05 09:30:53 -0600

    Don't generate URLs for custom errors (pydantic/pydantic-core#586)
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/403cce745dcc7d810e79f293511f68e6ffd11893

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -51,7 +51,7 @@ def test_pydantic_value_error_usage():
     with pytest.raises(ValidationError) as exc_info:
         v.validate_python(42)
 
-    assert exc_info.value.errors(include_url=False) == [
+    assert exc_info.value.errors() == [
         {
             'type': 'my_error',
             'loc': (),
@@ -74,8 +74,7 @@ def test_pydantic_value_error_invalid_dict():
     assert str(exc_info.value) == (
         '1 validation error for function-plain[my_function()]\n'
         "  (error rendering message: TypeError: 'tuple' object cannot be converted to 'PyString') "
-        '[type=my_error, input_value=42, input_type=int]\n'
-        f'    For further information visit https://errors.pydantic.dev/{__version__}/v/my_error'
+        '[type=my_error, input_value=42, input_type=int]'
     )
     with pytest.raises(TypeError, match="'tuple' object cannot be converted to 'PyString'"):
         exc_info.value.errors(include_url=False)
