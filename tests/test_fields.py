d2bae34723bd88ebf09e2889f1a8ab80b2e63905
Serge Matveenko <lig@countzero.co>
2023-05-09 11:21:57 +0200

    ✅ Fully cover `pydantic/fields.py`

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -4,6 +4,20 @@ from dataclasses import InitVar
 import pytest
 
 import pydantic.dataclasses
+from pydantic import fields
+
+
+def test_field_info_annotation_keyword_argument():
+    """This tests that `FieldInfo.from_field` raises an error if passed the `annotation` kwarg.
+
+    At the time of writing this test there is no way `FieldInfo.from_field` could receive the `annotation` kwarg from
+    anywhere inside Pydantic code. However, it is possible that this API is still being in use by applications and
+    third-party tools.
+    """
+    with pytest.raises(TypeError) as e:
+        fields.FieldInfo.from_field(annotation=())
+
+    assert e.value.args == ('"annotation" is not permitted as a Field keyword argument',)
 
 
 @pytest.mark.skipif(sys.version_info >= (3, 8), reason='No error is thrown for `InitVar` for Python 3.8+')
