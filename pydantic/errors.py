2c68e678ce0b56676707de9407e3ff87823d30d8
Terrence Dorsey <terrend@mishu.com>
2023-05-12 11:39:06 -0400

    updates to errors and fields docstrings (#5746)
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>

--- pydantic/errors.py
+++ pydantic/errors.py
@@ -115,7 +115,12 @@ class PydanticUndefinedAnnotation(PydanticErrorMixin, NameError):
 
 
 class PydanticImportError(PydanticErrorMixin, ImportError):
-    """Error occurs when an import fails due to module changes between V1 and V2."""
+    """
+    Error raised when an import fails due to module changes between V1 and V2.
+
+    Attributes:
+        message (str): Description of the error.
+    """
 
     def __init__(self, message: str) -> None:
         super().__init__(message, code='import-error')
