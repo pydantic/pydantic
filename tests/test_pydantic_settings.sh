0ee80ec591e1df3fc92e6b34c0a786da2acf233a
Hasan Ramezani <hasan.r67@gmail.com>
2023-05-15 14:54:51 +0330

    Address comments

--- tests/test_pydantic_settings.sh
+++ tests/test_pydantic_settings.sh
@@ -4,10 +4,6 @@ set -x
 set -e
 
 cd pydantic-settings
-git fetch --tags
-latest_tag_commit=$(git rev-list --tags --max-count=1)
-latest_tag=$(git describe --tags "${latest_tag_commit}")
-git checkout "${latest_tag}"
 
 python -m ensurepip --upgrade
 
