533ae748f2bc0cd6a6f67a1a66b291f4c8841956
Hasan Ramezani <hasan.r67@gmail.com>
2023-05-18 01:00:40 +0330

    Move remove current v1 command

--- update_v1.sh
+++ update_v1.sh
@@ -3,9 +3,6 @@
 set -x
 set -e
 
-# Remove current V1
-rm -rf pydantic/v1
-
 echo "cloning pydantic V1"
 git clone -b 1.10.X-fixes https://github.com/pydantic/pydantic.git pydantic-v1
 
@@ -15,6 +12,9 @@ pushd "$(dirname $0)/pydantic-v1"
 latest_tag=$(git describe --tags --abbrev=0)
 echo "latest tag in V1 is '${latest_tag}'"
 git checkout "${latest_tag}"
+
+# Remove current V1
+rm -rf ../pydantic/v1
 
 # Copy new V1 into pydantic/v1
 cp -r pydantic ../pydantic/v1
