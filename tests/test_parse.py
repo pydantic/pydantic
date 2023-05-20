7f1d2507303c9630ee5c9d95fcc6042746f63245
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-05-20 14:29:29 -0600

    Update tests/test_parse.py

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -48,7 +48,7 @@ def test_model_validate_wrong_model():
 
 def test_root_model_error():
     with pytest.raises(
-        TypeError, match="To define root models, use `pydantic.RootModel` rather than a field called '__root__"
+        TypeError, match="To define root models, use `pydantic.RootModel` rather than a field called '__root__'"
     ):
 
         class MyModel(BaseModel):
