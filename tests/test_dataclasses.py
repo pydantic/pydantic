776d9c3d08a22e0916a92e27aa6e0fbd787639ba
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2023-06-05 13:02:36 -0500

    Add override test

--- tests/test_dataclasses.py
+++ tests/test_dataclasses.py
@@ -2328,3 +2328,13 @@ def test_model_config(dataclass_decorator: Any) -> None:
 
     ta = TypeAdapter(Model)
     assert ta.validate_python({'x': 'ABC'}).x == 'abc'
+
+
+def test_model_config_override_in_decorator() -> None:
+    @pydantic.dataclasses.dataclass(config=ConfigDict(str_to_lower=False, str_strip_whitespace=True))
+    class Model:
+        x: str
+        __pydantic_config__ = ConfigDict(str_to_lower=True)
+
+    ta = TypeAdapter(Model)
+    assert ta.validate_python({'x': 'ABC'}).x == 'ABC'
