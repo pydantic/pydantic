214384680907cd321a5db79898522662e1c46199
Fynn Beuttenmüller <thefynnbe@gmail.com>
2023-06-26 23:09:22 +0200

    fix context may be None (pydantic/pydantic-core#696)
    Co-authored-by: Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
    
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/c84c0a901098362dd89e1fb1c370628350536767

--- pydantic_core/core_schema.py
+++ pydantic_core/core_schema.py
@@ -144,7 +144,7 @@ class ValidationInfo(Protocol):
     """
 
     @property
-    def context(self) -> Dict[str, Any]:
+    def context(self) -> Any | None:
         """Current validation context."""
         ...
 
