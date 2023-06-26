bdbe0f0f5b3e3277452bc36fdcfef06eea18e209
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-06-26 16:04:28 +0200

    👷 Enable FastAPI tests (#6249)

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -5,8 +5,10 @@ set -e
 
 cd fastapi
 git fetch --tags
-latest_tag_commit=$(git rev-list --tags --max-count=1)
-latest_tag=$(git describe --tags "${latest_tag_commit}")
+# TODO: Remove the comments once FastAPI 0.100.0 is released.
+# latest_tag_commit=$(git rev-list --tags --max-count=1)
+# latest_tag=$(git describe --tags "${latest_tag_commit}")
+latest_tag="0.100.0-beta1"
 git checkout "${latest_tag}"
 
 pip install -r requirements.txt
