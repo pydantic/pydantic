b95dd7b0b9b52902534f381ea16432e9c164b7e3
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2023-06-28 16:53:52 +0100

    small cleanups after move to python/ source dir (pydantic/pydantic-core#708)
    Original-commit-hash: 00e9f1b690264f54317a56b109deaa9c4fc8ddb8

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 use std::str::from_utf8;
 
 fn generate_self_schema() {
-    println!("cargo:rerun-if-changed=pydantic_core/core_schema.py");
+    println!("cargo:rerun-if-changed=python/pydantic_core/core_schema.py");
     println!("cargo:rerun-if-changed=generate_self_schema.py");
     if Path::new("./src/self_schema.py").exists() && option_env!("CI") == Some("true") {
         // self_schema.py already exists and CI indicates we're running on a github actions build,
