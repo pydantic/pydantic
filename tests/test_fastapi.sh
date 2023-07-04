53b13626e1dbb3b0d408715a70a89373afbe42c2
Serge Matveenko <lig@pydantic.dev>
2023-07-04 19:12:41 +0200

    💚 Use `main-pv2` FastAPI branch in integration tests (#6432)

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -5,10 +5,10 @@ set -e
 
 cd fastapi
 git fetch --tags
-# TODO: Remove the comments once FastAPI 0.100.0 is released.
-# latest_tag_commit=$(git rev-list --tags --max-count=1)
-# latest_tag=$(git describe --tags "${latest_tag_commit}")
-latest_tag="0.100.0-beta1"
+
+# TODO: Use the proper latest tag once FastAPI stable release is compatible with Pydantic V2.
+# latest_tag=$(git describe --tags --abbrev=0)
+latest_tag="main-pv2"
 git checkout "${latest_tag}"
 
 pip install -r requirements.txt
