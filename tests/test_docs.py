ba0fa56185a63ceb331dc4186d18c02ca41e62ad
Serge Matveenko <lig@pydantic.dev>
2023-07-11 18:33:11 +0200

    Add `enum` error type docs  (#6603)
    Co-authored-by: Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -233,7 +233,6 @@ def test_error_codes():
     assert code_error_codes == documented_error_codes, 'Error codes in code and docs do not match'
 
 
-@pytest.mark.xfail(reason='`enum` is not documented')
 def test_validation_error_codes():
     error_text = (DOCS_ROOT / 'usage/validation_errors.md').read_text()
 
