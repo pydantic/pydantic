9866f837e2e8a6c3d0da6f4309bb0e10a040329c
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2023-07-13 12:28:57 -0500

    Make TzInfo picklable (pydantic/pydantic-core#770)
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>
    
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/f499c73ccb264cf7e6f7c2a8440ca76e232a6410

--- tests/validators/test_datetime.py
+++ tests/validators/test_datetime.py
@@ -1,9 +1,11 @@
 import copy
 import json
+import pickle
 import platform
 import re
 from datetime import date, datetime, time, timedelta, timezone, tzinfo
 from decimal import Decimal
+from typing import Dict
 
 import pytest
 import pytz
@@ -476,3 +478,38 @@ def test_tz_constraint_too_high():
 def test_tz_constraint_wrong():
     with pytest.raises(SchemaError, match="Input should be 'aware' or 'naive"):
         SchemaValidator(core_schema.datetime_schema(tz_constraint='wrong'))
+
+
+def test_tz_pickle() -> None:
+    """
+    https://github.com/pydantic/pydantic-core/issues/589
+    """
+    v = SchemaValidator(core_schema.datetime_schema())
+    original = datetime(2022, 6, 8, 12, 13, 14, tzinfo=timezone(timedelta(hours=-12, minutes=-15)))
+    validated = v.validate_python('2022-06-08T12:13:14-12:15')
+    assert validated == original
+    assert pickle.loads(pickle.dumps(validated)) == validated == original
+
+
+def test_tz_hash() -> None:
+    v = SchemaValidator(core_schema.datetime_schema())
+    lookup: Dict[datetime, str] = {}
+    for day in range(1, 10):
+        input_str = f'2022-06-{day:02}T12:13:14-12:15'
+        validated = v.validate_python(input_str)
+        lookup[validated] = input_str
+
+    assert len(lookup) == 9
+    assert (
+        lookup[datetime(2022, 6, 8, 12, 13, 14, tzinfo=timezone(timedelta(hours=-12, minutes=-15)))]
+        == '2022-06-08T12:13:14-12:15'
+    )
+
+
+def test_tz_cmp() -> None:
+    v = SchemaValidator(core_schema.datetime_schema())
+    validated1 = v.validate_python('2022-06-08T12:13:14-12:15')
+    validated2 = v.validate_python('2022-06-08T12:13:14-12:14')
+
+    assert validated1 > validated2
+    assert validated2 < validated1
