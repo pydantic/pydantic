11b1289c8bfd9eb7e9cf1ad39a96da323f941ca5
Hasan Ramezani <hasan.r67@gmail.com>
2023-07-19 20:26:37 +0200

    Add tests

--- tests/mypy/modules/pydantic_settings.py
+++ tests/mypy/modules/pydantic_settings.py
@@ -6,3 +6,7 @@ class Settings(BaseSettings):
 
 
 s = Settings()
+
+s = Settings(foo='test', _case_sensitive=True, _env_prefix='test__', _env_file='test')
+
+s = Settings(foo='test', _case_sensitive=1, _env_prefix=2, _env_file=3)
