3570ffbcf0a53a499b87f6d7e6ee7320af6a29ae
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2023-07-19 11:09:54 +0100

    improve source locations for auxiliary packages in docs

--- build-docs.sh
+++ build-docs.sh
@@ -14,4 +14,12 @@ python3 -m pdm install -G docs
 
 python3 -m pdm run python -c 'import docs.plugins.main'
 
+# Adding local symlinks gets nice source locations like
+#   pydantic_core/core_schema.py
+# instead of
+#   .venv/lib/python3.10/site-packages/pydantic_core/core_schema.py
+ln -s .venv/lib/python*/site-packages/pydantic_core pydantic_core
+ln -s .venv/lib/python*/site-packages/pydantic_settings pydantic_settings
+ln -s .venv/lib/python*/site-packages/pydantic_extra_types pydantic_extra_types
+
 python3 -m pdm run mkdocs build
