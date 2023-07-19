5660c8c1c8777d57ffecd805ff80da182a4a43e1
Hasan Ramezani <hasan.r67@gmail.com>
2023-07-19 22:59:17 +0200

    Mypy plugin for settings (#6760)
    Co-authored-by: David Montague <35119617+dmontagu@users.noreply.github.com>

--- tests/mypy/outputs/1.1.1/mypy-default_ini/pydantic_settings.py
+++ tests/mypy/outputs/1.1.1/mypy-default_ini/pydantic_settings.py
@@ -7,3 +7,13 @@ class Settings(BaseSettings):
 
 s = Settings()
 # MYPY: error: Missing named argument "foo" for "Settings"  [call-arg]
+
+s = Settings(foo='test', _case_sensitive=True, _env_prefix='test__', _env_file='test')
+# MYPY: error: Unexpected keyword argument "_case_sensitive" for "Settings"  [call-arg]
+# MYPY: error: Unexpected keyword argument "_env_prefix" for "Settings"  [call-arg]
+# MYPY: error: Unexpected keyword argument "_env_file" for "Settings"  [call-arg]
+
+s = Settings(foo='test', _case_sensitive=1, _env_prefix=2, _env_file=3)
+# MYPY: error: Unexpected keyword argument "_case_sensitive" for "Settings"  [call-arg]
+# MYPY: error: Unexpected keyword argument "_env_prefix" for "Settings"  [call-arg]
+# MYPY: error: Unexpected keyword argument "_env_file" for "Settings"  [call-arg]
