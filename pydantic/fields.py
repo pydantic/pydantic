d9d4750ac36dbb0a7b712a282929b0beae1ed5ab
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-07-25 16:41:00 -0600

    Skip FieldInfo merging when unnecessary (#6862)

--- pydantic/fields.py
+++ pydantic/fields.py
@@ -380,6 +380,14 @@ class FieldInfo(_repr.Representation):
             flattened_field_infos.extend(x for x in field_info.metadata if isinstance(x, FieldInfo))
             flattened_field_infos.append(field_info)
         field_infos = tuple(flattened_field_infos)
+        if len(field_infos) == 1:
+            # No merging necessary, but we still need to make a copy and apply the overrides
+            field_info = copy(field_infos[0])
+            field_info._attributes_set.update(overrides)
+            for k, v in overrides.items():
+                setattr(field_info, k, v)
+            return field_info
+
         new_kwargs: dict[str, Any] = {}
         metadata = {}
         for field_info in field_infos:
