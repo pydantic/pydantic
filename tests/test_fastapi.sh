caea29dda3bf41cae2fa1b9cf530402978c97fd1
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-07-26 08:44:45 -0600

    Re-enable fastapi tests in CI

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -6,11 +6,18 @@ set -e
 cd fastapi
 git fetch --tags
 
-# TODO: Use the proper latest tag once FastAPI stable release is compatible with Pydantic V2.
-# latest_tag=$(git describe --tags --abbrev=0)
-# git switch -d "${latest_tag}"
-git switch "main-pv2"
-
 pip install -r requirements.txt
 
-./scripts/test.sh
+# ./scripts/test.sh accepts arbitrary arguments and passes them to the pytest call.
+# This may be necessary if we make low-consequence changes to pydantic, such as minor changes the details of a JSON
+# schema or the contents of a ValidationError
+#
+# To skip a specific test, add '--deselect path/to/test.py::test_name' to the end of this command
+#
+# To update the list of deselected tests, remove all deselections, run the tests, and re-add any remaining failures
+./scripts/test.sh \
+  --deselect tests/test_filter_pydantic_sub_model_pv2.py::test_validator_is_cloned \
+  --deselect tests/test_multi_body_errors.py::test_jsonable_encoder_requiring_error \
+  --deselect tests/test_multi_body_errors.py::test_put_incorrect_body_multiple \
+
+# TODO: Update the deselections after https://github.com/tiangolo/fastapi/pull/9943 is merged
