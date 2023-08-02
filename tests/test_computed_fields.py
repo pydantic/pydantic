bd1b3f91cad6d242b3b80eb3d890a53a38d4f2e8
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-08-02 14:00:28 +0100

    Improve test

--- tests/test_computed_fields.py
+++ tests/test_computed_fields.py
@@ -732,3 +732,12 @@ def test_generic_computed_field():
 
     assert A[int](x=1).model_dump() == {'x': 1, 'double_x': 2}
     assert A[str](x='abc').model_dump() == {'x': 'abc', 'double_x': 'abcabc'}
+
+    class B(BaseModel, Generic[T]):
+        @computed_field
+        @property
+        def double_x(self) -> T:
+            return 'abc'  # this may not match the annotated return type, and will warn if not
+
+    with pytest.warns(UserWarning, match='Expected `int` but got `str` - serialized value may not be as expected'):
+        B[int]().model_dump()
