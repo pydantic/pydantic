10ab787694ba355bb1c8c50fdc8c61a56bc2f7a6
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2023-08-10 11:57:45 -0400

    Allow parsing numbers w/ underscores (e.g. 1_000) from strings (pydantic/pydantic-core#868)
    Co-authored-by: David Hewitt <1939362+davidhewitt@users.noreply.github.com>
    
    Original-commit-hash: 87b4789a1943514785c967aca2a059e81fd2c2f1

--- tests/validators/test_int.py
+++ tests/validators/test_int.py
@@ -393,3 +393,15 @@ def test_int_key(py_and_json: PyAndJson):
     assert v.validate_test({'1': 1, '2': 2}) == {1: 1, 2: 2}
     with pytest.raises(ValidationError, match='Input should be a valid integer'):
         v.validate_test({'1': 1, '2': 2}, strict=True)
+
+
+def test_string_as_int_with_underscores() -> None:
+    v = SchemaValidator({'type': 'int'})
+    assert v.validate_python('1_000_000') == 1_000_000
+    assert v.validate_json('"1_000_000"') == 1_000_000
+
+    for edge_case in ('_1', '1__0', '1_0_', '1_0__0'):
+        with pytest.raises(ValidationError):
+            v.validate_python(edge_case)
+        with pytest.raises(ValidationError):
+            v.validate_json(f'"{edge_case}"')
