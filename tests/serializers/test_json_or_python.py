ee5722566245453699aece097f608fdb7e5e41b3
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2023-08-21 17:35:34 +0100

    fix json-or-python serializer as JSON object key (pydantic/pydantic-core#903)
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/8bf9e5f0cec009c882f306d47f6c5e35f77421a3

--- tests/serializers/test_json_or_python.py
+++ tests/serializers/test_json_or_python.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from pydantic_core import SchemaSerializer, core_schema
 
 
@@ -17,3 +19,24 @@ def test_json_or_python():
 
     assert s.to_json(0) == b'1'
     assert s.to_python(0) == 2
+
+
+def test_json_or_python_enum_dict_key():
+    # See https://github.com/pydantic/pydantic/issues/6795
+    class MyEnum(str, Enum):
+        A = 'A'
+        B = 'B'
+
+    print(MyEnum('A'))
+
+    s = SchemaSerializer(
+        core_schema.dict_schema(
+            core_schema.json_or_python_schema(
+                core_schema.str_schema(), core_schema.no_info_after_validator_function(MyEnum, core_schema.str_schema())
+            ),
+            core_schema.int_schema(),
+        )
+    )
+
+    assert s.to_json({MyEnum.A: 1, MyEnum.B: 2}) == b'{"A":1,"B":2}'
+    assert s.to_python({MyEnum.A: 1, MyEnum.B: 2}) == {MyEnum.A: 1, MyEnum.B: 2}
