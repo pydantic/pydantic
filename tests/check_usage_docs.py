0e0872c0f0d48249f5f765927e3b9aeee79c525b
Samuel Colvin <s@muelcolvin.com>
2023-08-24 10:11:04 +0100

    update usage links

--- tests/check_usage_docs.py
+++ tests/check_usage_docs.py
@@ -13,24 +13,29 @@ version = re.search(br"VERSION = '(.*)'", version_file.read_bytes()).group(1)
 version_major_minor = b'.'.join(version.split(b'.')[:2])
 
 paths = sys.argv[1:]
-errors = []
 error_count = 0
 for path_str in paths:
     path = ROOT_DIR / path_str
     if path.is_relative_to(PYDANTIC_DIR):
-        file_errors = []
         b = path.read_bytes()
-        for match in re.finditer(br'""" *usage.docs: ?https://docs\.pydantic\.dev/(.+?)/', b, flags=re.I):
-            link_version = match.group(1)
-            if link_version != version_major_minor:
-                line_number = b[: match.start()].count(b'\n') + 1
-                file_errors.append(f'    {line_number}: "{link_version.decode()}"')
-                error_count += 1
-        if file_errors:
-            errors.append(f'  {path_str}:\n' + '\n'.join(file_errors))
 
-if errors:
-    expected = version_major_minor.decode()
-    print(f'Found {error_count} usage docs links pointing to version that differ from "{expected}":')
-    print('\n'.join(errors))
+        changed = 0
+
+        def sub(m: re.Match) -> bytes:
+            global changed
+            link_version = m.group(2)
+            if link_version != version_major_minor:
+                changed += 1
+                return m.group(1) + version_major_minor + b'/'
+            else:
+                return m.group(0)
+
+        b = re.sub(br'(""" *usage.docs: ?https://docs\.pydantic\.dev/)(.+?)/', sub, b, flags=re.I)
+        if changed:
+            error_count += changed
+            path.write_bytes(b)
+            plural = 's' if changed > 1 else ''
+            print(f'{path_str:50} {changed} usage docs link{plural} updated')
+
+if error_count:
     sys.exit(1)
