04dee72e9d5879b677213c32c556ab5d09b71c07
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-09-08 16:34:18 +0200

    📝 Remove `file_types.md` page

--- pydantic/types.py
+++ pydantic/types.py
@@ -695,9 +695,77 @@ class PathType:
 
 
 FilePath = Annotated[Path, PathType('file')]
-"""A path that must point to a file."""
+"""A path that must point to a file.
+
+```py
+from pydantic import BaseModel, FilePath, ValidationError
+
+
+class Model(BaseModel):
+    f: FilePath
+
+
+m = Model(f='docs/usage/types/file_types.md')
+print(m.model_dump())
+#> {'f': PosixPath('docs/usage/types/file_types.md')}
+
+try:
+    Model(f='docs/usage/types/')  # directory
+except ValidationError as e:
+    print(e)
+    '''
+    1 validation error for Model
+    f
+      Path does not point to a file [type=path_not_file, input_value='docs/usage/types/', input_type=str]
+    '''
+
+try:
+    Model(f='docs/usage/types/not-exists-file')
+except ValidationError as e:
+    print(e)
+    '''
+    1 validation error for Model
+    f
+      Path does not point to a file [type=path_not_file, input_value='docs/usage/types/not-exists-file', input_type=str]
+    '''
+```
+"""
 DirectoryPath = Annotated[Path, PathType('dir')]
-"""A path that must point to a directory."""
+"""A path that must point to a directory.
+
+```py
+from pydantic import BaseModel, DirectoryPath, ValidationError
+
+
+class Model(BaseModel):
+    f: DirectoryPath
+
+
+m = Model(f='docs/usage/types/')
+print(m.model_dump())
+#> {'f': PosixPath('docs/usage/types')}
+
+try:
+    Model(f='docs/usage/types/file_types.md')  # file
+except ValidationError as e:
+    print(e)
+    '''
+    1 validation error for Model
+    f
+      Path does not point to a directory [type=path_not_directory, input_value='docs/usage/types/file_types.md', input_type=str]
+    '''
+
+try:
+    Model(f='docs/usage/not-exists-directory')
+except ValidationError as e:
+    print(e)
+    '''
+    1 validation error for Model
+    f
+      Path does not point to a directory [type=path_not_directory, input_value='docs/usage/not-exists-directory', input_type=str]
+    '''
+```
+"""
 NewPath = Annotated[Path, PathType('new')]
 """A path for a new file or directory that must not already exist."""
 
