51ae642cd90be1021611725359005bd5600102b4
sydney-runkle <sydneymarierunkle@gmail.com>
2023-09-11 10:10:03 -0500

    modifying header row + transposing table

--- docs/plugins/exclude_priority_table.py
+++ docs/plugins/exclude_priority_table.py
@@ -10,6 +10,7 @@ from .utils import generate_table_heading, generate_table_row
 class ExcludeSetting:
     name: str
     value: Union[bool, set]
+
     open_nowrap_span: str = '<span style="white-space: nowrap;">'
     close_nowrap_span: str = '</span>'
 
@@ -37,7 +38,7 @@ model_dump_exclude_settings: List[ExcludeSetting] = [
 ]
 model_dump_exclude_x_settings: List[ExcludeSetting] = [
     ExcludeSetting(name='exclude_none', value=True),
-    ExcludeSetting(name='exclude_none', value=False),
+    ExcludeSetting(name='exclude_none', value={}),
     ExcludeSetting(name='exclude_defaults', value=True),
     ExcludeSetting(name='exclude_defaults', value=False),
     ExcludeSetting(name='exclude_unset', value=True),
@@ -46,13 +47,13 @@ model_dump_exclude_x_settings: List[ExcludeSetting] = [
 
 
 def build_exclude_priority_table(
-    field_settings: List[ExcludeSetting], model_dump_settings: List[ExcludeSetting], constructor_kwargs: Dict[str, Any]
+        field_settings: List[ExcludeSetting], model_dump_settings: List[ExcludeSetting],
+        constructor_kwargs: Dict[str, Any]
 ) -> str:
     rows = []
-    for field_setting in field_settings:
+    for model_dump_setting in model_dump_settings:
         col_values = []
-        for model_dump_setting in model_dump_settings:
-
+        for field_setting in field_settings:
             class Dog(BaseModel):
                 """Example class for explanation of `exclude` priority."""
 
@@ -62,9 +63,15 @@ def build_exclude_priority_table(
             result = my_dog.model_dump(**model_dump_setting.kwargs_dict)
             col_values.append(result)
 
-        rows.append(generate_table_row(col_values=[field_setting.markdown_str, *[f'`{x}`' for x in col_values]]))
+        rows.append(generate_table_row(col_values=[model_dump_setting.markdown_str, *[f'`{x}`' for x in col_values]]))
 
-    table_heading = generate_table_heading(col_names=['Field Setting', *[x.markdown_str for x in model_dump_settings]])
+    table_heading = generate_table_heading(
+        col_names=[
+            f'<br></br>`model_dump` **Setting**',
+            f'`Field` **Setting**<br></br>{field_settings[0].markdown_str}',
+            *[f'<br></br>{x.markdown_str}' for x in field_settings[1:]]
+        ]
+    )
     table = ''.join([table_heading, *rows])
 
     return table
