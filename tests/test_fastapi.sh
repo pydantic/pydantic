d842579d90016a76f68a837026363a5ab784a607
sydney-runkle <sydneymarierunkle@gmail.com>
2023-10-09 10:51:09 -0500

    adding link to dependency issue

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -8,6 +8,7 @@ git fetch --tags
 git checkout 0.99.1
 
 # temp fix for flask dependency issue
+# see: https://stackoverflow.com/questions/77213053/importerror-cannot-import-name-url-quote-from-werkzeug-urls
 pip install Werkzeug==2.2.2
 pip install -r requirements.txt
 
