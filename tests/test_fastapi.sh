2e939dc3bfb88f54efb66f8f1a031ff22e4f9865
sydney-runkle <54324534+sydney-runkle@users.noreply.github.com>
2023-10-09 11:33:06 -0500

    Update v1 fixes ci to only deploy docs on release (#7740)

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -7,6 +7,9 @@ cd fastapi
 git fetch --tags
 git checkout 0.99.1
 
+# temp fix for flask dependency issue
+# see: https://stackoverflow.com/questions/77213053/importerror-cannot-import-name-url-quote-from-werkzeug-urls
+pip install Werkzeug==2.2.2
 pip install -r requirements.txt
 
 ./scripts/test.sh
