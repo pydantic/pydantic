0f4d0dc192621eee1d846e3607846584f7535316
alexdrydew <aleksey.suhorosov@gmail.com>
2023-10-21 16:10:47 +0200

    Import get_origin from typing_extensions

--- pydantic/_internal/_core_utils.py
+++ pydantic/_internal/_core_utils.py
@@ -10,12 +10,11 @@ from typing import (
     Union,
     _GenericAlias,  # type: ignore
     cast,
-    get_origin,
 )
 
 from pydantic_core import CoreSchema, core_schema
 from pydantic_core import validate_core_schema as _validate_core_schema
-from typing_extensions import TypeAliasType, TypeGuard, get_args
+from typing_extensions import TypeAliasType, TypeGuard, get_args, get_origin
 
 from . import _repr
 
