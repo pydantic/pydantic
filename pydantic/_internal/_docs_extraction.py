a044bcea661c513077a11d51eb950b9a4ed65869
Viicos <65306057+Viicos@users.noreply.github.com>
2023-10-25 21:11:31 +0200

    Fix handling of dataclasses with Python>=3.11

--- pydantic/_internal/_docs_extraction.py
+++ pydantic/_internal/_docs_extraction.py
@@ -45,12 +45,16 @@ def _extract_source_from_frame(cls_name: str) -> list[str] | None:
             # we don't want to error here.
             pass
         else:
-            if (
-                isinstance(lnum, int)
-                and len(lines) >= lnum
-                and re.match(fr'class\s+{cls_name}', lines[lnum - 1].strip())
-            ):
-                return inspect.getblock(lines[lnum - 1 :])  # type: ignore
+            if isinstance(lnum, int) and len(lines) >= lnum:
+                block_lines = inspect.getblock(lines[lnum - 1 :])
+                filtered_lines = [
+                    line
+                    for line in block_lines
+                    if not line.strip().startswith('@')  # Strip out decorators
+                    if not line.strip() == ''  # And potential empty lines between the decorators and the class def
+                ]
+                if re.match(fr'class\s+{cls_name}', filtered_lines[0].strip()):
+                    return block_lines  # type: ignore
         frame = frame.f_back
 
 
