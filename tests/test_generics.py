17fd1f9dab3a696ebb5269a02228130b7aef4036
sydney-runkle <sydneymarierunkle@gmail.com>
2023-10-25 20:57:37 -0500

    fix for generics with not required in typed dict

--- tests/test_generics.py
+++ tests/test_generics.py
@@ -35,8 +35,10 @@ from pydantic_core import CoreSchema, core_schema
 from typing_extensions import (
     Annotated,
     Literal,
+    NotRequired,
     OrderedDict,
     ParamSpec,
+    TypedDict,
     TypeVarTuple,
     Unpack,
     get_args,
@@ -53,6 +55,7 @@ from pydantic import (
     PositiveInt,
     PydanticSchemaGenerationError,
     PydanticUserError,
+    TypeAdapter,
     ValidationError,
     ValidationInfo,
     computed_field,
@@ -2849,3 +2852,16 @@ def test_mix_default_and_constraints() -> None:
 
         class _(BaseModel, Generic[T]):
             x: T
+
+
+def test_generic_with_not_required_in_typed_dict() -> None:
+    T = TypingExtensionsTypeVar('T')
+
+    class FooStr(TypedDict):
+        type: NotRequired[str]
+
+    class FooGeneric(TypedDict, Generic[T]):
+        type: NotRequired[T]
+
+    assert TypeAdapter(FooStr)
+    assert TypeAdapter(FooGeneric[str])
