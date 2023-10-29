75986a97930a1b529f664833b08e535cbf697d37
Alex Hall <alex.mojaki@gmail.com>
2023-10-29 22:54:23 +0200

    Add eval_type_backport to handle union operator in older Pythons

--- pydantic/_internal/_generate_schema.py
+++ pydantic/_internal/_generate_schema.py
@@ -637,7 +637,7 @@ class GenerateSchema:
         # class Model(BaseModel):
         #   x: SomeImportedTypeAliasWithAForwardReference
         try:
-            obj = _typing_extra.evaluate_fwd_ref(obj, globalns=self._types_namespace)
+            obj = _typing_extra.eval_type_backport(obj, globalns=self._types_namespace, localns=None)
         except NameError as e:
             raise PydanticUndefinedAnnotation.from_name_error(e) from e
 
