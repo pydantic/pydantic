89c84a843ed29bb068aaedfe0ffb07c1c5771834
David Hewitt <mail@davidhewitt.io>
2023-11-07 10:06:28 +0000

    Update `pydantic-core` to 2.12.0

--- docs/concepts/models.md
+++ docs/concepts/models.md
@@ -457,7 +457,7 @@ except ValidationError as e:
     print(e)
     """
     1 validation error for User
-      Invalid JSON: expected value at line 1 column 1 [type=json_invalid, input_value='Invalid JSON', input_type=str]
+      Invalid JSON: expected ident at line 1 column 3 [type=json_invalid, input_value='Invalid JSON', input_type=str]
     """
 ```
 
--- tests/test_types.py
+++ tests/test_types.py
@@ -3060,7 +3060,7 @@ ANY_THING = object()
             ],
         ),
         (dict(max_digits=2, decimal_places=2), Decimal('0.99'), Decimal('0.99')),
-        pytest.param(
+        (
             dict(max_digits=2, decimal_places=1),
             Decimal('0.99'),
             [
@@ -3074,7 +3074,6 @@ ANY_THING = object()
                     },
                 }
             ],
-            marks=pytest.mark.xfail(reason='Needs new release of pydantic-core'),
         ),
         (
             dict(max_digits=3, decimal_places=1),
