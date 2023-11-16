575dcd98d7cdc49d1427df8101f473ae6ced148e
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2023-11-16 09:33:14 -0600

    Adding short commit to `version_info` (#8145)
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>

--- pydantic/version.py
+++ pydantic/version.py
@@ -18,11 +18,14 @@ def version_short() -> str:
 def version_info() -> str:
     """Return complete version information for Pydantic and its dependencies."""
     import importlib.metadata as importlib_metadata
+    import os
     import platform
     import sys
     from pathlib import Path
 
     import pydantic_core._pydantic_core as pdc
+
+    from ._internal import _git as git
 
     # get data about packages that are closely related to pydantic, use pydantic or often conflict with pydantic
     package_names = {
@@ -41,6 +44,11 @@ def version_info() -> str:
         if name in package_names:
             related_packages.append(f'{name}-{dist.version}')
 
+    pydantic_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    most_recent_commit = (
+        git.git_revision(pydantic_dir) if git.is_git_repo(pydantic_dir) and git.have_git() else 'unknown'
+    )
+
     info = {
         'pydantic version': VERSION,
         'pydantic-core version': pdc.__version__,
@@ -49,6 +57,7 @@ def version_info() -> str:
         'python version': sys.version,
         'platform': platform.platform(),
         'related packages': ' '.join(related_packages),
+        'commit': most_recent_commit,
     }
     return '\n'.join('{:>30} {}'.format(k + ':', str(v).replace('\n', ' ')) for k, v in info.items())
 
