c2d9d229542325fbbca9ff036396a24f1458cc20
Alex Hall <alex.mojaki@gmail.com>
2023-11-22 21:01:03 +0200

    Use `eval_type_backport` package

--- pydantic/_internal/_typing_extra.py
+++ pydantic/_internal/_typing_extra.py
@@ -1,12 +1,10 @@
 """Logic for interacting with type annotations, mostly extensions, shims and hacks to wrap python's typing module."""
 from __future__ import annotations as _annotations
 
-import ast
 import dataclasses
 import sys
 import types
 import typing
-import uuid
 from collections.abc import Callable
 from functools import partial
 from types import GetSetDescriptorType
@@ -233,63 +231,11 @@ def is_unsupported_types_for_union_error(e: TypeError) -> bool:
     return str(e).startswith('unsupported operand type(s) for |: ')
 
 
-class _UnionTransformer(ast.NodeTransformer):
-    """Transforms `X | Y` into `Union[X, Y]` if `X | Y` is not supported."""
-
-    def __init__(self, globalns: dict[str, Any] | None, localns: dict[str, Any] | None):
-        # This logic for handling Nones is copied from typing.ForwardRef._evaluate
-        if globalns is None and localns is None:
-            globalns = localns = {}
-        elif globalns is None:
-            assert localns is not None  # apparently pyright doesn't infer this automatically
-            globalns = localns
-        elif localns is None:
-            assert globalns is not None  # apparently pyright doesn't infer this automatically
-            localns = globalns
-
-        self.typing_name = f'typing_{uuid.uuid4().hex}'
-        self.globalns = globalns
-        self.localns = {**localns, self.typing_name: typing}
-
-    def eval_type(self, node: ast.AST) -> Any:
-        if not isinstance(node, ast.Expression):
-            node = ast.copy_location(ast.Expression(node), node)
-        ref = typing.ForwardRef(ast.dump(node))
-        ref.__forward_code__ = compile(node, '<node>', 'eval')
-        return typing._eval_type(  # type: ignore
-            ref, self.globalns, self.localns
-        )
-
-    def visit_BinOp(self, node) -> ast.BinOp | ast.Subscript:
-        if isinstance(node.op, ast.BitOr):
-            left_node = self.visit(node.left)
-            right_node = self.visit(node.right)
-            left_val = self.eval_type(left_node)
-            right_val = self.eval_type(right_node)
-            try:
-                _ = left_val | right_val
-            except TypeError as e:
-                if not is_unsupported_types_for_union_error(e):
-                    raise
-                # Replace `left | right` with `typing.Union[left, right]`
-                replacement = ast.Subscript(
-                    value=ast.Attribute(
-                        value=ast.Name(id=self.typing_name, ctx=ast.Load()),
-                        attr='Union',
-                        ctx=ast.Load(),
-                    ),
-                    slice=ast.Index(value=ast.Tuple(elts=[left_node, right_node], ctx=ast.Load())),
-                    ctx=ast.Load(),
-                )
-                return ast.fix_missing_locations(replacement)
-
-        return node
-
-
 def eval_type_backport(
     value: Any, globalns: dict[str, Any] | None = None, localns: dict[str, Any] | None = None
 ) -> Any:
-    """Like `typing._eval_type`, but lets older Python versions use newer typing features.
+    """Like `typing._eval_type`, but falls back to the `eval_type_backport` package if it's
+    installed to let older Python versions use newer typing features.
     Currently this just means that `X | Y` is converted to `Union[X, Y]` if `X | Y` is not supported.
     This would also be the place to add support for `list[int]` instead of `List[int]` etc.
     """
@@ -300,10 +246,9 @@ def eval_type_backport(
     except TypeError as e:
         if not (isinstance(value, typing.ForwardRef) and is_unsupported_types_for_union_error(e)):
             raise
-        tree = ast.parse(value.__forward_arg__, mode='eval')
-        transformer = _UnionTransformer(globalns, localns)
-        tree = transformer.visit(tree)
-        return transformer.eval_type(tree)
+        from eval_type_backport import eval_type_backport
+
+        return eval_type_backport(value, globalns, localns, try_default=False)
 
 
 def get_function_type_hints(
