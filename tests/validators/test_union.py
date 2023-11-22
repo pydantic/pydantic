03cec923892c54054991785a009444f2f2409bd8
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2023-11-22 03:29:18 -0600

    Avoid using `?` with `get_item` to handle unhashable inputs properly (pydantic/pydantic-core#1089)
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>
    
    Original-commit-hash: 5b63e7a89df6d48ca0b48b4a885c96f7c2cfd413

--- tests/validators/test_union.py
+++ tests/validators/test_union.py
@@ -771,3 +771,35 @@ def test_int_not_coerced_to_enum():
     assert validator.validate_python(1) is not BinaryEnum.ONE
     assert validator.validate_python(BinaryEnum.ZERO) is BinaryEnum.ZERO
     assert validator.validate_python(BinaryEnum.ONE) is BinaryEnum.ONE
+
+
+def test_model_and_literal_union() -> None:
+    # see https://github.com/pydantic/pydantic/issues/8183
+    class ModelA:
+        pass
+
+    validator = SchemaValidator(
+        {
+            'type': 'union',
+            'choices': [
+                {
+                    'type': 'model',
+                    'cls': ModelA,
+                    'schema': {
+                        'type': 'model-fields',
+                        'fields': {
+                            'a': {'type': 'model-field', 'schema': {'type': 'int'}},
+                        },
+                    },
+                },
+                {'type': 'literal', 'expected': [True]},
+            ],
+        }
+    )
+
+    # validation against Literal[True] fails bc of the unhashable dict
+    # A ValidationError is raised, not a ValueError, which allows the validation against the union to continue
+    m = validator.validate_python({'a': 42})
+    assert isinstance(m, ModelA)
+    assert m.a == 42
+    assert validator.validate_python(True) is True
