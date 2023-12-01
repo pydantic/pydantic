c7c365fd4f986a5600c121f00b10a55487f89f4f
Viicos <65306057+Viicos@users.noreply.github.com>
2023-12-01 12:07:49 +0100

    Remove support for root models in mypy plugin

--- pydantic/mypy.py
+++ pydantic/mypy.py
@@ -475,7 +475,7 @@ class PydanticModelTransformer:
                 return False
 
         is_settings = any(base.fullname == BASESETTINGS_FULLNAME for base in info.mro[:-1])
-        self.add_initializer(fields, config, is_settings, is_root_model)
+        self.add_initializer(fields, config, is_settings)
         self.add_model_construct_method(fields, config, is_settings)
         try:
             self.set_frozen(fields, frozen=config.frozen is True)
@@ -840,7 +840,7 @@ class PydanticModelTransformer:
         return default
 
     def add_initializer(
-        self, fields: list[PydanticModelField], config: ModelConfigData, is_settings: bool, is_root_model: bool
+        self, fields: list[PydanticModelField], config: ModelConfigData, is_settings: bool
     ) -> None:
         """Adds a fields-aware `__init__` method to the class.
 
@@ -860,9 +860,6 @@ class PydanticModelTransformer:
                 use_alias=use_alias,
                 is_settings=is_settings,
             )
-            if is_root_model:
-                # convert root argument to positional argument
-                args[0].kind = ARG_POS if args[0].kind == ARG_NAMED else ARG_OPT
 
             if is_settings:
                 base_settings_node = self._api.lookup_fully_qualified(BASESETTINGS_FULLNAME).node
