05e039794bebb72e8decb012d5da5027ba55881c
Alex Hall <alex.mojaki@gmail.com>
2023-12-08 16:54:57 -0500

    move import, clarify names

--- pydantic/validate_call_decorator.py
+++ pydantic/validate_call_decorator.py
@@ -1,6 +1,7 @@
 """Decorator for validating function calls."""
 from __future__ import annotations as _annotations
 
+import functools
 from typing import TYPE_CHECKING, Any, Callable, TypeVar, overload
 
 from ._internal import _validate_call
@@ -50,17 +51,15 @@ def validate_call(
         if isinstance(function, (classmethod, staticmethod)):
             name = type(function).__name__
             raise TypeError(f'The `@{name}` decorator should be applied after `@validate_call` (put `@{name}` on top)')
-        v = _validate_call.ValidateCallWrapper(function, config, validate_return)
-
-        import functools
+        validate_call_wrapper = _validate_call.ValidateCallWrapper(function, config, validate_return)
 
         @functools.wraps(function)
-        def wrapper(*args, **kwargs):
-            return v(*args, **kwargs)
+        def wrapper_function(*args, **kwargs):
+            return validate_call_wrapper(*args, **kwargs)
 
-        wrapper.raw_function = function  # type: ignore
+        wrapper_function.raw_function = function  # type: ignore
 
-        return wrapper  # type: ignore
+        return wrapper_function  # type: ignore
 
     if __func:
         return validate(__func)
