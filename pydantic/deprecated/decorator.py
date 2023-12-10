c300dbaba39b0307987fa5935c55133be61ba253
Viicos <65306057+Viicos@users.noreply.github.com>
2023-12-10 20:55:28 +0100

    Revert change on overload

--- pydantic/deprecated/decorator.py
+++ pydantic/deprecated/decorator.py
@@ -1,7 +1,7 @@
 from functools import wraps
-from typing import TYPE_CHECKING, Any, Callable, Dict, List, Mapping, Optional, Tuple, Type, TypeVar, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Mapping, Optional, Tuple, Type, TypeVar, Union, overload
 
-from typing_extensions import deprecated, overload
+from typing_extensions import deprecated
 
 from .._internal import _config, _typing_extra
 from ..alias_generators import to_pascal
