c3541c901979716da9c7734376c98acde147c931
sydney-runkle <sydneymarierunkle@gmail.com>
2024-01-19 10:02:10 -0500

    helpful note

--- docs/extra/redirects.js
+++ docs/extra/redirects.js
@@ -96,6 +96,7 @@ function sanitizeURL(url) {
 }
 
 function main() {
+  // escape nonstandard characters to avoid XSS attacks
   const fragment = location.hash.substr(1).replace(/[^a-zA-Z0-9-_]/g, '') 
   if (fragment === '' || location.pathname !== '/') {
     // no fragment or not called from root
