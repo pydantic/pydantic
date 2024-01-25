ac14a4278d992f2df950b66d31e105e8f0bbd64c
sydney-runkle <sydneymarierunkle@gmail.com>
2024-01-25 10:23:24 -0600

    meh fix

--- pydantic/_internal/_generate_schema.py
+++ pydantic/_internal/_generate_schema.py
@@ -1390,25 +1390,13 @@ class GenerateSchema:
         item_type = self._get_first_arg_or_any(sequence_type)
         item_type_schema = self.generate_schema(item_type)
         list_schema = core_schema.list_schema(item_type_schema)
-        tuple_schema = core_schema.tuple_schema([item_type_schema], variadic_item_index=0)
 
         python_schema = core_schema.is_instance_schema(typing.Sequence, cls_repr='Sequence')
         if item_type != Any:
             from ._validators import sequence_validator
 
             python_schema = core_schema.chain_schema(
-                [
-                    python_schema,
-                    core_schema.no_info_wrap_validator_function(
-                        sequence_validator,
-                        core_schema.union_schema(
-                            [
-                                list_schema,
-                                tuple_schema,
-                            ]
-                        ),
-                    ),
-                ],
+                [python_schema, core_schema.no_info_wrap_validator_function(sequence_validator, list_schema)],
             )
         return core_schema.json_or_python_schema(json_schema=list_schema, python_schema=python_schema)
 
