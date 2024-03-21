bc06bc08340f2cbdc1189aa81e752e8b4cd58b38
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-03-21 10:36:42 -0500

    Uprev `speedate` (pydantic/pydantic-core#1244)
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/193f5d7a8627c988b1c7fab9a3712c779afbbe94

--- tests/serializers/test_timedelta.py
+++ tests/serializers/test_timedelta.py
@@ -14,8 +14,8 @@ def test_timedelta():
     v = SchemaSerializer(core_schema.timedelta_schema())
     assert v.to_python(timedelta(days=2, hours=3, minutes=4)) == timedelta(days=2, hours=3, minutes=4)
 
-    assert v.to_python(timedelta(days=2, hours=3, minutes=4), mode='json') == 'P2DT11040S'
-    assert v.to_json(timedelta(days=2, hours=3, minutes=4)) == b'"P2DT11040S"'
+    assert v.to_python(timedelta(days=2, hours=3, minutes=4), mode='json') == 'P2DT3H4M'
+    assert v.to_json(timedelta(days=2, hours=3, minutes=4)) == b'"P2DT3H4M"'
 
     with pytest.warns(
         UserWarning, match='Expected `timedelta` but got `int` - serialized value may not be as expected'
@@ -39,8 +39,8 @@ def test_timedelta_float():
 def test_timedelta_key():
     v = SchemaSerializer(core_schema.dict_schema(core_schema.timedelta_schema(), core_schema.int_schema()))
     assert v.to_python({timedelta(days=2, hours=3, minutes=4): 1}) == {timedelta(days=2, hours=3, minutes=4): 1}
-    assert v.to_python({timedelta(days=2, hours=3, minutes=4): 1}, mode='json') == {'P2DT11040S': 1}
-    assert v.to_json({timedelta(days=2, hours=3, minutes=4): 1}) == b'{"P2DT11040S":1}'
+    assert v.to_python({timedelta(days=2, hours=3, minutes=4): 1}, mode='json') == {'P2DT3H4M': 1}
+    assert v.to_json({timedelta(days=2, hours=3, minutes=4): 1}) == b'{"P2DT3H4M":1}'
 
 
 @pytest.mark.skipif(not pandas, reason='pandas not installed')
@@ -48,5 +48,5 @@ def test_pandas():
     v = SchemaSerializer(core_schema.timedelta_schema())
     d = pandas.Timestamp('2023-01-01T02:00:00Z') - pandas.Timestamp('2023-01-01T00:00:00Z')
     assert v.to_python(d) == d
-    assert v.to_python(d, mode='json') == 'PT7200S'
-    assert v.to_json(d) == b'"PT7200S"'
+    assert v.to_python(d, mode='json') == 'PT2H'
+    assert v.to_json(d) == b'"PT2H"'
