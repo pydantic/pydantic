548feec7d3cc2e9c12b73971e8b97722ec7e2b8c
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-03-27 11:33:04 -0500

    Add benchmark decorators (#9123)

--- tests/benchmarks/test_schema_build.py
+++ tests/benchmarks/test_schema_build.py
@@ -1,5 +1,6 @@
 from typing import Literal, Union
 
+import pytest
 from typing_extensions import Annotated
 
 from pydantic import BaseModel, Discriminator
@@ -25,6 +26,7 @@ class NestedModel(BaseModel):
     animal: Annotated[Union[Cat, Dog], Discriminator('type')]
 
 
+@pytest.mark.benchmark
 def test_construct_schema():
     @dataclass(frozen=True, kw_only=True)
     class Root:
