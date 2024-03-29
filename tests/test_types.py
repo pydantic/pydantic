95b3114a520fcb5248a0615dc106e529538db9be
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-03-29 13:11:58 -0700

    Update tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -5182,21 +5182,21 @@ def test_none_literal():
         my_json_none='null',
     )
 
-    # insert_assert(Model.model_json_schema())
+    #insert_assert(Model.model_json_schema())
     assert Model.model_json_schema() == {
         'title': 'Model',
         'type': 'object',
         'properties': {
-            'my_none': {'const': None, 'enum': [None], 'title': 'My None'},
-            'my_none_list': {'items': {'const': None, 'enum': [None]}, 'title': 'My None List', 'type': 'array'},
+            'my_none': {'const': None, 'enum': [None], 'title': 'My None', 'type': 'null'},
+            'my_none_list': {'items': {'const': None, 'enum': [None], 'type': 'null'}, 'title': 'My None List', 'type': 'array'},
             'my_none_dict': {
-                'additionalProperties': {'const': None, 'enum': [None]},
+                'additionalProperties': {'const': None, 'enum': [None], 'type': 'null'},
                 'title': 'My None Dict',
                 'type': 'object',
             },
             'my_json_none': {
                 'contentMediaType': 'application/json',
-                'contentSchema': {'const': None, 'enum': [None]},
+                'contentSchema': {'const': None, 'enum': [None], 'type': 'null'},
                 'title': 'My Json None',
                 'type': 'string',
             },
