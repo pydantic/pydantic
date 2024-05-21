275235bd7a5bb1f191c643ab63e0c3ad1b029d87
sydney-runkle <sydneymarierunkle@gmail.com>
2024-05-21 18:32:40 -0400

    add test

--- tests/test_callable.py
+++ tests/test_callable.py
@@ -2,8 +2,9 @@ import sys
 from typing import Callable
 
 import pytest
+from typing_extensions import Annotated
 
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, Field, TypeAdapter, ValidationError
 
 collection_callable_types = [Callable, Callable[[int], int]]
 if sys.version_info >= (3, 9):
@@ -28,3 +29,14 @@ def test_non_callable(annotation):
 
     with pytest.raises(ValidationError):
         Model(callback=1)
+
+
+def test_description_in_callable_schema() -> None:
+    FooType = Annotated[int, Field(default=1, description='foo type description')]
+
+    def func(foo: FooType):
+        ...
+
+    # Description is lost 🤔
+    json_schema = TypeAdapter(func).json_schema()
+    assert json_schema['properties']['foo']['description'] == 'foo type description'
