d06a6f66325ad70004e3207628850f34366f3662
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-06-03 10:21:44 -0500

    Uprev `jiter` to v0.4.1 and `pydantic-core` to v2.18.4 (pydantic/pydantic-core#1306)
    Co-authored-by: David Hewitt <david.hewitt@pydantic.dev>
    
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/37ad925981226755410e8a952b92ee8838b4e732

--- src/validators/json.rs
+++ src/validators/json.rs
@@ -2,7 +2,7 @@ use pyo3::intern;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 
-use jiter::JsonValue;
+use jiter::{JsonValue, PartialMode, PythonParse};
 
 use crate::errors::{ErrorType, ErrorTypeDefaults, ValError, ValLineError, ValResult};
 use crate::input::{EitherBytes, Input, InputType, ValidationMatch};
@@ -64,7 +64,15 @@ impl Validator for JsonValidator {
                 validator.validate(py, &json_value, &mut json_state)
             }
             None => {
-                let obj = jiter::python_parse(py, json_bytes, true, state.cache_str(), false)
+                let parse_builder = PythonParse {
+                    allow_inf_nan: true,
+                    cache_mode: state.cache_str(),
+                    partial_mode: PartialMode::Off,
+                    catch_duplicate_keys: false,
+                    lossless_floats: false,
+                };
+                let obj = parse_builder
+                    .python_parse(py, json_bytes)
                     .map_err(|e| map_json_err(input, e, json_bytes))?;
                 Ok(obj.unbind())
             }
