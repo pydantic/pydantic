cbebbba54454d695f95201bf468f69c1493b04b1
sydney-runkle <sydneymarierunkle@gmail.com>
2024-06-04 11:49:21 -0400

    patch fix for to_camel

--- pydantic/alias_generators.py
+++ pydantic/alias_generators.py
@@ -3,6 +3,10 @@ import re
 
 __all__ = ('to_pascal', 'to_camel', 'to_snake')
 
+# TODO: in V3, change the argument names to be more descriptive
+# Generally, don't only convert from snake_case, or name the functions
+# more specifically like snake_to_camel.
+
 
 def to_pascal(snake: str) -> str:
     """Convert a snake_case string to PascalCase.
@@ -26,6 +30,10 @@ def to_camel(snake: str) -> str:
     Returns:
         The converted camelCase string.
     """
+    # If the string is already in camelCase, return it as it is
+    if re.match('^[a-z]+[A-Za-z0-9]*$', snake):
+        return snake
+
     camel = to_pascal(snake)
     return re.sub('(^_*[A-Z])', lambda m: m.group(1).lower(), camel)
 
@@ -42,5 +50,6 @@ def to_snake(camel: str) -> str:
     snake = re.sub(r'([a-zA-Z])([0-9])', lambda m: f'{m.group(1)}_{m.group(2)}', camel)
     snake = re.sub(r'([a-z0-9])([A-Z])', lambda m: f'{m.group(1)}_{m.group(2)}', snake)
     snake = re.sub(r'([A-Z]+)([A-Z][a-z])', lambda m: f'{m.group(1)}_{m.group(2)}', snake)
+    # Replace hyphens with underscores to handle kebab-case
     snake = snake.replace('-', '_')
     return snake.lower()
