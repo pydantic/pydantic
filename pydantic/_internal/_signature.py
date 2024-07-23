2a9d2ba1ba63612d085dabea3cec1472385cc29c
Saurabh Misra <misra.saurabh1@gmail.com>
2024-07-23 15:11:48 -0700

    Fix formatting

--- pydantic/_internal/_signature.py
+++ pydantic/_internal/_signature.py
@@ -26,12 +26,9 @@ def _field_name_for_signature(field_name: str, field_info: FieldInfo) -> str:
     Returns:
         The correct name to use when generating a signature.
     """
-
     if isinstance(field_info.alias, str) and is_valid_identifier(field_info.alias):
         return field_info.alias
-    if isinstance(field_info.validation_alias, str) and is_valid_identifier(
-        field_info.validation_alias
-    ):
+    if isinstance(field_info.validation_alias, str) and is_valid_identifier(field_info.validation_alias):
         return field_info.validation_alias
 
     return field_name
