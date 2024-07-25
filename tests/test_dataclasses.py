7dd6f37c0e626eabe187d4c2837e920ef1059b4e
kc0506 <kchong0506@gmail.com>
2024-07-25 12:28:44 +0800

    Add test case

--- tests/test_dataclasses.py
+++ tests/test_dataclasses.py
@@ -2944,3 +2944,29 @@ def test_validation_works_for_cyclical_forward_refs() -> None:
         x: Union[X, None]
 
     assert Y(x={'y': None}).x.y is None
+
+@pytest.mark.parametrize("field_fn_unset", [Field, dataclasses.field])
+@pytest.mark.parametrize("field_fn2", [Field, dataclasses.field])
+def test_annotated_with_field_default_factory( field_fn_unset, field_fn2) -> None:
+    """
+    https://github.com/pydantic/pydantic/issues/9947
+    """
+
+    # wrap in lambda to create new instance for different fields
+    field1 =lambda: pydantic.Field(default_factory=lambda: 1)  # `Annotated` only works with pydantic field
+    field2 = lambda:field_fn2(default_factory=lambda: 2)
+
+    @pydantic.dataclasses.dataclass()
+    class A:
+        a: Annotated[int, field1()]
+        b: Annotated[int, field1()] = field_fn_unset()
+        c: Annotated[int, field2(), field1()] = field_fn_unset()
+        d: Annotated[int, field_fn_unset] = field2()
+        e: int = field2()
+        f: Annotated[int, field1()] = field2()
+
+    instance = A()
+    field_names = ("a", "b", "c", "d", "e", "f")
+    results = (1, 1, 1, 2, 2, 2)
+    for field_name,result in zip(field_names, results):
+        assert getattr(instance, field_name) == result
