05019480d347e355c8ea032684eef9282a1dc0fe
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-07-31 14:56:05 -0500

    Initial start at improving import times for modules, using caching primarily (#10009)
    Co-authored-by: David Montague <35119617+dmontagu@users.noreply.github.com>

--- pydantic/__init__.py
+++ pydantic/__init__.py
@@ -385,6 +385,7 @@ _dynamic_imports: 'dict[str, tuple[str, str]]' = {
     'schema_json_of': (__spec__.parent, '.deprecated.tools'),
     'FieldValidationInfo': ('pydantic_core', '.core_schema'),
 }
+_deprecated_dynamic_imports = {'FieldValidationInfo'}
 
 _getattr_migration = getattr_migration(__name__)
 
@@ -399,10 +400,17 @@ def __getattr__(attr_name: str) -> object:
     from importlib import import_module
 
     if module_name == '__module__':
-        return import_module(f'.{attr_name}', package=package)
+        result = import_module(f'.{attr_name}', package=package)
+        globals()[attr_name] = result
+        return result
     else:
         module = import_module(module_name, package=package)
-        return getattr(module, attr_name)
+        result = getattr(module, attr_name)
+        g = globals()
+        for k, (v_package, v_module_name) in _dynamic_imports.items():
+            if v_module_name == module_name and k not in _deprecated_dynamic_imports:
+                g[k] = getattr(module, k)
+        return result
 
 
 def __dir__() -> 'list[str]':
