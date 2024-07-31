7faea5f6f54ee7cd9c2911bd28f329ed27383118
sydney-runkle <sydneymarierunkle@gmail.com>
2024-07-31 18:29:38 -0400

    don't need functools cache

--- pydantic/_internal/_import_utils.py
+++ pydantic/_internal/_import_utils.py
@@ -1,11 +1,9 @@
-from functools import cache
 from importlib import import_module
 from typing import Any
 
 _cache = {}
 
 
-@cache
 def get_cached_component(module_name: str, component_name: str) -> Any:
     if key := (module_name, component_name) not in _cache:
         module = import_module(module_name)
