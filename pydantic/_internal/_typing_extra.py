f55fef233cf7b37df5f6bc696b8e60b3a844e2ee
Viicos <65306057+Viicos@users.noreply.github.com>
2024-08-02 17:50:00 +0200

    Check for Python versions

--- pydantic/_internal/_typing_extra.py
+++ pydantic/_internal/_typing_extra.py
@@ -266,26 +266,37 @@ def eval_type_backport(
             )
     except TypeError as e:
         if isinstance(value, typing.ForwardRef):
-            try:
-                from eval_type_backport import eval_type_backport
-            except ImportError:
-                raise TypeError(
-                    f'Unable to evaluate type annotation {value.__forward_arg__!r}. If you are making use '
-                    'of the new typing syntax (unions using `|` since Python 3.10 or builtins subscripting '
-                    'since Python 3.9), you should either replace the use of new syntax with the existing '
-                    '`typing` constructs or install the `eval_type_backport` package. Otherwise, it might be '
-                    "that the type being used isn't subscriptable."
-                ) from e
+            if is_backport_fixable_error(e):
+                try:
+                    from eval_type_backport import eval_type_backport
+                except ImportError:
+                    raise TypeError(
+                        f'Unable to evaluate type annotation {value.__forward_arg__!r}. If you are making use '
+                        'of the new typing syntax (unions using `|` since Python 3.10 or builtins subscripting '
+                        'since Python 3.9), you should either replace the use of new syntax with the existing '
+                        '`typing` constructs or install the `eval_type_backport` package. Otherwise, it might be '
+                        "that the type being used isn't subscriptable."
+                    ) from e
 
-            try:
                 return eval_type_backport(value, globalns, localns, try_default=False)
-            except TypeError as e:
-                raise TypeError(
-                    f'Unable to evaluate type annotation {value.__forward_arg__!r}. '
-                    "It might be that the type being used isn't subscriptable."
-                ) from e
+
+            raise TypeError(
+                f'Unable to evaluate type annotation {value.__forward_arg__!r}. '
+                "It might be that the type being used isn't subscriptable."
+            ) from e
 
         raise e
+
+
+def is_backport_fixable_error(e: TypeError) -> bool:
+    msg = str(e)
+
+    return (
+        sys.version_info <= (3, 8)
+        and msg.startswith('unsupported operand type(s) for |: ')
+        or sys.version_info <= (3, 9)
+        and "' object is not subscriptable" in msg
+    )
 
 
 def get_function_type_hints(
