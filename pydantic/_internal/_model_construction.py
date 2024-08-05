c71511799f962a7f2743224e7c8d3c521ec597e8
sydney-runkle <sydneymarierunkle@gmail.com>
2024-08-05 14:01:01 -0400

    removing weakref stuff

--- pydantic/_internal/_model_construction.py
+++ pydantic/_internal/_model_construction.py
@@ -6,7 +6,6 @@ import builtins
 import operator
 import typing
 import warnings
-import weakref
 from abc import ABCMeta
 from functools import lru_cache, partial
 from types import FunctionType
@@ -191,10 +190,7 @@ class ModelMetaclass(ABCMeta):
             for name, obj in private_attributes.items():
                 obj.__set_name__(cls, name)
 
-            if __pydantic_reset_parent_namespace__:
-                parent_namespace = parent_frame_namespace() or {}
-            else:
-                parent_namespace = {}
+            parent_namespace = parent_frame_namespace() or {} if __pydantic_reset_parent_namespace__ else {}
             types_namespace = get_cls_types_namespace(cls, parent_namespace)
             set_model_fields(cls, bases, config_wrapper, types_namespace)
 
@@ -622,77 +618,6 @@ class _DeprecatedFieldDescriptor:
     # as `BaseModel.__setattr__` is defined and takes priority.
     def __set__(self, obj: Any, value: Any) -> NoReturn:
         raise AttributeError(self.field_name)
-
-
-class _PydanticWeakRef:
-    """Wrapper for `weakref.ref` that enables `pickle` serialization.
-
-    Cloudpickle fails to serialize `weakref.ref` objects due to an arcane error related
-    to abstract base classes (`abc.ABC`). This class works around the issue by wrapping
-    `weakref.ref` instead of subclassing it.
-
-    See https://github.com/pydantic/pydantic/issues/6763 for context.
-
-    Semantics:
-        - If not pickled, behaves the same as a `weakref.ref`.
-        - If pickled along with the referenced object, the same `weakref.ref` behavior
-          will be maintained between them after unpickling.
-        - If pickled without the referenced object, after unpickling the underlying
-          reference will be cleared (`__call__` will always return `None`).
-    """
-
-    def __init__(self, obj: Any):
-        if obj is None:
-            # The object will be `None` upon deserialization if the serialized weakref
-            # had lost its underlying object.
-            self._wr = None
-        else:
-            self._wr = weakref.ref(obj)
-
-    def __call__(self) -> Any:
-        if self._wr is None:
-            return None
-        else:
-            return self._wr()
-
-    def __reduce__(self) -> tuple[Callable, tuple[weakref.ReferenceType | None]]:
-        return _PydanticWeakRef, (self(),)
-
-
-def build_lenient_weakvaluedict(d: dict[str, Any] | None) -> dict[str, Any] | None:
-    """Takes an input dictionary, and produces a new value that (invertibly) replaces the values with weakrefs.
-
-    We can't just use a WeakValueDictionary because many types (including int, str, etc.) can't be stored as values
-    in a WeakValueDictionary.
-
-    The `unpack_lenient_weakvaluedict` function can be used to reverse this operation.
-    """
-    if d is None:
-        return None
-    result = {}
-    for k, v in d.items():
-        try:
-            proxy = _PydanticWeakRef(v)
-        except TypeError:
-            proxy = v
-        result[k] = proxy
-    return result
-
-
-def unpack_lenient_weakvaluedict(d: dict[str, Any] | None) -> dict[str, Any] | None:
-    """Inverts the transform performed by `build_lenient_weakvaluedict`."""
-    if d is None:
-        return None
-
-    result = {}
-    for k, v in d.items():
-        if isinstance(v, _PydanticWeakRef):
-            v = v()
-            if v is not None:
-                result[k] = v
-        else:
-            result[k] = v
-    return result
 
 
 @lru_cache(maxsize=None)
