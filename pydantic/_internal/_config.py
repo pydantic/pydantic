8fd3a002d02df672da72cabfedadb2b586b48223
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-08-07 08:18:57 -0500

    Experimental feature: opt-in fast build (#10064)
    Co-authored-by: Victorien <65306057+Viicos@users.noreply.github.com>

--- pydantic/_internal/_config.py
+++ pydantic/_internal/_config.py
@@ -89,6 +89,7 @@ class ConfigWrapper:
     validation_error_cause: bool
     use_attribute_docstrings: bool
     cache_strings: bool | Literal['all', 'keys', 'none']
+    experimental_fast_build: bool
 
     def __init__(self, config: ConfigDict | dict[str, Any] | type[Any] | None, *, check: bool = True):
         if check:
@@ -272,6 +273,7 @@ config_defaults = ConfigDict(
     validation_error_cause=False,
     use_attribute_docstrings=False,
     cache_strings=True,
+    experimental_fast_build=False,
 )
 
 
