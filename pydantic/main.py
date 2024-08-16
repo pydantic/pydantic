0920d80c2f7095c1a92d103b7da92b289c5329c5
kc0506 <kchong0506@gmail.com>
2024-08-16 11:55:11 +0800

    update class docstring

--- pydantic/main.py
+++ pydantic/main.py
@@ -83,12 +83,12 @@ class BaseModel(metaclass=_model_construction.ModelMetaclass):
     A base class for creating Pydantic models.
 
     Attributes:
-        __class_vars__: The names of classvars defined on the model.
+        __class_vars__: The names of the class variables defined on the model.
         __private_attributes__: Metadata about the private attributes of the model.
-        __signature__: The signature for instantiating the model.
+        __signature__: The synthesized `__init__` [`Signature`][inspect.Signature] of the model.
 
         __pydantic_complete__: Whether model building is completed, or if there are still undefined fields.
-        __pydantic_core_schema__: The pydantic-core schema used to build the SchemaValidator and SchemaSerializer.
+        __pydantic_core_schema__: The core schema of the model.
         __pydantic_custom_init__: Whether the model has a custom `__init__` function.
         __pydantic_decorators__: Metadata containing the decorators defined on the model.
             This replaces `Model.__validators__` and `Model.__root_validators__` from Pydantic V1.
@@ -96,14 +96,14 @@ class BaseModel(metaclass=_model_construction.ModelMetaclass):
             __args__, __origin__, __parameters__ in typing-module generics. May eventually be replaced by these.
         __pydantic_parent_namespace__: Parent namespace of the model, used for automatic rebuilding of models.
         __pydantic_post_init__: The name of the post-init method for the model, if defined.
-        __pydantic_root_model__: Whether the model is a `RootModel`.
-        __pydantic_serializer__: The pydantic-core SchemaSerializer used to dump instances of the model.
-        __pydantic_validator__: The pydantic-core SchemaValidator used to validate instances of the model.
+        __pydantic_root_model__: Whether the model is a [`RootModel`][pydantic.root_model.RootModel].
+        __pydantic_serializer__: The `pydantic-core` `SchemaSerializer` used to dump instances of the model.
+        __pydantic_validator__: The `pydantic-core` `SchemaValidator` used to validate instances of the model.
 
-        __pydantic_extra__: An instance attribute with the values of extra fields from validation when
-            `model_config['extra'] == 'allow'`.
-        __pydantic_fields_set__: An instance attribute with the names of fields explicitly set.
-        __pydantic_private__: Instance attribute with the values of private attributes set on the model instance.
+        __pydantic_extra__: A dictionary containing extra values, if [`extra`][pydantic.config.ConfigDict.extra]
+            is set to `'allow'`.
+        __pydantic_fields_set__: The names of fields explicitly set during instantiation.
+        __pydantic_private__: Values of private attributes set on the model instance.
     """
 
     # Class attributes:
@@ -128,19 +128,6 @@ class BaseModel(metaclass=_model_construction.ModelMetaclass):
     model_computed_fields: ClassVar[Dict[str, ComputedFieldInfo]] = {}  # noqa: UP006
     """A dictionary of computed field names and their corresponding `ComputedFieldInfo` objects."""
 
-    __pydantic_complete__: ClassVar[bool] = False
-    """Whether model building is completed, or if there are still undefined fields."""
-
-    __pydantic_decorators__: ClassVar[_decorators.DecoratorInfos] = _decorators.DecoratorInfos()
-    """Metadata containing the decorators defined on the model.
-    This replaces `Model.__validators__` and `Model.__root_validators__` from Pydantic V1."""
-
-    __pydantic_parent_namespace__: ClassVar[Dict[str, Any] | None] = None  # noqa: UP006
-    """Parent namespace of the model, used for automatic rebuilding of models."""
-
-    __pydantic_root_model__: ClassVar[bool] = False
-    """Whether the model is a [`RootModel`][pydantic.root_model.RootModel]."""
-
     __class_vars__: ClassVar[set[str]]
     """The names of the class variables defined on the model."""
 
@@ -150,18 +137,31 @@ class BaseModel(metaclass=_model_construction.ModelMetaclass):
     __signature__: ClassVar[Signature]
     """The synthesized `__init__` [`Signature`][inspect.Signature] of the model."""
 
+    __pydantic_complete__: ClassVar[bool] = False
+    """Whether model building is completed, or if there are still undefined fields."""
+
     __pydantic_core_schema__: ClassVar[CoreSchema]
     """The core schema of the model."""
 
     __pydantic_custom_init__: ClassVar[bool]
     """Whether the model has a custom `__init__` method."""
 
+    __pydantic_decorators__: ClassVar[_decorators.DecoratorInfos] = _decorators.DecoratorInfos()
+    """Metadata containing the decorators defined on the model.
+    This replaces `Model.__validators__` and `Model.__root_validators__` from Pydantic V1."""
+
     __pydantic_generic_metadata__: ClassVar[_generics.PydanticGenericMetadata]
     """Metadata for generic models; contains data used for a similar purpose to
     __args__, __origin__, __parameters__ in typing-module generics. May eventually be replaced by these."""
 
+    __pydantic_parent_namespace__: ClassVar[Dict[str, Any] | None] = None  # noqa: UP006
+    """Parent namespace of the model, used for automatic rebuilding of models."""
+
     __pydantic_post_init__: ClassVar[None | Literal['model_post_init']]
     """The name of the post-init method for the model, if defined."""
+
+    __pydantic_root_model__: ClassVar[bool] = False
+    """Whether the model is a [`RootModel`][pydantic.root_model.RootModel]."""
 
     __pydantic_serializer__: ClassVar[SchemaSerializer]
     """The `pydantic-core` `SchemaSerializer` used to dump instances of the model."""
