2f5cb1ee49e477d92bfc7c1d660d5df1680ee43b
sydney-runkle <sydneymarierunkle@gmail.com>
2024-08-27 13:18:25 -0400

    reorganization

--- tests/benchmarks/test_model_serialization.py
+++ tests/benchmarks/test_model_serialization.py
@@ -1,31 +1,10 @@
-from typing import Dict, List, Optional, Union
+from typing import List
 
 import pytest
 
 from pydantic import BaseModel
 
-
-class SimpleModel(BaseModel):
-    field1: str
-    field2: int
-    field3: float
-
-
-class NestedModel(BaseModel):
-    field1: str
-    field2: List[int]
-    field3: Dict[str, float]
-
-
-class OuterModel(BaseModel):
-    nested: NestedModel
-    optional_nested: Optional[NestedModel]
-
-
-class ComplexModel(BaseModel):
-    field1: Union[str, int, float]
-    field2: List[Dict[str, Union[int, float]]]
-    field3: Optional[List[Union[str, int]]]
+from .shared import ComplexModel, NestedModel, OuterModel, SimpleModel
 
 
 @pytest.mark.benchmark(group='model_serialization')
