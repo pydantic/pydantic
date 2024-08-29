55e05470847850e81d71a4aafece1a5bf77d50f2
sydney-runkle <sydneymarierunkle@gmail.com>
2024-08-29 09:21:55 -0400

    update bc we already copy

--- pydantic/_internal/_generate_schema.py
+++ pydantic/_internal/_generate_schema.py
@@ -336,7 +336,7 @@ class TypesNamespaceStack:
 
     @contextmanager
     def push(self, for_type: type[Any]):
-        types_namespace = {**_typing_extra.get_cls_types_namespace(for_type), **(self.tail or {})}
+        types_namespace = _typing_extra.get_cls_types_namespace(for_type).update(self.tail or {})
         self._types_namespace_stack.append(types_namespace)
         try:
             yield
