c0a288f1457734c0ae7ea8d3ae2f5e458327c4cd
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-09-13 14:27:58 -0500

    Fix `ZoneInfo` with various invalid types (#10408)

--- pydantic/_internal/_generate_schema.py
+++ pydantic/_internal/_generate_schema.py
@@ -1657,7 +1657,7 @@ class GenerateSchema:
                 return value
             try:
                 return ZoneInfo(value)
-            except (ZoneInfoNotFoundError, ValueError):
+            except (ZoneInfoNotFoundError, ValueError, TypeError):
                 raise PydanticCustomError('zoneinfo_str', 'invalid timezone: {value}', {'value': value})
 
         metadata = build_metadata_dict(js_functions=[lambda _1, _2: {'type': 'string', 'format': 'zoneinfo'}])
