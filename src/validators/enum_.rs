2e99ed59bd3808465a16d4036a3c4e35d4c3874d
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-09-20 07:57:16 -0500

    Support wider variety of enum validation cases (pydantic/pydantic-core#1456)
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/8c1a0da3a3a2cba8af119de1030e04b920aa0bf0

--- src/validators/enum_.rs
+++ src/validators/enum_.rs
@@ -116,11 +116,15 @@ impl<T: EnumValidateValue> Validator for EnumValidator<T> {
                 },
                 input,
             ));
-        } else if let Some(v) = T::validate_value(py, input, &self.lookup, strict)? {
-            state.floor_exactness(Exactness::Lax);
+        }
+
+        state.floor_exactness(Exactness::Lax);
+
+        if let Some(v) = T::validate_value(py, input, &self.lookup, strict)? {
             return Ok(v);
+        } else if let Ok(res) = class.as_unbound().call1(py, (input.as_python(),)) {
+            return Ok(res);
         } else if let Some(ref missing) = self.missing {
-            state.floor_exactness(Exactness::Lax);
             let enum_value = missing.bind(py).call1((input.to_object(py),)).map_err(|_| {
                 ValError::new(
                     ErrorType::Enum {
@@ -146,6 +150,7 @@ impl<T: EnumValidateValue> Validator for EnumValidator<T> {
                 return Err(type_error.into());
             }
         }
+
         Err(ValError::new(
             ErrorType::Enum {
                 expected: self.expected_repr.clone(),
