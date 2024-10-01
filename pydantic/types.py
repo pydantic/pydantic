6337137917c0a1a620466f45f001078273b9fe95
sydney-runkle <sydneymarierunkle@gmail.com>
2024-10-01 11:37:15 -0400

    oops, formatting

--- pydantic/types.py
+++ pydantic/types.py
@@ -2480,9 +2480,7 @@ class EncodedBytes:
     def __get_pydantic_core_schema__(self, source: type[Any], handler: GetCoreSchemaHandler) -> core_schema.CoreSchema:
         schema = handler(source)
         if (schema_type := schema['type']) not in ('bytes', 'str'):
-            raise PydanticUserError(
-                f"'EncodedBytes' cannot annotate '{schema_type}'.", code='invalid-annotated-type'
-            )
+            raise PydanticUserError(f"'EncodedBytes' cannot annotate '{schema_type}'.", code='invalid-annotated-type')
 
         return core_schema.with_info_after_validator_function(
             function=self.decode,
