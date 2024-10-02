610b090a9b19236497a6557f600d93630d7976c0
sydney-runkle <sydney-runkle@users.noreply.github.com>
2024-10-02 17:12:12 +0000

    Deployed fd76b94 to 2.9 with MkDocs 1.6.1 and mike 2.1.3

--- 2.9/extra/fluff.js
+++ 2.9/extra/fluff.js
@@ -1,11 +1,66 @@
-async function set_download_count(el) {
-  const r = await fetch('https://errors.pydantic.dev/download-count.txt');
-  if (r.status === 200) {
-    el.innerText = await r.text();
+// set the download count in the "why pydantic" page
+(async function() {
+  const downloadCount = document.getElementById('download-count');
+  if (downloadCount) {
+    const r = await fetch('https://errors.pydantic.dev/download-count.txt');
+    if (r.status === 200) {
+      downloadCount.innerText = await r.text();
+    }
   }
-}
+})();
 
-const download_count = document.getElementById('download-count');
-if (download_count) {
-  set_download_count(download_count)
-}
+// update the announcement banner to change the app type
+(function() {
+  const el = document.getElementById('logfire-app-type');
+  const appTypes = [
+    ['/integrations/pydantic/', 'Pydantic validations.'],
+    ['/integrations/fastapi/', 'FastAPI app.'],
+    ['/integrations/openai/', 'OpenAI integration.'],
+    ['/integrations/asyncpg/', 'Postgres queries.'],
+    ['/integrations/redis/', 'task queue.'],
+    ['/integrations/system-metrics/', 'system metrics.'],
+    ['/integrations/httpx/', 'API calls.'],
+    ['/integrations/logging/', 'std lib logging.'],
+    ['/integrations/django/', 'Django app.'],
+    ['/integrations/anthropic/', 'Anthropic API calls.'],
+    ['/integrations/fastapi/', 'Flask app.'],
+    ['/integrations/mysql/', 'MySQL queries.'],
+    ['/integrations/sqlalchemy/', 'SQLAlchemy queries.'],
+    ['/integrations/structlog/', 'Structlog logs.'],
+    ['/integrations/stripe/', 'Stripe API calls.'],
+  ];
+  const docsUrl = 'https://logfire.pydantic.dev/docs';
+  let counter = 0;
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  // avoid multiple replaceText running at the same time (e.g. when the user has left the page)
+  let running = false;
+
+  const replaceText = async () => {
+    if (running) {
+      return;
+    }
+    running = true;
+    try {
+      const text = el.textContent;
+      for (let i = text.length; i >= 0; i--) {
+        el.textContent = text.slice(0, i);
+        await sleep(30);
+      }
+      await sleep(30);
+      counter++;
+      // change the link halfway through the animation
+      const [link, newText] = appTypes[counter % appTypes.length];
+      el.href = docsUrl + link;
+      await sleep(30);
+      for (let i = 0; i <= newText.length; i++) {
+        el.textContent = newText.slice(0, i);
+        await sleep(30);
+      }
+    } finally {
+      running = false;
+    }
+  };
+  setInterval(replaceText, 4000);
+})();
