4e71e66cc59a552482aa9f45d99f117d2a857763
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-10-07 09:39:02 -0400

    Improve error handling for in-evaluable refs for discriminator application (#10440)

--- pydantic/json_schema.py
+++ pydantic/json_schema.py
@@ -112,7 +112,7 @@ def update_json_schema(schema: JsonSchemaValue, updates: dict[str, Any]) -> Json
     return schema
 
 
-JsonSchemaWarningKind = Literal['skipped-choice', 'non-serializable-default']
+JsonSchemaWarningKind = Literal['skipped-choice', 'non-serializable-default', 'skipped-discriminator']
 """
 A type alias representing the kinds of warnings that can be emitted during JSON schema generation.
 
@@ -466,7 +466,6 @@ class GenerateJsonSchema:
                 core_ref = CoreRef(core_schema['ref'])  # type: ignore[typeddict-item]
                 defs_ref, ref_json_schema = self.get_cache_defs_ref_schema(core_ref)
                 json_ref = JsonRef(ref_json_schema['$ref'])
-                self.json_to_defs_refs[json_ref] = defs_ref
                 # Replace the schema if it's not a reference to itself
                 # What we want to avoid is having the def be just a ref to itself
                 # which is what would happen if we blindly assigned any
@@ -1210,9 +1209,14 @@ class GenerateJsonSchema:
                     continue  # this means that the "alias" does not represent a field
                 alias_is_present_on_all_choices = True
                 for choice in one_of_choices:
-                    while '$ref' in choice:
-                        assert isinstance(choice['$ref'], str)
-                        choice = self.get_schema_from_definitions(JsonRef(choice['$ref'])) or {}
+                    try:
+                        choice = self.resolve_schema_to_update(choice)
+                    except RuntimeError as exc:
+                        # TODO: fixme - this is a workaround for the fact that we can't always resolve refs
+                        # for tagged union choices at this point in the schema gen process, we might need to do
+                        # another pass at the end like we do for core schemas
+                        self.emit_warning('skipped-discriminator', str(exc))
+                        choice = {}
                     properties = choice.get('properties', {})
                     if not isinstance(properties, dict) or alias not in properties:
                         alias_is_present_on_all_choices = False
