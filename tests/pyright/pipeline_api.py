35f60f33c9a0e064ac16f97f14e54e61b8c65f3e
Viicos <65306057+Viicos@users.noreply.github.com>
2024-10-14 16:57:02 +0200

    Disable some PEP 746 related type checking errors

--- tests/pyright/pipeline_api.py
+++ tests/pyright/pipeline_api.py
@@ -3,8 +3,10 @@ from typing import Annotated
 
 from pydantic.experimental.pipeline import validate_as
 
-a1 = Annotated[str, validate_as(int)]  # pyright: ignore[reportInvalidTypeArguments]
-a2 = Annotated[str, validate_as(str).transform(lambda x: int(x))]  # pyright: ignore[reportInvalidTypeArguments]
+# TODO: since Pyright 1.1.384, support for PEP 746 was disabled.
+# `a1` and `a2` should have a `pyright: ignore[reportInvalidTypeArguments]` comment.
+a1 = Annotated[str, validate_as(int)]
+a2 = Annotated[str, validate_as(str).transform(lambda x: int(x))]
 a3 = Annotated[float, validate_as(float).gt(0)]  # should be able to compare float to int
 
 a4 = Annotated[datetime.datetime, validate_as(datetime.datetime).datetime_tz_naive()]
