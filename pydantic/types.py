d78161895741c4addb9a9d9dbcbdd599289eb8c9
Samuel Colvin <s@muelcolvin.com>
2024-11-01 18:22:33 +0000

    reinstate md section

--- pydantic/types.py
+++ pydantic/types.py
@@ -966,10 +966,11 @@ else:
 
         my_cos_default3 = ImportThingsMissingValidateDefault()
         assert my_cos_default3.obj == 'math.cos'  # just string, not evaluated
+        ```
 
         Serializing an `ImportString` type to json is also possible.
 
-        ```py lint="skip"
+        ```py
         from pydantic import BaseModel, ImportString
 
         class ImportThings(BaseModel):
