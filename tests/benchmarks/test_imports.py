2bf76648917b3ca2baaf2dd95d1ae7ff220bf98c
Samuel Colvin <s@muelcolvin.com>
2024-11-11 16:12:42 +0000

    fix benchmarks

--- tests/benchmarks/test_imports.py
+++ tests/benchmarks/test_imports.py
@@ -2,14 +2,18 @@ import pytest
 
 
 @pytest.mark.benchmark
-def test_import_basemodel() -> None:
-    from pydantic import BaseModel
+def test_import_basemodel(benchmark) -> None:
+    @benchmark
+    def run():
+        from pydantic import BaseModel
 
-    assert BaseModel
+        assert BaseModel
 
 
 @pytest.mark.benchmark
-def test_import_field() -> None:
-    from pydantic import Field
+def test_import_field(benchmark) -> None:
+    @benchmark
+    def run():
+        from pydantic import Field
 
-    assert Field
+        assert Field
