621489dde2adc07b806c9fefede1c677b772fef2
James Massucco <jmassucco17@gmail.com>
2024-11-11 17:23:59 -0800

    lint

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -149,24 +149,24 @@ def test_computed_field_raises_correct_attribute_error():
 def test_property_visible_when_extra_present():
     class Model(BaseModel):
         model_config = ConfigDict(extra='allow')
-    
+
         columns: List[str]
-    
+
         @property
         def queried_columns(self) -> List[str]:
             return [c for c in self.columns if c.startswith('query')]
-    
-    
+
+
     class SubModel(Model):
         pass
-    
-    
+
+
     instance = SubModel.model_validate(
         dict(
             columns=['query_1', 'query_2', 'other'],
             test='extra_value',
         ))
-    
+
     assert instance.queried_columns == ['query_1', 'query_2']
     assert instance.__getattribute__('queried_columns') == ['query_1', 'query_2']
     assert instance.__getattr__('queried_columns') == ['query_1', 'query_2']
