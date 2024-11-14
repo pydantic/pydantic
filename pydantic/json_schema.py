d12ba73816ad5ddd3210e34048a3ed9a7a348af5
Markus Sintonen <markus.sintonen@gmail.com>
2024-11-14 20:58:13 +0200

    Remove invalid_schema

--- pydantic/json_schema.py
+++ pydantic/json_schema.py
@@ -601,11 +601,6 @@ class GenerateJsonSchema:
 
     # ### Schema generation methods
 
-    def invalid_schema(self, schema: core_schema.InvalidSchema) -> JsonSchemaValue:
-        """Placeholder - should never be called."""
-
-        raise RuntimeError('Cannot generate schema for invalid_schema. This is a bug! Please report it.')
-
     def any_schema(self, schema: core_schema.AnySchema) -> JsonSchemaValue:
         """Generates a JSON schema that matches any value.
 
