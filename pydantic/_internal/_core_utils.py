12d637b46da748f0447c95b7460032574d69fb88
Viicos <65306057+Viicos@users.noreply.github.com>
2024-11-20 17:51:35 +0100

    Fixes

--- pydantic/_internal/_core_utils.py
+++ pydantic/_internal/_core_utils.py
@@ -66,7 +66,7 @@ def is_list_like_schema_with_items_schema(
     return schema['type'] in _LIST_LIKE_SCHEMA_WITH_ITEMS_TYPES
 
 
-def get_type_ref(type_: type[Any], args_override: tuple[type[Any], ...] | None = None) -> str:
+def get_type_ref(type_: Any, args_override: tuple[type[Any], ...] | None = None) -> str:
     """Produces the ref to be used for this type by pydantic_core's core schemas.
 
     This `args_override` argument was added for the purpose of creating valid recursive references
