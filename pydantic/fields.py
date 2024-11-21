dcfe613f6c2c2bb9b033b5cf95423d5503d5040b
sydney-runkle <sydneymarierunkle@gmail.com>
2024-11-21 09:10:45 -0500

    linting

--- pydantic/fields.py
+++ pydantic/fields.py
@@ -599,7 +599,7 @@ class FieldInfo(_repr.Representation):
             if _fields.takes_validated_data_argument(self.default_factory):
                 if validated_data is None:
                     raise ValueError(
-                       "The default factory requires the 'validated_data' argument, which was not provided when calling 'get_default'."
+                        "The default factory requires the 'validated_data' argument, which was not provided when calling 'get_default'."
                     )
                 return self.default_factory(validated_data)
             else:
