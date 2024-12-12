80756c197ed66c7ef9256366bbcf9dead41e37c9
David Hewitt <david.hewitt@pydantic.dev>
2024-12-12 19:42:26 +0000

    clean up deprecations in PyO3 0.23 for tests / benches (#1575)

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,7 @@
-#![allow(deprecated)] // FIXME: just used during upgrading PyO3 to 0.23
-
 #[cfg(test)]
 mod tests {
     use _pydantic_core::{SchemaSerializer, SchemaValidator, WarningsArg};
+    use pyo3::ffi::c_str; // can switch to c"" literals on MSRV >= 1.77
     use pyo3::prelude::*;
     use pyo3::types::PyDict;
 
@@ -24,7 +23,8 @@ mod tests {
             //         'type': 'function-wrap',
             //         'function': lambda: None,
             //     },
-            let code = r"{
+            let code = c_str!(
+                r"{
                 'type': 'definitions',
                 'schema': {'type': 'definition-ref', 'schema_ref': 'C-ref'},
                 'definitions': [
@@ -46,8 +46,9 @@ mod tests {
                         },
                     },
                 ]
-            }";
-            let schema: Bound<'_, PyDict> = py.eval_bound(code, None, None).unwrap().extract().unwrap();
+            }"
+            );
+            let schema: Bound<'_, PyDict> = py.eval(code, None, None).unwrap().extract().unwrap();
             SchemaSerializer::py_new(schema, None).unwrap();
         });
     }
@@ -55,7 +56,8 @@ mod tests {
     #[test]
     fn test_serialize_computed_fields() {
         Python::with_gil(|py| {
-            let code = r#"
+            let code = c_str!(
+                r#"
 class A:
     @property
     def b(self) -> str:
@@ -74,9 +76,10 @@ schema = {
     "type": "model",
 }
 a = A()
-            "#;
-            let locals = PyDict::new_bound(py);
-            py.run_bound(code, None, Some(&locals)).unwrap();
+            "#
+            );
+            let locals = PyDict::new(py);
+            py.run(code, None, Some(&locals)).unwrap();
             let a = locals.get_item("a").unwrap().unwrap();
             let schema = locals
                 .get_item("schema")
@@ -111,7 +114,8 @@ a = A()
     #[test]
     fn test_literal_schema() {
         Python::with_gil(|py| {
-            let code = r#"
+            let code = c_str!(
+                r#"
 schema = {
     "type": "dict",
     "keys_schema": {
@@ -124,9 +128,10 @@ schema = {
     "strict": False,
 }
 json_input = '{"a": "something"}'
-            "#;
-            let locals = PyDict::new_bound(py);
-            py.run_bound(code, None, Some(&locals)).unwrap();
+            "#
+            );
+            let locals = PyDict::new(py);
+            py.run(code, None, Some(&locals)).unwrap();
             let schema = locals.get_item("schema").unwrap().unwrap();
             let json_input = locals.get_item("json_input").unwrap().unwrap();
             let binding = SchemaValidator::py_new(py, &schema, None)
@@ -142,7 +147,8 @@ json_input = '{"a": "something"}'
     #[test]
     fn test_segfault_for_recursive_schemas() {
         Python::with_gil(|py| {
-            let code = r"
+            let code = c_str!(
+                r"
 schema = {
     'type': 'definitions',
     'schema': {
@@ -177,9 +183,10 @@ schema = {
 }
 dump_json_input_1 = 1
 dump_json_input_2 = {'a': 'something'}
-            ";
-            let locals = PyDict::new_bound(py);
-            py.run_bound(code, None, Some(&locals)).unwrap();
+            "
+            );
+            let locals = PyDict::new(py);
+            py.run(code, None, Some(&locals)).unwrap();
             let schema = locals
                 .get_item("schema")
                 .unwrap()
