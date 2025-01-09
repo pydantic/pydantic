27d0a26be2f139a903623b56dcbbf875c43f0510
Viicos <65306057+Viicos@users.noreply.github.com>
2025-01-09 18:24:14 +0100

    Simplify `_update_fields_from_docstrings` signature
    Let callers be responsible for checking if using docstrings is
    necessary.

--- pydantic/_internal/_fields.py
+++ pydantic/_internal/_fields.py
@@ -64,12 +64,11 @@ def _general_metadata_cls() -> type[BaseMetadata]:
     return _PydanticGeneralMetadata  # type: ignore
 
 
-def _update_fields_from_docstrings(cls: type[Any], fields: dict[str, FieldInfo], config_wrapper: ConfigWrapper) -> None:
-    if config_wrapper.use_attribute_docstrings:
-        fields_docs = extract_docstrings_from_cls(cls)
-        for ann_name, field_info in fields.items():
-            if field_info.description is None and ann_name in fields_docs:
-                field_info.description = fields_docs[ann_name]
+def _update_fields_from_docstrings(cls: type[Any], fields: dict[str, FieldInfo]) -> None:
+    fields_docs = extract_docstrings_from_cls(cls)
+    for ann_name, field_info in fields.items():
+        if field_info.description is None and ann_name in fields_docs:
+            field_info.description = fields_docs[ann_name]
 
 
 def collect_model_fields(  # noqa: C901
@@ -255,7 +254,8 @@ def collect_model_fields(  # noqa: C901
         for field in fields.values():
             field.apply_typevars_map(typevars_map)
 
-    _update_fields_from_docstrings(cls, fields, config_wrapper)
+    if config_wrapper.use_attribute_docstrings:
+        _update_fields_from_docstrings(cls, fields)
     return fields, class_vars
 
 
@@ -372,8 +372,8 @@ def collect_dataclass_fields(
             # Can't we juste use `_generics.replace_types`?
             field.apply_typevars_map(typevars_map)
 
-    if config_wrapper is not None:
-        _update_fields_from_docstrings(cls, fields, config_wrapper)
+    if config_wrapper is not None and config_wrapper.use_attribute_docstrings:
+        _update_fields_from_docstrings(cls, fields)
 
     return fields
 
