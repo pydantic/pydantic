46f30755fbd977c23527094f455ac6a1c4380481
Saurabh Misra <misra.saurabh1@gmail.com>
2025-01-16 03:07:48 -0800

    Only compute normalized decimal places if necessary in `decimal_places_validator` (#11281)

--- pydantic/_internal/_validators.py
+++ pydantic/_internal/_validators.py
@@ -398,12 +398,13 @@ def max_digits_validator(x: Any, max_digits: Any) -> Any:
 def decimal_places_validator(x: Any, decimal_places: Any) -> Any:
     try:
         decimal_places_, _ = _extract_decimal_digits_info(x)
-        normalized_decimal_places, _ = _extract_decimal_digits_info(x.normalize())
-        if (decimal_places_ > decimal_places) and (normalized_decimal_places > decimal_places):
-            raise PydanticKnownError(
-                'decimal_max_places',
-                {'decimal_places': decimal_places},
-            )
+        if decimal_places_ > decimal_places:
+            normalized_decimal_places, _ = _extract_decimal_digits_info(x.normalize())
+            if normalized_decimal_places > decimal_places:
+                raise PydanticKnownError(
+                    'decimal_max_places',
+                    {'decimal_places': decimal_places},
+                )
         return x
     except TypeError:
         raise TypeError(f"Unable to apply constraint 'decimal_places' to supplied value {x}")
