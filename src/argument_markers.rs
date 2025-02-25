796f6cb1241b04074d27fd7694e808b80964fdbd
David Hewitt <david.hewitt@pydantic.dev>
2025-02-25 13:37:11 +0000

    bump to PyO3 0.23.5, support PyPy 3.11 (pydantic/pydantic-core#1646)
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/21eef8db71f30937de89367d0a596928b0e0ee0b

--- src/argument_markers.rs
+++ src/argument_markers.rs
@@ -5,12 +5,7 @@ use pyo3::types::{PyDict, PyTuple};
 
 use crate::tools::safe_repr;
 
-// see https://github.com/PyO3/pyo3/issues/4894 - freelist is currently unsound with GIL disabled
-#[cfg_attr(
-    not(Py_GIL_DISABLED),
-    pyclass(module = "pydantic_core._pydantic_core", get_all, frozen, freelist = 100)
-)]
-#[cfg_attr(Py_GIL_DISABLED, pyclass(module = "pydantic_core._pydantic_core", get_all, frozen))]
+#[pyclass(module = "pydantic_core._pydantic_core", get_all, frozen, freelist = 100)]
 #[derive(Debug, Clone)]
 pub struct ArgsKwargs {
     pub(crate) args: Py<PyTuple>,
