ed246462e0e1c6ba5b8af76cbd2f8adfa56433fe
Victorien <65306057+Viicos@users.noreply.github.com>
2025-03-25 14:38:03 +0100

    Update pydantic/networks.py

--- pydantic/networks.py
+++ pydantic/networks.py
@@ -374,7 +374,7 @@ class _BaseMultiHostUrl:
     def query_params(self) -> list[tuple[str, str]]:
         """The query part of the URL as a list of key-value pairs.
 
-        e.g. `[('baz', 'qux')]` in `https://foo.com,bar.com/path?baz=qux#fragment`
+        e.g. `[('foo', 'bar')]` in `https://foo.com,bar.com/path?foo=bar#fragment`
         """
         return self._url.query_params()
 
