8d5633c1d49a1f44ac99ef2bc540b82b73a533d0
David Hewitt <david.hewitt@pydantic.dev>
2025-04-02 09:34:17 +0100

    Pin `inline-snapshot` with an upper bound to fix wasm build (pydantic/pydantic-core#1680)
    Original-commit-link: https://github.com/pydantic/pydantic-core/commit/f6ded645fb27cb2f387bab481d610e98860e56cc

--- tests/emscripten_runner.js
+++ tests/emscripten_runner.js
@@ -98,7 +98,8 @@ import importlib
 
 await micropip.install([
     'dirty-equals',
-    'inline-snapshot',
+    # inline-snapshot 0.21 requires pytest 8.3.4, pyodide 0.26 ships with 8.1.1
+    'inline-snapshot < 0.21',
     'hypothesis',
     'pytest-speed',
     'pytest-mock',
