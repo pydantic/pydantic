48f4de533587aa2a4f0cd4ff4f4be0da38cf90d3
Viicos <65306057+Viicos@users.noreply.github.com>
2025-04-17 17:01:33 +0200

    Remove coercion of decimal constraints
    This is now done in `pydantic-core`, and wasn't done right
    as it resulted in some floating point precision errors.

--- pydantic/_internal/_known_annotated_metadata.py
+++ pydantic/_internal/_known_annotated_metadata.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from collections import defaultdict
 from collections.abc import Iterable
 from copy import copy
-from decimal import Decimal
 from functools import lru_cache, partial
 from typing import TYPE_CHECKING, Any
 
@@ -220,10 +219,7 @@ def apply_known_metadata(annotation: Any, schema: CoreSchema) -> CoreSchema | No
             if constraint == 'union_mode' and schema_type == 'union':
                 schema['mode'] = value  # type: ignore  # schema is UnionSchema
             else:
-                if schema_type == 'decimal' and constraint in {'multiple_of', 'le', 'ge', 'lt', 'gt'}:
-                    schema[constraint] = Decimal(value)
-                else:
-                    schema[constraint] = value
+                schema[constraint] = value
             continue
 
         #  else, apply a function after validator to the schema to enforce the corresponding constraint
