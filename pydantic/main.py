5aefad873b3dfd60c419bd081ffaf0ac197c7b60
Victorien <65306057+Viicos@users.noreply.github.com>
2025-05-21 10:11:12 +0200

    Do not delete mock validator/serializer in `model_rebuild()`
    Backport of: #11890

--- pydantic/main.py
+++ pydantic/main.py
@@ -617,9 +617,11 @@ class BaseModel(metaclass=_model_construction.ModelMetaclass):
             return None
 
         for attr in ('__pydantic_core_schema__', '__pydantic_validator__', '__pydantic_serializer__'):
-            if attr in cls.__dict__:
+            if attr in cls.__dict__ and not isinstance(getattr(cls, attr), _mock_val_ser.MockValSer):
                 # Deleting the validator/serializer is necessary as otherwise they can get reused in
-                # pydantic-core. Same applies for the core schema that can be reused in schema generation.
+                # pydantic-core. We do so only if they aren't mock instances, otherwise — as `model_rebuild()`
+                # isn't thread-safe — concurrent model instantiations can lead to the parent validator being used.
+                # Same applies for the core schema that can be reused in schema generation.
                 delattr(cls, attr)
 
         cls.__pydantic_complete__ = False
