52a8a475f79f4b33d4e0e9119dd73fd5431cfd5c
Oliver Parker <oliver.parker@gridedge.co.uk>
2025-08-18 16:09:50 +0100

    style: lint:

--- pydantic/config.py
+++ pydantic/config.py
@@ -596,7 +596,7 @@ class ConfigDict(TypedDict, total=False):
 
     - `'iso8601'` will serialize timedeltas to [ISO 8601 text format](https://en.wikipedia.org/wiki/ISO_8601#Durations).
     - `'float'` will serialize timedeltas to the total number of seconds.
-    
+
     !!! warning
         Starting in v2.11, it is recommended to use the [`ser_json_temporal`][pydantic.config.ConfigDict.ser_json_temporal]
         setting instead of `ser_json_timedelta`. This setting will be deprecated in v3.
