baabf506690b4e3baeb576ffd46c7655791b0c83
Viicos <65306057+Viicos@users.noreply.github.com>
2025-09-29 13:59:17 +0200

    Do not error on fields overridden by methods in the mypy plugin

--- tests/mypy/modules/frozen_field.py
+++ tests/mypy/modules/frozen_field.py
@@ -8,3 +8,16 @@ class Foo(BaseModel):
 foo = Foo()
 
 foo.a = 2
+
+
+class Parent(BaseModel):
+    parent_attr: str = Field(exclude=True)
+
+
+# We don't wan't to froze `parent_attr` in the plugin:
+class Chield(Parent):
+    child_attr: str = Field(exclude=True)
+
+    @property
+    def parent_attr(self) -> str:
+        return self.child_attr
--- tests/mypy/outputs/mypy-plugin_ini/frozen_field.py
+++ tests/mypy/outputs/mypy-plugin_ini/frozen_field.py
@@ -9,3 +9,18 @@ foo = Foo()
 
 foo.a = 2
 # MYPY: error: Property "a" defined in "Foo" is read-only  [misc]
+
+
+class Parent(BaseModel):
+    parent_attr: str = Field(exclude=True)
+
+
+# We don't wan't to froze `parent_attr` in the plugin:
+class Chield(Parent):
+    child_attr: str = Field(exclude=True)
+
+    @property
+# MYPY: error: BaseModel field may only be overridden by another field  [misc]
+    def parent_attr(self) -> str:
+# MYPY: error: Cannot override writeable attribute with read-only property  [override]
+        return self.child_attr
