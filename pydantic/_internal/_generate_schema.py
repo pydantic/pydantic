07324c638da4009e1a76222c9a1ebfc8afbd25cc
Victorien <65306057+Viicos@users.noreply.github.com>
2025-10-07 10:12:04 +0200

    Emit warning when using `default_factory` with a validated data parameter in invalid contexts (#12335)

--- pydantic/_internal/_generate_schema.py
+++ pydantic/_internal/_generate_schema.py
@@ -2244,9 +2244,8 @@ class GenerateSchema:
                 # HACK: we don't want to emit the warning for `FieldInfo` subclasses, because FastAPI does weird manipulations
                 # with its subclasses and their annotations:
                 and type(metadata) is FieldInfo
-                and (unsupported_attributes := self._get_unsupported_field_info_attributes(metadata))
             ):
-                for attr, value in unsupported_attributes:
+                for attr, value in (unsupported_attributes := self._get_unsupported_field_info_attributes(metadata)):
                     warnings.warn(
                         f'The {attr!r} attribute with value {value!r} was provided to the `Field()` function, '
                         f'which has no effect in the context it was used. {attr!r} is field-specific metadata, '
@@ -2255,6 +2254,18 @@ class GenerateSchema:
                         'used, or if the `Field()` function was attached to a single member of a union type.',
                         category=UnsupportedFieldAttributeWarning,
                     )
+
+                if (
+                    metadata.default_factory_takes_validated_data
+                    and self.model_type_stack.get() is None
+                    and 'defaut_factory' not in unsupported_attributes
+                ):
+                    warnings.warn(
+                        "A 'default_factory' taking validated data as an argument was provided to the `Field()` function, "
+                        'but no validated data is available in the context it was used.',
+                        category=UnsupportedFieldAttributeWarning,
+                    )
+
             for field_metadata in metadata.metadata:
                 schema = self._apply_single_annotation(schema, field_metadata)
 
