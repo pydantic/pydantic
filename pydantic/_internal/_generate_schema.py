fa319414db73cff08febdd2c4917b0fafd4529bb
Tobias Pitters <tobias.pitters@gmail.com>
2025-10-18 17:35:03 +0200

    WIP: add fraction

--- pydantic/_internal/_generate_schema.py
+++ pydantic/_internal/_generate_schema.py
@@ -623,20 +623,7 @@ class GenerateSchema:
 
     def _fraction_schema(self) -> CoreSchema:
         """Support for [`fractions.Fraction`][fractions.Fraction]."""
-        from ._validators import fraction_validator
-
-        # TODO: note, this is a fairly common pattern, re lax / strict for attempted type coercion,
-        # can we use a helper function to reduce boilerplate?
-        return core_schema.lax_or_strict_schema(
-            lax_schema=core_schema.no_info_plain_validator_function(fraction_validator),
-            strict_schema=core_schema.json_or_python_schema(
-                json_schema=core_schema.no_info_plain_validator_function(fraction_validator),
-                python_schema=core_schema.is_instance_schema(Fraction),
-            ),
-            # use str serialization to guarantee round trip behavior
-            serialization=core_schema.to_string_ser_schema(when_used='always'),
-            metadata={'pydantic_js_functions': [lambda _1, _2: {'type': 'string', 'format': 'fraction'}]},
-        )
+        return core_schema.fraction_schema()
 
     def _arbitrary_type_schema(self, tp: Any) -> CoreSchema:
         if not isinstance(tp, type):
