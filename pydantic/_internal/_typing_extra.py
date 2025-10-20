c8035874e61637aa8e3178e102265e898a4f0302
Victorien <65306057+Viicos@users.noreply.github.com>
2025-10-20 17:05:47 +0200

    Fix issue with forward references in parent `TypedDict` classes (#12427)

--- pydantic/_internal/_typing_extra.py
+++ pydantic/_internal/_typing_extra.py
@@ -526,6 +526,13 @@ def _eval_type(
             globalns,
             localns,
             type_params=type_params,
+            # This is relevant when evaluating types from `TypedDict` classes, where string annotations
+            # are automatically converted to `ForwardRef` instances with a module set. In this case,
+            # Our `globalns` is irrelevant and we need to indicate `typing._eval_type()` that it should
+            # infer it from the `ForwardRef.__forward_module__` attribute instead (`typing.get_type_hints()`
+            # does the same). Note that this would probably be unnecessary if we properly iterated over the
+            # `__orig_bases__` for TypedDicts in `get_cls_type_hints()`:
+            prefer_fwd_module=True,
         )
         if evaluated is None:
             evaluated = type(None)
