0ff72f81f3dbfafcc6d4d35e2ea6e90f8322f02c
Viicos <65306057+Viicos@users.noreply.github.com>
2026-03-05 12:42:32 +0100

    feedback

--- pydantic/functional_validators.py
+++ pydantic/functional_validators.py
@@ -782,12 +782,7 @@ else:
 
         @classmethod
         def __get_pydantic_core_schema__(cls, source: Any, handler: GetCoreSchemaHandler) -> core_schema.CoreSchema:
-            from pydantic import (
-                PydanticForbiddenQualifier,
-                PydanticInvalidForJsonSchema,
-                PydanticSchemaGenerationError,
-                PydanticUndefinedAnnotation,
-            )
+            from pydantic._internal._generate_schema import GENERATE_SCHEMA_ERRORS
 
             # use the generic _origin_ as the second argument to isinstance when appropriate
             instance_of_schema = core_schema.is_instance_schema(_generics.get_origin(source) or source)
@@ -795,12 +790,7 @@ else:
             try:
                 # Try to generate the "standard" schema, which will be used when loading from JSON
                 original_schema = handler(source)
-            except (
-                PydanticForbiddenQualifier,
-                PydanticInvalidForJsonSchema,
-                PydanticSchemaGenerationError,
-                PydanticUndefinedAnnotation,
-            ):
+            except GENERATE_SCHEMA_ERRORS:
                 # If that fails, just produce a schema that can validate from python
                 return instance_of_schema
             else:
