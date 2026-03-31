bf52378af28ec4b6ebc71b2ff95bf73b64fc4afd
Victorien <65306057+Viicos@users.noreply.github.com>
2026-03-31 15:16:06 +0200

    Box large fields in CombinedValidator/CombinedSerializer to reduce enum size (#13006)
    Co-authored-by: Michele Zoncheddu <michelezoncheddu@outlook.it>

--- pydantic-core/src/validators/enum_.rs
+++ pydantic-core/src/validators/enum_.rs
@@ -46,7 +46,7 @@ impl BuildValidator for BuildEnumValidator {
         let class: Bound<PyType> = schema.get_as_req(intern!(py, "cls"))?;
         let class_repr = class_repr(schema, &class)?;
 
-        let lookup = LiteralLookup::new(py, expected.into_iter())?;
+        let lookup = Box::new(LiteralLookup::new(py, expected.into_iter())?);
 
         macro_rules! build {
             ($vv:ty, $name_prefix:literal) => {
@@ -87,7 +87,7 @@ pub trait EnumValidateValue: std::fmt::Debug + Clone + Send + Sync {
 pub struct EnumValidator<T: EnumValidateValue> {
     phantom: PhantomData<T>,
     class: Py<PyType>,
-    lookup: LiteralLookup<Py<PyAny>>,
+    lookup: Box<LiteralLookup<Py<PyAny>>>,
     missing: Option<Py<PyAny>>,
     expected_repr: String,
     strict: bool,
