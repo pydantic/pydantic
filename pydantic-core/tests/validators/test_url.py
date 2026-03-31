76ace355164ddbba491ada60750b22f657099f15
Arvind Saripalli <arvind@haventax.com>
2026-03-31 16:46:50 -0700

    Multi-host URL with none values test which fails

--- pydantic-core/tests/validators/test_url.py
+++ pydantic-core/tests/validators/test_url.py
@@ -1479,6 +1479,24 @@ def test_multi_url_build_hosts() -> None:
     )
 
 
+def test_multi_url_build_hosts_with_none_values() -> None:
+    hosts = [
+        {'host': 'host-1.com', 'password': 'pass', 'username': 'user', 'port': 27017},
+        {'host': 'host-2.com', 'password': None, 'username': None, 'port': 27017},
+    ]
+    url = MultiHostUrl.build(
+        scheme='mongodb',
+        hosts=hosts,
+        path='db',
+        query='replicaSet=xxx&authSource=admin',
+    )
+    assert str(url) == 'mongodb://user:pass@host-1.com:27017,host-2.com:27017/db?replicaSet=xxx&authSource=admin'
+    assert url.hosts() == [
+        {'username': 'user', 'password': 'pass', 'host': 'host-1.com', 'port': 27017},
+        {'username': None, 'password': None, 'host': 'host-2.com', 'port': 27017},
+    ]
+
+
 def test_multi_url_build_neither_host_and_hosts_set() -> None:
     with pytest.raises(ValueError):
         MultiHostUrl.build(
