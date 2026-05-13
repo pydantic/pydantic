498a486a3401331736810f95d84de44246f9841d
Ivan Levkivskyi <levkivskyi@gmail.com>
2026-05-13 16:52:43 +0100

    Update test output

--- tests/mypy/outputs/mypy-plugin_ini/create_model_var.py
+++ tests/mypy/outputs/mypy-plugin_ini/create_model_var.py
@@ -10,3 +10,12 @@ SubModel = create_model('SubModel', __base__=Model)
 
 class Main(BaseModel):
     sub: SubModel
+
+
+def dyn_model() -> type[BaseModel]:
+    Inner = create_model("Inner", __base__=BaseModel, value=(int, ...))
+
+    class Outer(Inner):
+        pass
+
+    return Outer
