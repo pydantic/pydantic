89bfa03d158b483b1412692697a4e9db9ffeefb0
Victorien <65306057+Viicos@users.noreply.github.com>
2026-05-16 12:03:36 +0200

    Apply suggestions from code review
    Co-authored-by: Victorien <65306057+Viicos@users.noreply.github.com>

--- tests/mypy/modules/create_model_var.py
+++ tests/mypy/modules/create_model_var.py
@@ -12,6 +12,7 @@ class Main(BaseModel):
     sub: SubModel
 
 
+# This only crashes in parallel mode (see https://github.com/python/mypy/issues/21472):
 def dyn_model() -> type[BaseModel]:
     Inner = create_model("Inner", __base__=BaseModel, value=(int, ...))
 
