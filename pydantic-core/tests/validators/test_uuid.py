65e625042eaae3da8eb587e088f6385d52eb0f8e
Benjamin A. Beasley <code@musicinmybrain.net>
2026-06-05 06:52:09 +0100

    Update `uuid` to 1.23.2; fix test expectations
    Release 1.23.2 contains https://github.com/uuid-rs/uuid/pull/882, which
    improved error messes. The change included a switch from one-based to
    zero-based indices, which affects one of the expected messages in
    `test_uuid`.

--- pydantic-core/tests/validators/test_uuid.py
+++ pydantic-core/tests/validators/test_uuid.py
@@ -38,7 +38,7 @@ class MyStr(str): ...
         (UUID('12345678-1234-5678-1234-567812345678'), UUID('12345678-1234-5678-1234-567812345678')),
         (UUID('550e8400-e29b-41d4-a716-446655440000'), UUID('550e8400-e29b-41d4-a716-446655440000')),
         # Invalid UUIDs
-        ('not-a-valid-uuid', Err('Input should be a valid UUID, invalid character: found `n` at 1')),
+        ('not-a-valid-uuid', Err('Input should be a valid UUID, invalid character: found `n` at 0')),
         (
             '12345678-1234-5678-1234-5678123456789',
             Err('Input should be a valid UUID, invalid group length in group 4: expected 12, found 13'),
