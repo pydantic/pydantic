948910b6fa6326bcee9ac45624e9657e1851184b
ferkans-amir <amir.rezaei@tu-berlin.de>
2026-07-27 23:04:28 +0200

    fix(types): end-anchor byte_string_pattern in ByteSize to reject trailing data

--- pydantic/types.py
+++ pydantic/types.py
@@ -2100,7 +2100,7 @@ class ByteSize(int):
     }
     byte_sizes.update({k.lower()[0]: v for k, v in byte_sizes.items() if 'i' not in k})
 
-    byte_string_pattern = r'^\s*(\d*\.?\d+)\s*(\w+)?'
+    byte_string_pattern = r'^\s*(\d*\.?\d+)\s*(\w+)?\s*$'
     byte_string_re = re.compile(byte_string_pattern, re.IGNORECASE)
 
     @classmethod
