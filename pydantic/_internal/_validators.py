db6073ee6503efe84e23b94e1d96f4c623d1c808
Victorien <65306057+Viicos@users.noreply.github.com>
2026-08-10 08:52:14 +0200

    Apply suggestions from code review
    Co-authored-by: Victorien <65306057+Viicos@users.noreply.github.com>

--- pydantic/_internal/_validators.py
+++ pydantic/_internal/_validators.py
@@ -389,7 +389,7 @@ def max_digits_validator(x: Any, max_digits: Any) -> Any:
 def decimal_places_validator(x: Any, decimal_places: Any) -> Any:
     try:
         decimal_places_, _ = _extract_decimal_digits_info(x)
-if decimal_places_ > decimal_places:
+        if decimal_places_ > decimal_places:
             normalized_decimal_places, _ = _extract_decimal_digits_info(x.normalize())
             if normalized_decimal_places > decimal_places:
                 raise PydanticKnownError(
