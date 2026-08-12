c439459177654cc30b9ae45091d53f1a004610d6
Victorien <65306057+Viicos@users.noreply.github.com>
2026-08-12 10:44:39 +0200

    Workaround circular import error in Mypy (#13640)

--- pydantic/mypy.py
+++ pydantic/mypy.py
@@ -7,6 +7,8 @@ from collections.abc import Callable, Iterator
 from configparser import ConfigParser
 from typing import Any
 
+# To avoid import error (https://github.com/python/mypy/issues/17726):
+import mypy.types  # noqa: F401
 from mypy.errorcodes import ErrorCode
 from mypy.expandtype import expand_type, expand_type_by_instance
 from mypy.nodes import (
